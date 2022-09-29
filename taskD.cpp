a855df2e6df91781103e65b57af0f2e9322251ac
seesmof <110289150+seesmof@users.noreply.github.com>
2022-09-29 19:50:19 +0300

    fix: :bug: make taskD work, yoohoo

--- taskD.cpp
+++ taskD.cpp
@@ -3,34 +3,26 @@
 #include <math.h>
 using namespace std;
 
-// FIXME: this doesn't work
 int main(int argc, char **argv)
 {
-    int x;
-    int result;
-
-    double pi = 2 * acos(0.0);
+    double x, z, f;
+    z = pow(3, asin(x));
 
     cout << "Enter x: " << endl;
     cin >> x;
 
-    // Calculate z which equals to arcsin(x) to the power of 3
-    int z = pow(3, asin(x));
-
-    if (z > 0.5)
+    if (z > -0.5)
     {
-        result = (pow(2, 2 * z + 1)) / (2.75 - pow(2, x));
+        f = pow(2, (2 * z + 1)) / 2.75 - pow(2, x);
     }
     else if (z >= -0.5 && z <= 0.001)
     {
-        result = pow(3, sin(z)) - sin(z / 3 * double(pi));
+        f = pow(3, sin(z)) - sin(z / 3);
     }
     else if (z > 0.001)
     {
-        result = (tan(z + x) - exp(x)) / (3.5 * x);
+        f = (tan(z + x) - exp(x)) / (3.5 * x);
     }
 
-    cout << "Result is: " << result << endl;
-
-    return 0;
+    cout << "The result is: " << f << endl;
 }
