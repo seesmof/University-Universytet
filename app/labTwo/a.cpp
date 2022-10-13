7bcc0e42712d709cfca25bac3b6b74b367c85561
seesmof <110289150+seesmof@users.noreply.github.com>
2022-10-13 18:02:48 +0300

    chore: :construction: working
    finish tasks b and d for lab two
    
    tasks a and c pending

--- app/labTwo/a.cpp
+++ app/labTwo/a.cpp
@@ -1,10 +1,18 @@
+// include necessary libraries
 #include <iostream>
-#include <math.h>
-
 using namespace std;
 
+// declare main function
 int main(int argc, char **argv)
 {
+    // declare variables A and B
+    int iA, iB;
 
-    return 0;
-} // 2.3.2.23.a
+    // ask the user to input A and B
+    cout << "Please enter A and B: ";
+    cin >> iA >> iB;
+
+    // create for loop to devide the numbers by two
+    // create a new variable to keep track of the number of iterations the loop had
+    // if the iterations number is more than 5, output an error, otherwise output success message
+}
