fe27158617ffec43cee0c85d73290594b333313a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-02-21 10:17:13 +0200

    chore: :construction: working
    start of term one year one, working on lab one

---
+++ test/tap_template.cpp
@@ -0,0 +1,39 @@
+// include necessary libraries
+#include "../../../lib.h"
+using namespace std;
+
+// function prototypes //
+
+/////////////////////////
+
+// func main start
+int main()
+{
+    // declare local variables //
+    srand(time(NULL));
+    char doContinue;
+    int userDecision;
+    /////////////////////////////
+
+    // project intro
+    cout << "\n";
+    do
+    {
+        //////////////////////////////////////////////////////////////////////////////////
+        cout << "Testing\n";
+        //////////////////////////////////////////////////////////////////////////////////
+        cout << "\nWould you like to continue program execution? (Y | N): ";
+        cin >> doContinue;
+        if (doContinue == 'Y' || doContinue == 'y')
+        {
+            cout << "\n\n";
+            continue;
+        }
+        else
+            break;
+    } while (doContinue = 'Y' || doContinue == 'y');
+
+    // func main end
+    cout << "\nThanks for using this program\n\n";
+    return 0;
+}
