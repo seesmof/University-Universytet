414ce7bdf8b83cf9396789b4510d16c30419b424
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-05 12:22:21 +0300

    update test.asm

--- t2y1/asm_lab3/test.asm
+++ t2y1/asm_lab3/test.asm
@@ -1,51 +1,45 @@
-.model tiny     ; set program model as tiny
+comment @
+    Онищенко Олег
+    КНТ-122
+@
+
+.model small    ; set program model as small
+.stack 100h     ; set stack size to 100h
+
+sseg segment para stack 'stack'     ; declare stack segment
+    db 256 dup(?)       ; reserve memory for stack
+sseg ends   ; end stack segment 
+
+dseg segment para public 'data'     ; declare data segment
+    g   equ 100
+    a   db 0
+    b   dw ?
+    c   db 'A'
+    d   db 65
+    e   db 041h
+dseg ends   ; end data segment
 
 cseg segment para public 'code'     ; declare code segment
-    assume cs:cseg, ds:cseg, ss:cseg, es:nothing    ; set each code segment to code seg as it is the only segment in the program 
-    org 100h    ; start loading the first instruction at 100h
+    assume cs:cseg, ss:sseg, es:nothing ; set segment register to corresponding ones
 
 start:  ; declare program entry point
-    symbol db 'U'   ; declare a symbol to output
+    assume ds:dseg  ; set data segment register
+    mov bx, dseg    ; add data segment to bx register
+    mov ds, bx  ; set ds register to bx register
 
-    xor ax, ax   ; clear ax register
-    mov dl, symbol      ; load symbol to stdout 
+    call main   ; call main function
+
+    mov ah, 04Ch     ; exit to OS
+    mov bl, 06Ch     ; set error code to 108 in hex
+    int 21h     ; call interrupt
+
+main proc near  ; declare main function
+    mov dl, symbol  ; load symbol into dl register
     mov ah, 02h     ; output symbol to stdout
     int 21h     ; call interrupt
 
-    mov dl, 'k'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
- 
-    mov dl, 'r'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
+    ret     ; stop function execution
+main endp   ; end main function
 
-    mov dl, 'a'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
-
-    mov dl, 'i'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
-
-    mov dl, 'n'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
-
-    mov dl, 'e'     ; load symbol to stdout
-    mov ah, 02h     ; output symbol to stdout
-    int 21h     ; call interrupt
-
-    mov dl, 10      ; set dl register to new line
-    mov ah, 02h     ; output it to stdout
-    int 21h     ; call interrupt
-    mov dl, 13      ; set dl register to carret return
-    mov ah, 02h     ; output it to stdout
-    int 21h     ; call interrupt
-
-    mov ah, 04Ch    ; exit to OS
-    mov bl, 6Ch     ; set error code to 108 in hex 
-    int 21h         ; call interrupt
-
-cseg ends   ; close segment
+cseg ends   ; end code segment
 end start   ; end program execution
