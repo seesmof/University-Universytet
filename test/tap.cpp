ee467470d34a22ff93e074592f1b7c376fb9a824
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-07 18:39:34 +0300

    test: update tap.cpp and tap.exe

--- test/tap.cpp
+++ test/tap.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
+#include "../lib.h"
 
 int main()
 {
-    std::cout << "----------------------------------------" << std::endl;
-    std::cout << "|             Onyshchenko Oleh                  |" << std::endl;
-    std::cout << "|          Software Developer           |" << std::endl;
-    std::cout << "|           Acme Corporation            |" << std::endl;
-    std::cout << "|                                       |" << std::endl;
-    std::cout << "|      email: john.doe@acme.com         |" << std::endl;
-    std::cout << "|      phone: +1 (555) 123-4567         |" << std::endl;
-    std::cout << "|        www.acme.com/john.doe          |" << std::endl;
-    std::cout << "----------------------------------------" << std::endl;
+    short n = 0;
+    cout << "Loop: 0";
+    while (n < 5, ++n)
+        cout << n << " ";
+    cout << "N = " << n;
 
     return 0;
 }
