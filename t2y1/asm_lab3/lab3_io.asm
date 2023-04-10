0d388daf38d8a9bb64c4f38f1a4e817af4ddda03
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-10 13:50:57 +0300

    feat: create lab3_io.asm

---
+++ t2y1/asm_lab3/lab3_io.asm
@@ -0,0 +1,41 @@
+.model small    ; set program model as small
+.stack 100h     ; set stack size to 100h
+
+cseg segment para public 'code'     ; declare code segment
+    assume cs:cseg, ss:sseg, es:nothing ; set segment register to corresponding ones
+
+start:  ; declare program entry point
+    assume ds:dseg  ; set data segment register
+    mov bx, dseg    ; add data segment to bx register
+    mov ds, bx  ; set ds register to bx register
+
+    call main   ; call main function
+
+    mov ah, 04Ch     ; exit to OS
+    mov bl, 06Ch     ; set error code to 108 in hex
+    int 21h     ; call interrupt
+
+main proc near  ; declare main function
+    lea dx, edl
+    call outputString ; set output string
+
+    ret     ; stop function execution
+main endp   ; end main function
+
+outputString proc near
+    sub ax, ax
+    mov ah, 09h
+    int 21h
+    ret
+outputString endp
+cseg ends   ; end code segment
+
+sseg segment para stack 'stack'     ; declare stack segment
+    db 256 dup(?)       ; reserve memory for stack
+sseg ends   ; end stack segment 
+
+dseg segment para public 'data'     ; declare data segment
+    
+dseg ends   ; end data segment
+
+end start   ; end program execution
