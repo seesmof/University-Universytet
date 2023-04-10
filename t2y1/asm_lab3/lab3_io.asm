847352fe277deab02f5cf31acd34507890613d22
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-10 13:54:41 +0300

    update lab3_io.asm

--- t2y1/asm_lab3/lab3_io.asm
+++ t2y1/asm_lab3/lab3_io.asm
@@ -47,7 +47,9 @@ sseg ends   ; end stack segment
 
 dseg segment para public 'data'     ; declare data segment
     new_line db 0Dh, 0Ah, '$'
-    input_buffer db 100 dup (0)
+    input_buffer db 10
+    input_length db ?
+    buffer_cont db 10 (' ')
 dseg ends   ; end data segment
 
 end start   ; end program execution
