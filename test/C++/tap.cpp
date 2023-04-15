9f971918f90e15a9534b0919b01bda4a95f5aa9c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-15 14:56:34 +0300

    delete 6 files

---
+++ test/C++/tap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}[];:,.<>?"; // Define the characters to use in the password
+    const int password_length = 64;                                                                               // Define the desired length of the password
+    string password = "";                                                                                         // Initialize an empty string for the password
+    srand(time(NULL));                                                                                            // Seed the random number generator with the current time
+    for (int i = 0; i < password_length; i++)
+    {                                               // Loop through the desired length of the password
+        password += chars[rand() % chars.length()]; // Add a random character from the character string to the password
+    }
+    cout << "Random Password: " << password << endl; // Print the generated password
+    return 0;
+}
--- test/С++/tap.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../lib.h"
-
-int main()
-{
-    return 0;
-}
