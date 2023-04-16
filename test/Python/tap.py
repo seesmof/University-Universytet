3f40f9440798e3ce75b502a42a821626726729ce
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-16 11:02:42 +0300

    update 1 file and delete 1 file

--- test/Python/tap.py
+++ test/Python/tap.py
@@ -1,49 +1,33 @@
+import tkinter as tk
 import requests
-import re
 
-question = input(": ")
 
-url = "https://www.phind.com/api/infer/creative"
-data = {
-    "question": question,
-    "codeContext": "",
-    "options": {
-        "skill": "intermediate",
-        "date": "14/04/2023",
-        "language": "en-GB",
-        "detailed": False,
-        "creative": True
-    }
-}
-headers = {
-    "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-    "Accept": "*/*",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Origin": "https://www.phind.com",
-    "Referer": "https://www.phind.com/search?q=Go+vs+Rust+vs+C%2B%2B&c=&source=searchbox&init=true",
-    "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \";Not A Brand\";v=\"99\"",
-    "sec-ch-ua-mobile": "?0",
-    "Connection": "keep-alive",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site"
-}
-cookies = {
-    "__cf_bm": "",
-    "mp__mixpanel": "",
-}
-response = requests.post(url, json=data, headers=headers, cookies=cookies)
-print(response.status_code)
+def get_weather():
+    city = city_entry.get()
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY'
+    response = requests.get(url)
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = str(round(data['main']['temp'] - 273.15, 1)) + '°C'
+    weather_label.config(text=f'Weather: {weather}')
+    temperature_label.config(text=f'Temperature: {temperature}')
 
-data = response.text
-text = data.replace("data: ", "")
-text = text.replace("\n ", "")
-text = f'''{text}'''
-text = text.replace('''\r\n\r\n''', "")
-text = text.replace('''\r\n\r''', " ")
-text = text.replace('''`''', "")
 
-print(text)
+root = tk.Tk()
+
+city_label = tk.Label(root, text='City:')
+city_label.pack()
+
+city_entry = tk.Entry(root)
+city_entry.pack()
+
+button = tk.Button(root, text='Get Weather', command=get_weather)
+button.pack()
+
+weather_label = tk.Label(root, text='')
+weather_label.pack()
+
+temperature_label = tk.Label(root, text='')
+temperature_label.pack()
+
+root.mainloop()
