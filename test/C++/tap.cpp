ed9b9d142270ffe2869ee89ad81ebddffc323a2a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-20 13:25:17 +0000

    Thursday, April 20, 2023, 4:25:17 PM Eastern European Summer Time

--- test/C++/tap.cpp
+++ test/C++/tap.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 float MyFunction(int number)
 {
-    float result = 1 for (int a = 0; a < number; a++)
+    float result = 1;
+    for (int a = 0; a < number; a++)
     {
         result = result * (a < 10 ? 2 : 1.5);
     }
