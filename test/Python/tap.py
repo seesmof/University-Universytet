ed3f04317b7a70eed6ae02460b8ed4b503a2ff63
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-21 13:18:03 +0000

    Friday, April 21, 2023, 4:18:03 PM Eastern European Summer Time

--- test/Python/tap.py
+++ test/Python/tap.py
@@ -34,29 +34,13 @@ headers = {
 }
 
 response = requests.post(url, json=data, headers=headers)
+print(response.status_code)
 
 data = response.text
 text = data.replace("data: ", "")
 text = text.replace("\n ", "")
-
 text = f'''{text}'''
-text = text.replace('/r/n/r/n/r/n/r/n', "\n")
-text = text.replace('/r/n/r/n/r/n', "\n")
-text = text.replace('/r/n/r/n/r', "\n")
-text = text.replace('/r/n/r/n', "")
-text = text.replace('/r/n/r', " ")
-text = text.replace('/r/n', "")
-text = text.replace('/r', "")
-text = text.replace(':', ": \n")
-text = text.replace(':1.', ":\n1.")
-text = text.replace('.1.', ".\n1.")
-text = text.replace('.2.', ".\n2.")
-text = text.replace('.3.', ".\n3.")
-text = text.replace('.4.', ".\n4.")
-text = text.replace('.5.', ".\n5.")
-text = text.replace('.6.', ".\n6.")
-text = text.replace('.7.', ".\n7.")
-text = text.replace('.8.', ".\n8.")
-text = text.replace('.9.', ".\n9.")
-text = text.replace('.10.', ".\n10.")
+text = text.replace('''\r\n\r\n''', "")
+text = text.replace('''\r\n\r''', " ")
+
 print(text)
