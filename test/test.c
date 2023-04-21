f890b827d9bce96449408912eb1a401eaa1aa84f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-21 06:45:48 +0000

    Friday, April 21, 2023, 9:45:48 AM Eastern European Summer Time

--- test/test.c
+++ test/test.c
@@ -1,7 +1,7 @@
 float MyFunction(int number)
 {
     float result = 1
-    for (int a = 0; a < number; a++)
+    for (int a = 0; a < number; a += 5)
     {
         result = result * (a < 10 ? 2 : 1.5);
     }
