864f1f784bb14aeccc7c4273dc1075de318d71a1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-21 13:21:14 +0000

    Friday, April 21, 2023, 4:21:14 PM Eastern European Summer Time

--- year1-term2/OOP/lb7/tsk2/sup.h
+++ year1-term2/OOP/lb7/tsk2/sup.h
@@ -2,19 +2,3 @@
 #include "sup.cpp"
 #include "D:\repos\university\lib.h"
 
-class DynamicString;
-
-// for showing the main menu of the application
-void outputMenu(vector<DynamicString> &container);
-
-// define an output stream operator function
-ostream &operator<<(ostream &outputStream, const DynamicString &OUTPUT);
-
-// define an input stream operator function
-istream &operator>>(istream &inputStream, DynamicString &inputHolder);
-
-// define an output stream operator function for file
-ofstream &operator<<(ofstream &outputStream, const DynamicString &OUTPUT);
-
-// define an input stream operator function for file
-ifstream &operator>>(ifstream &inputStream, DynamicString &inputHolder);
