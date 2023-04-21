5b8433b633957e17d3655aade1a62c08845b3174
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-21 13:40:05 +0000

    Friday, April 21, 2023, 4:40:05 PM Eastern European Summer Time

--- year1-term2/OOP/lb7/tsk2/main.cpp
+++ year1-term2/OOP/lb7/tsk2/main.cpp
@@ -1,11 +1,9 @@
-// include necessary libraries
 #include "D:\repos\university\lib.h"
 #include "sup.h"
 using namespace std;
 
 // Виконати завдання з лабораторної роботи #5 з довільним типом даних
 
-// func main start
 int main()
 {
     // declare local variables //
@@ -24,29 +22,23 @@ int main()
 
         do
         {
-            // output menu to user
             outputMenu(container);
 
-            // ask user if they would like to return to menu
             cout << "\nWould you like to return to menu? (Y | N): ";
             cin >> doReturnToMenu;
-            // if so, continue loop execution
             if (doReturnToMenu == 'Y' || doReturnToMenu == 'y')
             {
                 cout << endl
                      << endl;
                 continue;
             }
-            // if not, break out of loop
             else
                 break;
 
         } while (doReturnToMenu == 'y' || doReturnToMenu == 'Y');
-        // execute while user chooses to return to menu
 
         ///////////////////////////////////////
 
-        // ask user if they would like to continue execution of program
         cout << "\nWould you like to continue program execution? (Y | N): ";
         cin >> doContinue;
         if (doContinue == 'Y' || doContinue == 'y')
@@ -58,7 +50,6 @@ int main()
             break;
     } while (doContinue = 'Y' || doContinue == 'y');
 
-    // stop main function execution
     cout << "\nThanks for using this program\n\n";
     return 0;
 }
