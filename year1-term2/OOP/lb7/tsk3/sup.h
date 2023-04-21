1da9b3fd44b3495a8f86b9cdbfe16de6d1c5e534
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-21 13:55:55 +0000

    Friday, April 21, 2023, 4:55:55 PM Eastern European Summer Time

--- year1-term2/OOP/lb7/tsk3/sup.h
+++ year1-term2/OOP/lb7/tsk3/sup.h
@@ -1,17 +1,3 @@
 #pragma once
-#include <bits/stdc++.h>
 #include "sup.cpp"
-
-using namespace std;
-
-// delta class declaration
-class Delta;
-
-// object deletion function
-void deleteObjects(vector<unique_ptr<Delta>> &deltaObjectsVector);
-
-// object creation function
-void createObjects(vector<unique_ptr<Delta>> &deltaObjectsVector);
-
-// printing objects function
-void printObjects(vector<unique_ptr<Delta>> &deltaObjectsVector);
+#include "D:\repos\university\lib.h"
