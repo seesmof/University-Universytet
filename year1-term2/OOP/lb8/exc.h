3f01da50f2ff0582a7af523658b84795da83339a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-23 10:27:21 +0000

    Sunday, April 23, 2023, 1:27:21 PM Eastern European Summer Time

--- year1-term2/OOP/lb8/exc.h
+++ year1-term2/OOP/lb8/exc.h
@@ -3,12 +3,3 @@
 #include "D:\repos\university\lib.h"
 
 class Exception;
-class IOException : public Exception
-{
-};
-class ArithmeticException : public Exception
-{
-};
-class MemoryException : public Exception
-{
-};
