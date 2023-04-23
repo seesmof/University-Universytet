54674cf2721a457c2eb6c6e76a04d0fe92e8da59
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-23 10:27:03 +0000

    Sunday, April 23, 2023, 1:27:03 PM Eastern European Summer Time

--- year1-term2/OOP/lb8/exc.h
+++ year1-term2/OOP/lb8/exc.h
@@ -1,3 +1,14 @@
 #pragma once
 #include "exc.cpp"
 #include "D:\repos\university\lib.h"
+
+class Exception;
+class IOException : public Exception
+{
+};
+class ArithmeticException : public Exception
+{
+};
+class MemoryException : public Exception
+{
+};
--- year1-term2/OOP/lb8/sup.h
+++ year1-term2/OOP/lb8/sup.h
@@ -3,18 +3,9 @@
 #include "D:\repos\university\lib.h"
 
 class DynamicString;
-
-// for showing the main menu of the application
+void showStrings(vector<DynamicString> &container);
+void showStrings(vector<DynamicString> &container, const string &FILE);
+void addStrings(vector<DynamicString> &container);
+void addStrings(vector<DynamicString> &container, const string &FILE);
+void removeString(vector<DynamicString> &container);
 void outputMenu(vector<DynamicString> &container);
-
-// define an output stream operator function
-ostream &operator<<(ostream &outputStream, const DynamicString &OUTPUT);
-
-// define an input stream operator function
-istream &operator>>(istream &inputStream, DynamicString &inputHolder);
-
-// define an output stream operator function for file
-ofstream &operator<<(ofstream &outputStream, const DynamicString &OUTPUT);
-
-// define an input stream operator function for file
-ifstream &operator>>(ifstream &inputStream, DynamicString &inputHolder);
