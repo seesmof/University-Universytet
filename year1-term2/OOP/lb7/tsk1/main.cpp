98a1acd99c97d47e6eb3f9f5360f44c88b87ae8a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 07:45:57 +0000

    Monday, April 24, 2023, 10:45:57 AM Eastern European Summer Time

--- year1-term2/OOP/lb7/tsk1/main.cpp
+++ year1-term2/OOP/lb7/tsk1/main.cpp
@@ -1,18 +1,15 @@
-// include necessary libraries
 #include "D:\repos\university\lib.h"
 #include "sup.h"
 
 // Створити клас Delta таким чином, щоб кожний об’єкт вміщував свій персональний номер (дескриптор об’єкта) та функцію, яка повертає його значення. Дескриптор об’єкта – унікальне для об’єктів даного типу ціле число.
 // Виконати завдання з лабораторної роботи №1, де тип елементу заданої структури даних довільний. Використати шаблонні функції.
 
-// func main start
 int main()
 {
     srand(time(NULL));
     char doContinue;
     char doReturnToMenu;
 
-    // project intro
     cout << "\n";
     do
     {
@@ -21,32 +18,20 @@ int main()
         vector<string> menuItems = {
             "Integer",
             "Double",
-
             "String",
             "Exit"};
-        cout << BOLD << "Choose a data type\n"
-             << UNBOLD;
-        cout << "1. Integer\n";
-        cout << "2. Double\n";
-        cout << "3. String\n";
-        cout << "4. Exit\n";
-        cout << "Enter: ";
-        userDecision = getNum();
-        cout << endl;
+        ll userDecision = showMenu(menuItems);
 
-        // if user chose to add objects
         if (userDecision == 1)
         {
             vector<unique_ptr<Delta<int>>> deltaObjectsVector;
             outputMenu(deltaObjectsVector);
         }
-        // if user chose to delete objects
         else if (userDecision == 2)
         {
             vector<unique_ptr<Delta<double>>> deltaObjectsVector;
             outputMenu(deltaObjectsVector);
         }
-        // if user chose to print objects
         else if (userDecision == 3)
         {
             vector<unique_ptr<Delta<string>>> deltaObjectsVector;
@@ -55,7 +40,6 @@ int main()
 
         ///////////////////////////////////////
 
-        // ask user if they would like to continue execution of program
         cout << "\nWould you like to continue program execution? (Y | N): ";
         cin >> doContinue;
         if (doContinue == 'Y' || doContinue == 'y')
