90daac3bb90d73b0e31470d97281920dd9f109ae
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 12:30:01 +0000

    Monday, April 24, 2023, 3:30:01 PM Eastern European Summer Time

--- year1-term2/ASM/lb3/test.asm
+++ year1-term2/ASM/lb3/test.asm
@@ -27,6 +27,8 @@ main proc near  ; declare main function
 
     lea dx, my_string
     call outputString
+    lea dx, new_line
+    call outputString
 
     ret
 main endp   ; end main function
@@ -57,7 +59,7 @@ dseg segment para public 'data'     ; declare data segment
     my_byte db 10h
     my_word dw 1234h
     my_dword dd 5678h
-    my_string db 'Oleh Onyshchenko KHT-122', 0
+    my_string db 'Oleh Onyshchenko KHT-122', '$'
     my_array db 1, 2, 3, 4, 5
     my_matrix dw 1, 2, 3, 4, 5, 6, 7, 8, 9
 dseg ends   ; end data segment
