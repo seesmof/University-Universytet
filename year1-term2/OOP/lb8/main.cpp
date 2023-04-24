ce8638f63279f25c797b99a19ce2ace1aef063a5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 09:44:45 +0000

    Monday, April 24, 2023, 12:44:45 PM Eastern European Summer Time

--- year1-term2/OOP/lb8/main.cpp
+++ year1-term2/OOP/lb8/main.cpp
@@ -1,4 +1,3 @@
-// include necessary libraries
 #include "D:\repos\university\lib.h"
 #include "sup.h"
 #include "exc.h"
@@ -6,50 +5,37 @@ using namespace std;
 
 // Для завдання з лабораторної роботи #5 виконати обробку виняткових ситуацій з використанням класу Exception.
 
-// func main start
 int main()
 {
-    // declare local variables //
     srand(time(NULL));
     char doContinue;
-    /////////////////////////////
+    vector<DynamicString> container;
+    char doReturnToMenu;
 
-    // project intro
     cout << "\n";
     do
     {
         ///////////////////////////////////////
 
-        // for storing class object pointers
-        vector<DynamicString> container;
-        // for manipulating program flow
-        char doReturnToMenu;
-
         do
         {
-            // output menu to user
             outputMenu(container);
 
-            // ask user if they would like to return to menu
             cout << "\nWould you like to return to menu? (Y | N): ";
             cin >> doReturnToMenu;
-            // if so, continue loop execution
             if (doReturnToMenu == 'Y' || doReturnToMenu == 'y')
             {
                 cout << endl
                      << endl;
                 continue;
             }
-            // if not, break out of loop
             else
                 break;
 
         } while (doReturnToMenu == 'y' || doReturnToMenu == 'Y');
-        // execute while user chooses to return to menu
 
         ///////////////////////////////////////
 
-        // ask user if they would like to continue execution of program
         cout << "\nWould you like to continue program execution? (Y | N): ";
         cin >> doContinue;
         if (doContinue == 'Y' || doContinue == 'y')
@@ -61,7 +47,6 @@ int main()
             break;
     } while (doContinue = 'Y' || doContinue == 'y');
 
-    // stop main function execution
     cout << "\nThanks for using this program\n\n";
     return 0;
 }
