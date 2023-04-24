d415d8e7090a6839de80e03ec3667bc96613784f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 07:17:06 +0000

    Monday, April 24, 2023, 10:17:06 AM Eastern European Summer Time

--- test/C++/tap.cpp
+++ test/C++/tap.cpp
@@ -1,1 +1,17 @@
-#in
+#include "D:\repos\university\lib.h"
+
+template <typename T>
+T _max(T a, T b)
+{
+    return a > b ? a : b;
+}
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << _max(a, b);
+    double a, b;
+    cin >> a >> b;
+    cout << _max(a, b);
+}
