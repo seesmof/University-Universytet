1406a699cb49108cc71ae0dd533e84bf96c01e38
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-21 19:04:34 +0000

    🌿: Sunday, May 21, 2023 at 10:04:34 PM Eastern European Summer Time

--- year1-term2/ASM/lb4/new.asm
+++ year1-term2/ASM/lb4/new.asm
@@ -1,116 +1,66 @@
-.model small
-.stack 100h
+.MODEL SMALL
+.STACK 100H
 
-.data
-num1 db 6 ; maximum number of digits to read for num1
-num2 db 6 ; maximum number of digits to read for num2
-buf1 db 6 dup('?') ; buffer to store num1 string
-buf2 db 6 dup('?') ; buffer to store num2 string
-result dw ? 
+.DATA
+    MSG1 DB 0DH, 0AH, "Enter the first number: $"
+    MSG2 DB 0DH, 0AH, "Enter the second number: $"
+    RESULT DB 0DH, 0AH, "Result in binary: $"
+    BUFFER DB 9 DUP ('$') ; Buffer to store the binary representation
 
-.code
-start:
-mov ax, @data
-mov ds, ax
+.CODE
+.STARTUP
+    MOV AX, @DATA
+    MOV DS, AX
 
-; Prompt user to enter first number
-mov ah, 9
-mov dx, offset msg_num1
-int 21h
+    ; Outputting a message to the console (message 1)
+    MOV AH, 09H
+    LEA DX, MSG1
+    INT 21H
 
-; Read first number from user input
-mov ah, 0Ah
-mov dx, offset buf1
-int 21h
+    ; Reading the first number from the console
+    MOV AH, 01H
+    INT 21H
+    SUB AL, '0'
+    MOV BL, AL
 
-; Convert string to number
-call parse_num1
+    ; Outputting a message to the console (message 2)
+    MOV AH, 09H
+    LEA DX, MSG2
+    INT 21H
 
-; Prompt user to enter second number
-mov ah, 9
-mov dx, offset msg_num2
-int 21h
+    ; Reading the second number from the console
+    MOV AH, 01H
+    INT 21H
+    SUB AL, '0'
+    MOV CL, AL
 
-; Read second number from user input
-mov ah, 0Ah
-mov dx, offset buf2
-int 21h
+    ; Adding the numbers together
+    ADD BL, CL
 
-; Convert string to number
-call parse_num2
+    ; Converting the result into binary
+    MOV CH, 8
+    LEA SI, BUFFER
 
-; Add the numbers and store result in 'result' variable
-mov ax, num1
-add ax, num2
-mov result, ax
+CONVERT_LOOP:
+    ROL BL, 1 ; Rotate left through carry
+    ADC DL, 0 ; Add carry to DL
+    ADD DL, '0' ; Convert DL to ASCII character
+    MOV [SI], DL ; Store character in buffer
+    INC SI
 
-; Convert result to binary using 16-bit registers
-mov bx, 0
-mov cx, 16
+    LOOP CONVERT_LOOP
 
-loop1:
-rol ax, 1
-adc bh, 0
-loop loop1
+    ; Displaying the resulting text string to the console
+    MOV AH, 09H
+    LEA DX, RESULT
+    INT 21H
 
-; Print the binary result to console
-mov ah, 9
-mov dx, offset msg_result
-int 21h
+    ; Displaying the binary representation from the buffer
+    MOV DX, OFFSET BUFFER
+    MOV AH, 09H
+    INT 21H
 
-mov cx, 16
-
-loop2:
-shr bh, 1
-rcr result, 1
-mov dl, '0'
-adc dl, 0
-mov ah, 2
-int 21h
-loop loop2
-
-mov ah, 4ch
-int 21h
-
-parse_num1:
-mov si, offset buf1
-mov cl, [si] ; first character in buffer is count of digits
-inc si ; move to start of actual digits
-mov al, 0 ; clear out register
-loop1:
-mov bl, [si] ; current character
-inc si ; move to next character
-cmp bl, 0x0d ; check for carriage return
-je done1
-sub bl, '0' ; convert from ASCII to number
-mul byte ptr [num10] ; multiply current number by 10
-add al, bl ; add to current number
-jmp loop1
-done1:
-mov num1, ax ; store converted number
-ret
-
-; Subroutine to convert string buffer to number
-parse_num2:
-mov si, offset buf2
-mov cl, [si] ; first character in buffer is count of digits
-inc si ; move to start of actual digits
-mov al, 0 ; clear out register
-loop2:
-mov bl, [si] ; current character
-inc si ; move to next character
-cmp bl, 0x0d ; check for carriage return
-je done2
-sub bl, '0' ; convert from ASCII to number
-mul byte ptr [num10] ; multiply current number by 10
-add al, bl ; add to current number
-jmp loop2
-done2:
-mov num2, ax ; store converted number
-ret
-
-msg_num1 db 'Enter the first number: $'
-msg_num2 db 'Enter the second number: $'
-msg_result db 'The binary result is: $'
-num10 db 10 ; used for converting ASCII to number
-end start
+.EXIT
+    MOV AH, 4CH
+    INT 21H
+END
