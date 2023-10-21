9da0fe22d4dee5ed0778452458b896ddde63aeb9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-21 15:19:48 +0300

    feat: destroyed

--- year1/term2/ООП/lb7/tsk3/sup.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sup.cpp"
-#include "D:\repos\university\lib.h"
-
-class Delta;
-void showObjs(vector<unique_ptr<Delta>> &container);
-void addObjs(vector<unique_ptr<Delta>> &container);
-void delObjs(vector<unique_ptr<Delta>> &container);
-void outputMenu(vector<unique_ptr<Delta>> &container);
