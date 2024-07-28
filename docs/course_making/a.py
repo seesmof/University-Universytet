16979effb9a2845dd812b5fd94c9a309d42e1248
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-07-28 18:24:08 +0300

    feat: HALLELUJAH THANK YOU JESUS CHRIST our HOLY LORD GOD ALMIGHTY KING OF KINGS AND LORD OF LORDS

--- docs/course_making/a.py
+++ docs/course_making/a.py
@@ -1,6 +1,13 @@
 import os
 
 
+def create_folder(course_name: str):
+    course_dir = os.path.join(root, course_name)
+    os.mkdir(course_dir)
+    for folder in folders:
+        os.mkdir(os.path.join(course_dir, folder))
+
+
 folders = [
     "exams",
     "notes",
@@ -8,13 +15,12 @@ folders = [
     "tasks",
     "terms",
 ]
-course_name = input("Введіть назву курсу: ")
-course_name = course_name.upper().strip()
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
+courses = os.path.join(current_dir, "..", "courses")
 root = os.path.join(current_dir, "..", "..")
+current_id = "31"
 
-course_dir = os.path.join(root, course_name)
-os.mkdir(course_dir)
-for folder in folders:
-    os.mkdir(os.path.join(course_dir, folder))
+with open(os.path.join(courses, current_id + ".txt"), "r", encoding="utf-8") as f:
+    courses = f.read().splitlines()
+for course in courses:
+    create_folder(course)
