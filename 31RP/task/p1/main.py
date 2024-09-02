68027924be9c40bf6dcf21ed7c3ab40548570616
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-02 20:52:05 +0300

    feat: JESUS THANK YOU JESUS HALLELUJAH

---
+++ 31RP/task/p1/main.py
@@ -0,0 +1,12 @@
+""" 
+movies grid
+for each movie a page with different rooms 
+for each room a grid of seats: marked red if busy and green if free
+    each seat is a button, when pressed it will add a sit to our ticket somewhere on the right 
+once the seats are selected, a user can either buy the ticket or cancel it
+in main menu there would be a section with tickets with ability to give up a ticket 
+
+optionally:
+add dates for each ticket and status to ticket 
+    if current date is later than the ticket, set it as expired
+"""
