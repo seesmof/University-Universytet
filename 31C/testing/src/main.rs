29b41f631a86d2d60f74f0fc9e667731983b3d1d
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-10 23:04:19 +0300

    feat: rust loops, JESUS THANK YOU JESUS HALLELUJAH

--- 31C/testing/src/main.rs
+++ 31C/testing/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    let x: u8 = 0;
-    println!("JESUS THANK YOU JESUS, JESUS please let x be {x}", x = x);
+    for i in 1..5 {
+        println!("JESUS THANK YOU for the first loop - {} {}", i, i * i);
+    }
+    for i in 1..=10 {
+        println!("JESUS THANK YOU for the second loop - {} {}", i, i * i);
+    }
+    for i in [1, 2, 3, 4, 5] {
+        println!("JESUS THANK YOU for the third loop - {} {}", i, i * i);
+    }
+
+    loop {
+        println!("JESUS THANK YOU for the fourth loop");
+        break;
+    }
 }
