df2f3c0a62b753e532e5bef69c7f326891bc3270
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-11 08:21:39 +0300

    feat: rust learn, JESUS THANK YOU JESUS HALLELUJAH

--- 31C/testing/src/main.rs
+++ 31C/testing/src/main.rs
@@ -1,16 +1,19 @@
 fn main() {
-    for i in 1..5 {
-        println!("JESUS THANK YOU for the first loop - {} {}", i, i * i);
+    let arr = [[3, 7, 12], [40, 12, 7], [1, 3, 10]];
+    let mut searched_through = 0;
+    let target_value = 12;
+    'outer: for i in 0..3 {
+        for j in 0..3 {
+            println!("{} {}", arr[i][j], i);
+            searched_through += 1;
+            if arr[i][j] == target_value {
+                println!("found");
+                break 'outer;
+            }
+        }
     }
-    for i in 1..=10 {
-        println!("JESUS THANK YOU for the second loop - {} {}", i, i * i);
-    }
-    for i in [1, 2, 3, 4, 5] {
-        println!("JESUS THANK YOU for the third loop - {} {}", i, i * i);
-    }
-
-    loop {
-        println!("JESUS THANK YOU for the fourth loop");
-        break;
-    }
+    println!(
+        "JESUS hedlped us search through {} elements",
+        searched_through
+    );
 }
