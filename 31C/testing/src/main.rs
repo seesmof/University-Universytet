d77d1614b105487877e3ed542573682ae5bc864e
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-11 08:33:32 +0300

    feat: JESUS THANK YOU JESUS HALLELUJAH

--- 31C/testing/src/main.rs
+++ 31C/testing/src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-    let arr = [[3, 7, 12], [40, 12, 7], [1, 3, 10]];
-    let mut searched_through = 0;
-    let target_value = 12;
-    'outer: for i in 0..3 {
-        for j in 0..3 {
-            println!("{} {}", arr[i][j], i);
-            searched_through += 1;
-            if arr[i][j] == target_value {
-                println!("found");
-                break 'outer;
-            }
-        }
-    }
-    println!(
-        "JESUS hedlped us search through {} elements",
-        searched_through
-    );
+    let y = 13;
+    let x = {
+        let a = 10;
+        println!("{}", a);
+        a + y
+    };
+    println!("{}", x);
 }
