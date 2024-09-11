c62e71a807d213f6b0573becbf75fc85517a8442
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-11 09:47:03 +0300

    feat: done with rust, JESUS THAKN YOU JESUS HALLELUJAH

---
+++ 31C/rust_test/target/debug/deps/rust_test-e4b2c60abcd05298.d
@@ -0,0 +1,5 @@
+E:\University\31C\rust_test\target\debug\deps\librust_test-e4b2c60abcd05298.rmeta: src/main.rs
+
+E:\University\31C\rust_test\target\debug\deps\rust_test-e4b2c60abcd05298.d: src/main.rs
+
+src/main.rs:
