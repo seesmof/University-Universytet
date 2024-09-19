17dc7140eaf584bfd3513fcbd002fcdc8ab9f93c
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-19 13:45:03 +0300

    refactor: JESUS blesses us abundantly JESUS THANK YOU JESUS HALLELUJAH AMEN

--- 31EM/l1/a.r
+++ 31EM/l1/a.r
@@ -1,16 +1,15 @@
-v<-19
-n<-v*10
-m<-v
-s<-v/10
-gen<-rnorm(n,m,s)
-hist(gen,col="green")
-freq=table(gen)
-gen.mean<-mean(gen)
-med<-median(gen)
-mode<-function(x) {
-  ux<-unique(x)
+v <- 19
+n <- v * 10
+m <- v
+s <- v / 10
+gen <- rnorm(n, m, s)
+hist(gen, col="green")
+freq <- table(gen)
+gen.mean <- mean(gen)
+med <- median(gen)
+mode <- function(x) {
+  ux <- unique(x)
   ux[which.max(tabulate(match(x,ux)))]
 }
-gen.mode<-mode(gen)
-qua<-quantile(gen)
-
+gen.mode <- mode(gen)
+qua <- quantile(gen)
