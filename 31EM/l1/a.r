0acd55369cad4a2515f6ae6cfb8e6ac0bedc5a23
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-19 17:17:52 +0300

    feat: JESUS  blessed us greatly by letting us make a simpel R program

--- 31EM/l1/a.r
+++ 31EM/l1/a.r
@@ -1,15 +1,25 @@
-v <- 19
-n <- v * 10
-m <- v
-s <- v / 10
-gen <- rnorm(n, m, s)
-hist(gen, col="green")
-freq <- table(gen)
-gen.mean <- mean(gen)
-med <- median(gen)
 mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x,ux)))]
 }
-gen.mode <- mode(gen)
-qua <- quantile(gen)
+
+v <- 19
+n <- v * 10
+m <- v
+s <- v / 10
+
+dataset <- rnorm(n, m, s)
+dataset <- round(dataset)
+hist(dataset, col="green")
+
+frequency_table <- table(dataset)
+mean_value <- mean(dataset)
+median_value <- median(dataset)
+mode_value <- mode(dataset)
+qunatiles <- quantile(dataset)
+
+qunatiles
+mode_value
+median_value
+mean_value
+frequency_table
