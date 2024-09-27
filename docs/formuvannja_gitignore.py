53733b890b267d39cd756eae38614dccd28228d8
Oleh Onyshchenko <seesmwork@gmail.com>
2024-09-27 16:35:30 +0300

    feat: Sformovano gitignore, vidredagovano formuval'nyk trohy

--- docs/formuvannja_gitignore.py
+++ docs/formuvannja_gitignore.py
@@ -3,7 +3,7 @@ import os
 POTOCHNA_TEKA: str = os.path.dirname(os.path.abspath(__file__))
 STO_MEGABAJT = 100 * 1024 * 1024
 
-fajly = set(".obsidian", "__pycache__", ".venv")
+fajly = set([".obsidian", "__pycache__", ".venv"])
 potribna_teka = os.path.join(POTOCHNA_TEKA, "../")
 for root, dirs, files in os.walk(potribna_teka):
     for file in files:
@@ -12,6 +12,6 @@ for root, dirs, files in os.walk(potribna_teka):
             fajly.add(file)
             print(f"Znajshlo fajl {file}")
 
-with open(os.path.join(POTOCHNA_TEKA, ".gitignore"), "w", encoding="utf-8") as f:
+with open(os.path.join(potribna_teka, ".gitignore"), "w", encoding="utf-8") as f:
     for fajl in fajly:
         f.write(f"{fajl}\n")
