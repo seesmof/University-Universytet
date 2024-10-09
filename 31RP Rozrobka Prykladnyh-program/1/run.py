7e9b27e31393778e7fd7fa26deeb70ddb83605bc
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-09 10:56:31 +0300

    feat: ISUSE SPASY

--- 31RP Rozrobka Prykladnyh-program/1/run.py
+++ 31RP Rozrobka Prykladnyh-program/1/run.py
@@ -5,4 +5,13 @@ window.resizable(0,0)
 window.configure(background="white")
 window.title("Kinoteatr")
 window.bind("<Escape>",lambda _:window.destroy())
+
+container=Frame(window,background="white")
+container.pack(fill=BOTH,expand=1,padx=7,pady=7)
+
+movies=["JESUS"]
+for m in movies:
+    b=Button(container,text=m)
+    b.pack(side=TOP,fill=X)
+
 if __name__=="__main__":window.mainloop()
