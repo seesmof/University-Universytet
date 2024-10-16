67b68b33d10c0c76cd0da73b32c6dbfc4a0c88f9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-16 15:28:17 +0300

    feat: read json movies data into python objects

---
+++ 31RP Rozrobka Prykladnyh-program/1/run.py
@@ -0,0 +1,23 @@
+import os 
+import json 
+
+current_folder=os.path.dirname(os.path.abspath(__file__))
+movies_data_file_path=os.path.join(current_folder,"movies.json")
+
+with open(movies_data_file_path,mode="r",encoding="utf-8") as f: movies_data=json.load(f)
+
+class Movie:
+    name: str 
+    description: str 
+    year: int 
+    image: str 
+
+    def __init__(self, name:str, description:str, year:int, image: str): 
+        self.name=name
+        self.description=description
+        self.year=year
+        self.image=image
+
+for movie in movies_data:
+    movie_object=Movie(**movie)
+    print(movie_object.name, movie_object.year)
