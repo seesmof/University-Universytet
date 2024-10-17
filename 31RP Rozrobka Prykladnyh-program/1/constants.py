5872555f5abc0988b610c3258e0763f9c84efc84
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-17 08:25:51 +0300

    feat: more code documenting, working on report

--- 31RP Rozrobka Prykladnyh-program/1/constants.py
+++ 31RP Rozrobka Prykladnyh-program/1/constants.py
@@ -1,3 +1,6 @@
+# всі кольори обрано зі стандартних кольорів tkinter
 GREEN_BUTTON="lawn green"
 BLUE_BUTTON="dodger blue"
+# ці кольори незавершені
+# вони мають варіації з номером на кінці
 COLORS=["AntiqueWhite","LightSteelBlue","khaki"]
--- 31RP Rozrobka Prykladnyh-program/1/objects.py
+++ 31RP Rozrobka Prykladnyh-program/1/objects.py
@@ -1,6 +1,10 @@
+# головний об'єкт вікна програми
 window=None
+# глобально обраний фільм
 movie=None
+# глобально вибрана зала
 room=None
+# контейнери для фільмів, зал, місць
 movies_container=None
 rooms_container=None
 seats_container=None
