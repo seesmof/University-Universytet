471fb123a090b35228db5d3811e20cfeb066fb68
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-21 21:43:05 +0300

    feat: learning plt JESUS THANK YOU LORD GOD ALMIGHTY KING OF KINGS AND LORD OF LORDS

--- 31RP Rozrobka Prykladnyh-program/2/run.py
+++ 31RP Rozrobka Prykladnyh-program/2/run.py
@@ -3,23 +3,28 @@ import scipy as sp
 from matplotlib import pyplot as plt
 
 def one():
-    """ 
+    ''' 
     Обчислити визначений інтеграл
-    """
+    '''
     PI=np.pi
     bounds={
-        "lower":PI/4,
-        "upper":PI/7
+        'lower':PI/4,
+        'upper':PI/7
     }
     target_function=lambda x:(np.cos(2*x)+np.sin(x)**2)/np.sin(3*x)
-    solution=sp.integrate.quad(target_function,bounds["lower"],bounds["upper"])
+    solution=sp.integrate.quad(target_function,bounds['lower'],bounds['upper'])
     print(solution)
 def two():
-    """ 
+    ''' 
     Побудувати графік функції
     Знайти її мінімум методом Нелдера-Міда: точність 10
-    """
-    import matplotlib
-    print(matplotlib.__version__)
+    '''
+    x=np.array([1,2,6,8])
+    y=np.array([3,8,1,10])
+    plt.plot(x,y,'o-g')
+    plt.xlabel('Вертикаль')
+    plt.ylabel('Горизонталь')
+    plt.title('Графік функції')
+    plt.show()
 one()
 two()
