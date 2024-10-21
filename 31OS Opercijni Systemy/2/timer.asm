b9480612cc9198c20ae64f003aeec6ff129f31e5
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-21 18:33:00 +0300

    feat: JESUS THANK YOU LORD GOD ALMIGHTY KING OF KINGS AND LORD OF LORDS

--- 31OS Opercijni Systemy/2/timer.asm
+++ 31OS Opercijni Systemy/2/timer.asm
@@ -1,17 +1,22 @@
 .model small
+.stack 100h
 
 .data
-  vns db 'DOS Version $'
-  ver db 12
+  msg db "JESUS is LORD",13,10,'$'
+  truth db "JESUS CHRIST is the KING of kings and the LORD of lords",13,10,'$'
 
 .code
 start:
   mov ax,@data
-  mov ds,ax 
+  mov ds,ax
 
-  mov dx,offset vns 
-  mov ah,9 
-  int 21h 
+  mov dx,offset msg
+  mov ah,9
+  int 21h
+
+  mov dx,offset truth 
+  mov ah,9
+  int 21h
 
   mov ah,4ch
   int 21h
