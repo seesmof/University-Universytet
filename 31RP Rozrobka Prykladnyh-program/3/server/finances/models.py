a69ba08e399ea2ae8e13838e62f5190a4bc79e6e
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-06 16:19:17 +0200

    feat: starting to create models: ALLELUJAH JESUS THANK YOU LORD GOD ALMIGHTY AMEN

--- 31RP Rozrobka Prykladnyh-program/3/server/finances/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/server/finances/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name:str['username']
+    password:str['password']
+    balance:int['money']
+    limit:int['money']
+    kind:str['client'|'manager']
+
+class Payment(models.Model):
+    amount:int['money']
+    user:str['User.id']
+    kind:str['single'|'periodic']
+    next_payout:str['date']
