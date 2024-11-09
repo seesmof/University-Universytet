032234dcfae25531f64271b5628a99c133b9f0e4
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-09 18:37:27 +0200

    feat: JESUS THAKN YOU LORD GOD ALMIGHTY ALLELUJAH AMEN

--- 31EM Empirychni Metody/2/a.r
+++ 31EM Empirychni Metody/2/a.r
@@ -3,7 +3,11 @@ n=v*100
 m=v
 s=v/10
 
-v
-n
-m
-s
+d=rnorm(n,m,s)
+d
+
+cfile='E:\\Universytet\\31EM Empirychni Metody\\2\\a.xlsx'
+tfile='E:\\Universytet\\31EM Empirychni Metody\\2\\a.txt'
+
+library(openxlsx)
+write.table(d,cfile)
