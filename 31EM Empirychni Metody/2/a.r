b3d327fc63a383d62c6a31f2afd1334d10317999
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-11 12:52:11 +0200

    feat: JESUS THANK YOU LORD

--- 31EM Empirychni Metody/2/a.r
+++ 31EM Empirychni Metody/2/a.r
@@ -1,13 +1,21 @@
+TABLE_FILE_PATH='E:\\Universytet\\31EM Empirychni Metody\\2\\a.xlsx'
+
 v=19
 n=v*100
 m=v
 s=v/10
 
-d=rnorm(n,m,s)
-d
+dataset=rnorm(n,m,s)
+write.table(dataset,TABLE_FILE_PATH)
 
-cfile='E:\\Universytet\\31EM Empirychni Metody\\2\\a.xlsx'
-tfile='E:\\Universytet\\31EM Empirychni Metody\\2\\a.txt'
+dataset.arr=array(dataset,dim=c(v,s))
+dataset.arr
 
-library(openxlsx)
-write.table(d,cfile)
+hist(dataset.arr,main='Histogram of observed data ALLELUJAH')
+plot(density(dataset.arr),main='Density estimate of data ALLELUJAH')
+
+probabilities.arr=pnorm(dataset.arr,mean=m,sd=s)
+# this one is for Pearson
+chisq.test(probabilities.arr)
+# this one is for Kolmogorom
+ks.test(dataset.arr,probabilities.arr)
