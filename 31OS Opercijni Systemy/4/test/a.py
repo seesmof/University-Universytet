670c515ad7ee922dec020a0093c85ea0f20c6ec2
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-13 16:21:54 +0200

    AMEN

--- 31OS Opercijni Systemy/4/test/a.py
+++ 31OS Opercijni Systemy/4/test/a.py
@@ -1,6 +1,8 @@
 import win32api as wa
 import win32file as wf
 
+B_TO_GB=10**-9
+
 dts={
     wf.DRIVE_UNKNOWN:'UNKNOWN',
     wf.DRIVE_NO_ROOT_DIR:'NO_ROOT_DIR',
@@ -15,4 +17,7 @@ ds=[d for d in wa.GetLogicalDriveStrings().split('\000') if d]
 for d in ds:
     dt=wf.GetDriveType(d)
     dtn=dts[dt]
-    print(d,dtn)
+    drive_free_space,drive_total_space,_=wf.GetDiskFreeSpaceEx(d)
+    drive_free_space=round(drive_free_space*B_TO_GB)
+    drive_total_space=round(drive_total_space*B_TO_GB)
+    print(d,drive_free_space,drive_total_space)
