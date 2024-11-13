f30b88adb059f42378e8ae2ef25353153330e00d
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-13 16:10:45 +0200

    AMEN

--- 31OS Opercijni Systemy/4/test/a.py
+++ 31OS Opercijni Systemy/4/test/a.py
@@ -1,7 +1,18 @@
 import win32api as wa
 import win32file as wf
 
+dts={
+    wf.DRIVE_UNKNOWN:'UNKNOWN',
+    wf.DRIVE_NO_ROOT_DIR:'NO_ROOT_DIR',
+    wf.DRIVE_REMOVABLE:'REMOVABLE',
+    wf.DRIVE_FIXED:'FIXED',
+    wf.DRIVE_REMOTE:'REMOTE',
+    wf.DRIVE_CDROM:'CDROM',
+    wf.DRIVE_RAMDISK:'RAMDISK'
+}
+
 ds=[d for d in wa.GetLogicalDriveStrings().split('\000') if d]
 for d in ds:
     dt=wf.GetDriveType(d)
-    print(d,dt)
+    dtn=dts[dt]
+    print(d,dtn)
