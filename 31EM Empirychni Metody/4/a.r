c0dc8338eba6f21e3903484290031dd06f0c746f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 10:55:09 +0200

    AMEN

--- 31EM Empirychni Metody/4/a.r
+++ 31EM Empirychni Metody/4/a.r
@@ -1,21 +1,4 @@
-library(datarium)
-data("jobsatisfaction", package="datarium")
-str(jobsatisfaction)
-
-library(ggplot2)
-ggplot(data=jobsatisfaction,aes(x=gender,y=score))+geom_boxplot(aes(fill=education_level))
-
-require(doBy)
-summaryBy(score~gender+education_level,data=jobsatisfaction,FUN=c(mean,sd,length))
-
-plot.design(jobsatisfaction)
-
-with(jobsatisfaction,interaction.plot(x.factor=education_level,trace.factor=gender,response=score))
-
-# weightgain = score
-# type = gender
-# source = education_level
-
-Model=lm(score~gender*education_level,data=jobsatisfaction)
-summary(Model)
-anova(Model)
+x=matrix(c(1:12),nrow=3,ncol=4)
+x
+cor(x,y=NULL,use='everything')
+method=c('pearson','kendall','spearman')
