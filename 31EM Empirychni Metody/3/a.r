0014c5fc8fa2871b75984687c915e0b2285b0852
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 09:14:32 +0200

    AMEN

--- 31EM Empirychni Metody/3/a.r
+++ 31EM Empirychni Metody/3/a.r
@@ -4,3 +4,6 @@ str(jobsatisfaction)
 
 library(ggplot2)
 ggplot(data=jobsatisfaction,aes(x=gender,y=score))+geom_boxplot(aes(fill=education_level))
+
+require(doBy)
+summaryBy(score~gender+education_level,data=jobsatisfaction,FUN=c(mean,sd,length))
--- 31EM Empirychni Metody/3/t.r
+++ 31EM Empirychni Metody/3/t.r
@@ -3,4 +3,4 @@ data(weightgain)
 str(weightgain)
 
 library(ggplot2)
-ggplot(data=weightgain,aes(x=type,y=weightgain))
+ggplot(data=weightgain,aes(x=type,y=weightgain))+geom_boxplot(aes(fill=source))
