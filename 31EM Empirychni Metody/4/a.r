4b58afa51c69b1954009ca6f806fb01ca9be809d
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 11:15:55 +0200

    AMEN

--- 31EM Empirychni Metody/4/a.r
+++ 31EM Empirychni Metody/4/a.r
@@ -4,3 +4,10 @@ cor(x.vector,y.vector)
 
 z.vector=-3*x.vector+8
 cor(x.vector,z.vector)
+
+n=7
+year=sample(2000:2024,n,replace=T)
+sort(year)
+rate=sample(1:10,n,replace=T)
+sort(rate)
+plot(year,rate,main='Процентна ставка')
