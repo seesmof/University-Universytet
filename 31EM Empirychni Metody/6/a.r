cb02c4be7f43309b050ef55ad26a3a49406282e7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 16:05:24 +0200

    AMEN

--- 31EM Empirychni Metody/6/a.r
+++ 31EM Empirychni Metody/6/a.r
@@ -31,3 +31,8 @@ for (k in 1:M)
   a[k]=a[k]/N
 
 s=c('x1', 'x2', 'x3', 'x4', 'x1*x2', 'x1*x3', 'x1*x4', 'x2*x3', 'x2*x4', 'x3*x4')
+
+s1=''
+for (k in 1:M)
+  s1=paste0(s1,sprintf('% + f*%s',a[k],s[k]))
+s1
