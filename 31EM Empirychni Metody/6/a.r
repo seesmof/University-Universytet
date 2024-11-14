5db1d538783bcb8e07d555274e919dae96e4d04a
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 15:34:40 +0200

    AMEN

--- 31EM Empirychni Metody/6/a.r
+++ 31EM Empirychni Metody/6/a.r
@@ -11,6 +11,20 @@ x12=x1*x2
 x13=x1*x3
 x23=x2*x3
 
-x12 
-x13 
-x23 
+x=rbind(x1,x2,x3,x12,x13,x23)
+N=length(y)
+a0=sum(y)/N
+a=c(0,0,0,0,0,0)
+M=length(a)
+for (j in 1:N)
+  for (i in 1:M)
+    a[i]=a[i]+(y[j]*x[i,j])
+for (i in 1:M)
+  a[i]=a[i]/N
+s=c('x1','x2','x3','x1*x2','x1*x3','x2*x3')
+s1=''
+for (i in 1:M)
+  s1=paste0(s1,sprintf('%+f*%s',a[i],s[i]))
+s2=paste0('y=',a0,s1)
+s2
+
