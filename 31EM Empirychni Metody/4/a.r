00fba06da54525ce017a88e663aaebb480d0e214
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 11:01:40 +0200

    AMEN

--- 31EM Empirychni Metody/4/a.r
+++ 31EM Empirychni Metody/4/a.r
@@ -1,4 +1,9 @@
-x=matrix(c(1:12),nrow=3,ncol=4)
-x
-cor(x,y=NULL,use='everything')
+v=19
+n=v*100
+m=v
+s=v/10
+data=rnorm(n,m,s)
+d=matrix(data,nrow=n,ncol=m)
+
+cor(d,y=NULL,use='everything')
 method=c('pearson','kendall','spearman')
