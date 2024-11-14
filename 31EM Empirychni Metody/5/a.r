77161b5b6bffbeb80b36493d1886a9517c5fd9d5
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-14 12:19:36 +0200

    AMEN

--- 31EM Empirychni Metody/5/a.r
+++ 31EM Empirychni Metody/5/a.r
@@ -3,3 +3,8 @@ library(ggpubr)
 library(rstatix)
 
 PlantGrowth %>% sample_n_by(group,size=1)
+PlantGrowth=PlantGrowth %>% reorder_levels(group,order=c('ctrl','trt1','trt2'))
+PlantGrowth %>% group_by(group) %>% get_summary_stats(weight,type='common')
+ggboxplot(PlantGrowth,x='group',y='weight')
+res.kruskal=PlantGrowth %>% kruskal_test(weight~group)
+res.kruskal
