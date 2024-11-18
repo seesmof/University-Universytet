3d8075e8f5f5b941a405c263a68941db7b08f2a1
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 17:59:36 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/a.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/a.py
@@ -3,4 +3,7 @@ class Employee:
     can_delete=False
     varbose_name='employees'
 
-print([v for v in dir(Employee) if not v.startswith('__')])
+def get_class_fields(class_name):
+    return [v for v in dir(class_name) if not v.startswith('__')]
+
+print(get_class_fields(Employee))
