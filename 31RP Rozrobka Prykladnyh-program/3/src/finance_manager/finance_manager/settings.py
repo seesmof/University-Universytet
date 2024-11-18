678b66fc15fbffdae986c4a5717df4d3325adf22
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 14:10:05 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/finance_manager/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/finance_manager/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'finance_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'data',
+        'USER':'root',
+        'HOST':'localhost',
+        'PASSWORD':'1313',
+        'DATABASE':'data',
+        'PORT':'3306',
     }
 }
 
