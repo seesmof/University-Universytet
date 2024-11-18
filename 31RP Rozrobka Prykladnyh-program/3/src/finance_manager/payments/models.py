3822ca06e3a73a178c41b3c05b0e39e7c7ca4d2e
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 13:46:32 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/payments/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/payments/models.py
@@ -19,7 +19,6 @@ class Transaction(models.Model):
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
-    description = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.transaction_type}: {self.amount} on {self.date}"
+        return f"{self.transaction_type}: {self.amount} on {self.date.strftime('%d-%m-%Y')}"
