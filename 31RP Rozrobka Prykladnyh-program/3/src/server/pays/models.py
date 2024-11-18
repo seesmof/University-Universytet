5ef0dcccc110c59c3cf3b4f9feedd9f46a8ed3e2
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 21:03:34 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
@@ -7,9 +7,15 @@ class Account(models.Model):
     limit = models.PositiveBigIntegerField(default=0)
     manager = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.name}{" (admin)" if self.manager else ""}, has ₴{self.balance} with ₴{self.limit} limit'
+
 class Pay(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     purpose = models.CharField(max_length=255)
     amount = models.PositiveBigIntegerField()
     periodic = models.BooleanField(default=False)
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'₴{self.amount} by {self.user.name} for {self.purpose}'
