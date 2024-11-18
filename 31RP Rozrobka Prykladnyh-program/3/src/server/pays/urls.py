ea08e041bbae5a64e419a67973e073b411a4f3e9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 21:08:54 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/urls.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 
 urlpatterns = [
+    path('users/', views.users_view, name='users')
 ]
--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+from .models import Account
+
+def users_view(request):
+    users = Account.objects.all().values()
+    return render(request, 'users.html', {'users': users})
