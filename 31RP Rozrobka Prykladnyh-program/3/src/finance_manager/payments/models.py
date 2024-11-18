e61273741977bf0949dc8388ac58c0a85fe11143
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 13:34:28 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/payments/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/finance_manager/payments/models.py
@@ -3,12 +3,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Account(models.Model):
-    # links Account to each User
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # current account balance
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0) 
-    # maximum credit allowed 
     credit_limit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.user.username + '\'s Account'
+    
+class Transaction(models.Model):
+    TRANSACTION_TYPE = (
+        ('Deposit', 'Deposit'),
+        ('Payment', 'Payment'),
+    )
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.transaction_type}: {self.amount} on {self.date}"
