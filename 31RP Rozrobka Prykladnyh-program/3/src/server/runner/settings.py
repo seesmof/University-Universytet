adb74cd2f7f9aabf95f022712715a47366c5b6e7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 17:33:53 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pays.apps.PaysConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'runner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '1313',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'NAME': 'data',
     }
 }
 
