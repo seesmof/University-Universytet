526a0e513ae89879a5e20ea7c4ac242d0447aa90
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 20:42:28 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/urls.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.login_view, name='index'),
+    path('login/', views.login_view, name='login')
 ]
--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import logout
 
 # Create your views here.
-def index(request):
+def login_view(request):
+    if request.user.is_authenticated:
+        print("YES LORD JESUS")
+        print(request.user.username)
+        logout(request)
     return HttpResponse('JESUS IS LORD')
