0fb0b2db511af780c4f86a0ca901cc0a03ccc7a3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 21:10:45 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Account
 
 def users_view(request):
-    users = Account.objects.all().values()
+    users = Account.objects.all()
     return render(request, 'users.html', {'users': users})
