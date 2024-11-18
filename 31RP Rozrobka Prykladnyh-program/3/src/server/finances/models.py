d61df967a20f5845dd7731531d36d6fdb1412f9c
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 10:54:06 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/finances/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/finances/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    limit=models.PositiveBigIntegerField(default=0,null=True,blank=True)
+    balance=models.PositiveBigIntegerField(default=0,null=True,blank=True)
