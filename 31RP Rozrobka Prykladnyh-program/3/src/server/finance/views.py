a212fd2986811b1ca3b10c2a31938732dc66b108
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 15:21:54 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/finance/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/finance/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from .forms import NameForm
 
 def home(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
+    if request.method=='POST':
+        form=NameForm(request.POST)
         if form.is_valid():
-            return HttpResponse(form.cleaned_data, 'ALLELUJAH')
-    else:
+            user_name=form.cleaned_data['user_name']
+            return HttpResponse(user_name)
+    if request.method=='GET':
         form = NameForm()
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form':form})
