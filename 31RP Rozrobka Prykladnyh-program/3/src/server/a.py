653435d9ac80beae32207a66acff2558d06c83f4
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 13:25:51 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/a.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    mode='default'
-    can_delete=False
-    varbose_name='employees'
-
-def get_class_fields(class_name):
-    return [v for v in dir(class_name) if not v.startswith('__')]
-
-print(get_class_fields(Employee))
