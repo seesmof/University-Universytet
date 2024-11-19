c5e7e7dd9ac14bdb71d81c2724eaaffa7ef0178f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 19:54:36 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/finance/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/finance/models.py
@@ -8,3 +8,12 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Payment(models.Model):
+    client=models.ForeignKey(Client,on_delete=models.CASCADE)
+    when=models.DateTimeField()
+    purpose=models.TextField()
+    amount=models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.purpose} on {self.when} by {self.client.name} for {self.amount}'
--- 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
@@ -116,6 +116,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = '%d.%m.%Y %H:%M:%S'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
