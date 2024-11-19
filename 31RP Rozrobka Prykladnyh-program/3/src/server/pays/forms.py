c8b2d083e551c859a4ff96063040eb0b6e7f74ae
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 11:28:25 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class Login(forms.Form):
-    login = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
-
-from . import models
-from .forms import Login
-
-def index(request):
-    return render(request,'login.html',{'form':Login()})
