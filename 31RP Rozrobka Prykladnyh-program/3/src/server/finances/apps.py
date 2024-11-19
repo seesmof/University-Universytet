db9e7438e41bd40a070f892dd6eca6135f545c87
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 13:10:29 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/finances/apps.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/finances/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FinanceManagerConfig(AppConfig):
+class FinancesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finance_manager'
+    name = 'finances'
