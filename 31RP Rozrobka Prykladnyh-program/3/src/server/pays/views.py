083fe069497094af956847707d964be99d948794
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 07:55:50 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -7,5 +7,5 @@ def users_view(request):
     return render(request, 'users.html', {'users': users})
 
 def user_view(request, user_id):
-    user = Account.objects.filter(pk=user_id)
+    user = Account.objects.filter(pk=user_id).first()
     return render(request, 'user.html', {'user': user})
