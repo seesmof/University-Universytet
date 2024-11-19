1b8dad2167afd168040d5fcebd8177ad05262d78
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 12:22:07 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/accounts/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    balance=models.PositiveIntegerField(default=0)
+    limit=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.username} has {self.balance} with limit of {self.limit}'
--- 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
@@ -129,3 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+AUTH_USER_MODEL='accounts.CustomUser'
