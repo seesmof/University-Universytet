8aa2efe265570e4bec2040bc7f36932b5499dc9f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 13:55:12 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/finances/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/finances/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def start(request):
-    return HttpResponse('JESUS IS LORD')
+    return render(request, '')
--- 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'uk'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
