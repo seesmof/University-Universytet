5a2bae723151a702999d45ef055add7aeb003dc4
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 10:11:47 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
@@ -6,21 +6,14 @@ class Account(models.Model):
     limit = models.PositiveBigIntegerField(default=0)
     manager = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f'{self.name}{" (admin)" if self.manager else ""}, has ₴{self.balance} with ₴{self.limit} limit'
+class Payment(models.Model):
+    pass
 
-class Pay(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    purpose = models.CharField(max_length=255)
-    amount = models.PositiveBigIntegerField()
-    periodic = models.BooleanField(default=False)
-    class Period(models.TextChoices):
-        DAY='DAY','Day'
-        WEEK='WEEK','Week'
-        MONTH='MONTH','Month'
-        YEAR='YEAR','Year'
-    period=models.CharField(max_length=5,choices=Period.choices,default=None,blank=True)
-    date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f'₴{self.amount} by {self.user.name} for {self.purpose}'
+class PeriodicPayment(models.Model):
+    PAYMENT_PERIOD=[
+        ('DAY','Day'),
+        ('MONTH','Month'),
+        ('YEAR','Year')
+    ]
+    period = models.CharField(choices=PAYMENT_PERIOD,default='DAY')
+    next_date = models.DateField()
