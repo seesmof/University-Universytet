f1ec1d4c77a4bd5543f5b38447df80185f67d7f7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 10:36:41 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-""" 
 class User(models.Model):
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
@@ -8,6 +7,7 @@ class User(models.Model):
     limit = models.PositiveBigIntegerField(default=0)
     is_manager = models.BooleanField(default=False)
 
+""" 
 class Payment(models.Model):
     pass
 
