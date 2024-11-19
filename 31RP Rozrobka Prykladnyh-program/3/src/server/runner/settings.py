2f36ac40fb7344eaaa2cd9074b75250218691a93
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 11:31:12 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/runner/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authtest.apps.AuthtestConfig',
     'pays.apps.PaysConfig',
     'django.contrib.admin',
     'django.contrib.auth',
