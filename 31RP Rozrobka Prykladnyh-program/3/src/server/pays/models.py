e37293225e515f6138d90531b2ced4730306c3a3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 10:04:11 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Account(models.Model):
     name = models.CharField(max_length=100)
     balance = models.PositiveBigIntegerField(default=0)
@@ -15,6 +14,12 @@ class Pay(models.Model):
     purpose = models.CharField(max_length=255)
     amount = models.PositiveBigIntegerField()
     periodic = models.BooleanField(default=False)
+    class Period(models.TextChoices):
+        DAY='DAY','Day'
+        WEEK='WEEK','Week'
+        MONTH='MONTH','Month'
+        YEAR='YEAR','Year'
+    period=models.CharField(max_length=5,choices=Period.choices,default=None,blank=True)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
