8d199e7fe33cd148937709a5698e7be8c7e5a5fe
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-19 10:25:15 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
+++ 31RP Rozrobka Prykladnyh-program/3/src/server/pays/views.py
@@ -1,20 +1,3 @@
 from django.shortcuts import render
 
 from .models import *
-
-def users_view(request):
-    users = Account.objects.all()
-    return render(request, 'users.html', {'users': users})
-
-def user_view(request, user_id):
-    user = Account.objects.filter(pk=user_id).first()
-    payments = Pay.objects.filter(user=user.id)
-    users=None
-    if user.manager:
-        users = Account.objects.all().values()
-    context={
-        'user':user,
-        'payments':payments,
-        'users':users
-    }
-    return render(request, 'user.html', context)
