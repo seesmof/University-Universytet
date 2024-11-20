4c1c4bbde3e707d61cf8c8f427af98cd61b745e3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 21:00:22 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/a.py
+++ 31RP Rozrobka Prykladnyh-program/5/a.py
@@ -1,4 +1,7 @@
+from sklearn.cluster import KMeans
+import matplotlib.image as img
 from nicegui import ui, app
+import numpy as np 
 
 '''
 get an image from user 
@@ -6,5 +9,17 @@ get a number of colors from user
 get a list of N colors for the image: select the most common colors (maybe a slider between 1 and 3)
 show images with same colors 
 '''
+
+def get_colors(
+    image_path:str,
+    colors_count:int,
+):
+    image=img.imread(image_path)
+    w,h,d=tuple(image.shape)
+    pixel=np.reshape(image,(w*h,d))
+
+    model=KMeans(n_clusters=colors_count,random_state=40).fit(pixel)
+    palette=np.uint8(model.cluster_centers_)
+    return palette
 
 ui.run(favicon='🖼️',title='Image Colors')
