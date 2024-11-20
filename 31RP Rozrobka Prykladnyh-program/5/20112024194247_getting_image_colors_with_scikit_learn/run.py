62b8757b59f88d79ed593ddb91b9ad9dc997b5f7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:48:19 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
@@ -1,4 +1,9 @@
+import os
 import matplotlib.image as img 
 import matplotlib.pyplot as plt 
 from scipy.cluster.vq import whiten, kmeans
 import pandas as pd
+
+image_path=r'E:\Universytet\31RP Rozrobka Prykladnyh-program\5\landscape_red_thurm_schwyz.jpg'
+image=img.imread(image_path)
+print(image)
