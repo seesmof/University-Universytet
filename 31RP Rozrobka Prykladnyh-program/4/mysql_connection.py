6d4a4a1b668823368ca8cddf7e015b755014cd07
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 08:43:49 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
+++ 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
@@ -7,3 +7,11 @@ PORT='3306'
 from peewee import *
 
 db=MySQLDatabase(NAME,user=USER,password=PASSWORD,host=HOST,port=PORT)
+
+import mysql.connector
+
+db=mysql.connector.connect(database=NAME,user=USER,password=PASSWORD,host=HOST,port=PORT)
+c=db.cursor()
+c.execute('SELECT table FROM tables')
+r=c.fetchall()
+print(r)
