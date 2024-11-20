e4d815e8fce52a88a1857715c03f2f3d5a9334de
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:49:24 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
@@ -7,3 +7,12 @@ import pandas as pd
 image_path=r'E:\Universytet\31RP Rozrobka Prykladnyh-program\5\landscape_red_thurm_schwyz.jpg'
 image=img.imread(image_path)
 print(image)
+
+r,g,b=[],[],[]
+for row in image:
+    for tr,tg,tb,t in row:
+        r.append(tr)
+        g.append(tg)
+        b.append(tb)
+
+print(r,g,b)
