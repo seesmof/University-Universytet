0897c9fcba564c7519da07292e9f34ef5c51ae1b
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 20:10:48 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024200439_looking_at_pandas_dataframes/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024200439_looking_at_pandas_dataframes/run.py
@@ -5,7 +5,8 @@ df=pd.DataFrame(data)
 
 data={
     'name':'Matthew,Mark,Luke,John,Mary,David,Jonah'.split(','),
-    'position':'tax collector,evangelist,historian,evangelist,charity worker,king of Israel,prophet'.split(',')
+    'position':'tax collector,evangelist,historian,evangelist,charity worker,king of Israel,prophet'.split(','),
+    'mentioned_in':'Matthew and Luke,Mark and Acts,Luke and Acts,John and Revelation,John,1 and 2 Kings,Jonah'.split(','),
 }
 df=pd.DataFrame(data)
-print(df)
+print(df[['name','mentioned_in']])
