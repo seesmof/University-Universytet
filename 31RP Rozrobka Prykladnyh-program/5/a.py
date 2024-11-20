21e14808d11f51d83df7ec53b3e874c4a740cc5f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:18:43 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/a.py
+++ 31RP Rozrobka Prykladnyh-program/5/a.py
@@ -1,3 +1,10 @@
 from nicegui import ui, app
 
+'''
+get an image from user 
+get a number of colors from user 
+get a list of N colors for the image: select the most common colors (maybe a slider between 1 and 3)
+show images with same colors 
+'''
+
 ui.run(favicon='🖼️',title='Image Colors')
