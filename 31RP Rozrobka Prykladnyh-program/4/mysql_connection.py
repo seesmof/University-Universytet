fb338e00934976b7873101b08c7f9402ee4fc838
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 08:58:19 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
+++ 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
@@ -16,3 +16,19 @@ PERIODIC_PAYMENTS_TABLE='finance_periodicpayment'
 c.execute(f'SELECT * FROM {CLIENTS_TABLE}')
 r=c.fetchall()
 print(r)
+
+import random
+
+while 1:
+    HELP_MESSAGE='''Доступні команди:
+- вийти АБО вихід: закриття програми
+- поможи АБО допомога: виведення цього повідомлення
+'''.strip()
+
+    request=input('> ')
+    request.lower()
+
+    if 'вихід' in request or 'вийти' in request: break
+    elif 'поможи' in request or 'допомога' in request: print(HELP_MESSAGE)
+    elif 'привіт' in request or 'вітаю' in request: print(request)
+    elif 'як справи' in request: print(random.choice(['нормально','добре','чудово']))
