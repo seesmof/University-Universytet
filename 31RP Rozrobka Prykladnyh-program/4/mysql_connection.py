d8402fb84c972c280b9f8e9142cd2021a31cd7a4
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 08:46:05 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
+++ 31RP Rozrobka Prykladnyh-program/4/mysql_connection.py
@@ -12,6 +12,11 @@ import mysql.connector
 
 db=mysql.connector.connect(database=NAME,user=USER,password=PASSWORD,host=HOST,port=PORT)
 c=db.cursor()
-c.execute('SELECT table FROM tables')
+
+CLIENTS_TABLE='finance_client'
+PAYMENTS_TABLE='finance_payment'
+PERIODIC_PAYMENTS_TABLE='finance_periodicpayment'
+
+c.execute(f'SELECT * FROM {CLIENTS_TABLE}')
 r=c.fetchall()
 print(r)
