568d5549d560833eba46aa9384bccb7fdfdf83b9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 20:34:52 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/auto.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/auto.py
@@ -1,20 +1,16 @@
-import matplotlib
 from sklearn.cluster import KMeans
 import numpy as np 
 import matplotlib.pyplot as plt 
-import matplotlib.image as mpimg
+import matplotlib.image as img
 
 image_path=r'E:\Universytet\31RP Rozrobka Prykladnyh-program\5\landscape_red_thurm_schwyz.jpg'
-image=mpimg.imread(image_path)
+image=img.imread(image_path)
 
 w,h,d=tuple(image.shape)
-
 pixel=np.reshape(image,(w*h,d))
 
 n_colors=3
-
 model=KMeans(n_clusters=n_colors,random_state=40).fit(pixel)
-
 palette=np.uint8(model.cluster_centers_)
 print(palette)
 
