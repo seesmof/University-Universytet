361c7641da4d845eafbae7d1a9c3b0160a51df64
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:56:17 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/run.py
@@ -25,3 +25,8 @@ red_std,green_std,blue_std=df[['red','green','blue']].std()
 for center in cluster_centers:
     red_scaled,green_scaled,blue_scaled=center
     dominant_colors.append((red_scaled*red_std/255,green_scaled*green_std/255,blue_scaled*blue_std/255))
+
+dominant_colors=dominant_colors[:2]
+print(len(dominant_colors))
+plt.imshow([dominant_colors])
+plt.show()
