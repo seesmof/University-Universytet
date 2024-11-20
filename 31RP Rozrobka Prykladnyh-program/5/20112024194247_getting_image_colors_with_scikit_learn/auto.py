48f5fc82f4320ba8418ede38ea1aff044e4406a7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 20:38:02 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/auto.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024194247_getting_image_colors_with_scikit_learn/auto.py
@@ -3,16 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.image as img
 
-image_path=r'E:\Universytet\31RP Rozrobka Prykladnyh-program\5\landscape_red_thurm_schwyz.jpg'
-image=img.imread(image_path)
 
-w,h,d=tuple(image.shape)
-pixel=np.reshape(image,(w*h,d))
+def get_colors(
+    image_path:str,
+):
+    image=img.imread(image_path)
+    w,h,d=tuple(image.shape)
+    pixel=np.reshape(image,(w*h,d))
 
-n_colors=3
-model=KMeans(n_clusters=n_colors,random_state=40).fit(pixel)
-palette=np.uint8(model.cluster_centers_)
-print(palette)
+    n_colors=3
+    model=KMeans(n_clusters=n_colors,random_state=40).fit(pixel)
+    palette=np.uint8(model.cluster_centers_)
+    return palette
 
-plt.imshow([palette])
-plt.show()
+folder_path=r'E:\Universytet\31RP Rozrobka Prykladnyh-program\5'
+images='landscape_red_thurm_schwyz.jpg,landscape_moor_nature_reserve.jpg,nature_reserve_moor_forest_0.jpg'.split(',')
+for image in images:
+    palette=get_colors(rf'{folder_path}\{image}')
+    plt.imshow([palette])
+    plt.show()
