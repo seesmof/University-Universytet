5efbd3553d0306157eef5a18cec7aa0f9ed098ec
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:33:10 +0200

    AMEN

---
+++ 31RP Rozrobka Prykladnyh-program/5/getting_image_colors/run.py
@@ -0,0 +1,10 @@
+import os
+from PIL import Image
+
+current_dir=os.path.dirname(os.path.abspath(__file__))
+root=os.path.join(current_dir,'..')
+image_name='landscape_red_thurm_schwyz.jpg'
+image_path=os.path.join(root,image_name)
+img = Image.open(image_path)
+max_colors = 3
+print(img.getcolors(max_colors))
