130051f9c2b835f42bb692b5ec17315e20a6063f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 20:09:23 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/20112024200439_looking_at_pandas_dataframes/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/20112024200439_looking_at_pandas_dataframes/run.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 data='ALLEUJAH AMEN GREAT GOD our LORD AND SAVIOR JESUS CHRIST REIGNS FOREVER AMEN'.split()
 df=pd.DataFrame(data)
+
+data={
+    'name':'Matthew,Mark,Luke,John,Mary,David,Jonah'.split(','),
+    'position':'tax collector,evangelist,historian,evangelist,charity worker,king of Israel,prophet'.split(',')
+}
+df=pd.DataFrame(data)
 print(df)
