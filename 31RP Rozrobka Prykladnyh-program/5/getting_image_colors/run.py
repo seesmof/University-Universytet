5e2273763069281da518a6af5f1191c0580394a5
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-20 19:35:27 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/getting_image_colors/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/getting_image_colors/run.py
@@ -6,5 +6,5 @@ root=os.path.join(current_dir,'..')
 image_name='landscape_red_thurm_schwyz.jpg'
 image_path=os.path.join(root,image_name)
 img = Image.open(image_path)
-max_colors = 3
-print(img.getcolors(max_colors))
+colors=img.convert('RGB').getcolors()
+print(colors)
