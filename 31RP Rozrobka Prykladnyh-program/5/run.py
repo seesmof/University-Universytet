3857e11610b510f25c174d7094bb2d1bd5c0ccf9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-21 08:53:14 +0200

    AMEN

--- 31RP Rozrobka Prykladnyh-program/5/run.py
+++ 31RP Rozrobka Prykladnyh-program/5/run.py
@@ -56,11 +56,16 @@ image_colors=get_colors_dataset(
 )
 average_image_colors=get_average_colors(image_colors)
 
+def update_image_output():
+    results_image.source=os.path.join(images_folder,image_file)
+    results_image.update()
+
 ui.label('Image to process').classes(LABEL_CLASSES)
 ui.select(
     options=image_files,
     with_input=True,
     value=image_file,
+    on_change=update_image_output,
 ).classes('w-full').bind_value(globals(),'image_file')
 
 ui.label('Number of colors').classes('mt-7 '+LABEL_CLASSES)
@@ -70,35 +75,29 @@ ui.slider(
     value=colors_count
 ).bind_value(globals(),'colors_count')
 
-with ui.dialog() as result, ui.card():
-    image_path=os.path.join(images_folder,image_file)
-    image_average_color=average_image_colors[image_file]
-    closest_colors=sorted(
-        ((image_name,color_value) for image_name,color_value in average_image_colors.items() if color_value<=image_average_color and image_name!=image_file),
-        key=lambda color: color[1],
-        reverse=True
-    )
+image_average_color=average_image_colors[image_file]
+closest_colors=sorted(
+    ((image_name,color_value) for image_name,color_value in average_image_colors.items() if color_value<=image_average_color and image_name!=image_file),
+    key=lambda color: color[1],
+    reverse=True
+)
 
-    ui.label(f'Results for {image_file}').classes(LABEL_CLASSES)
-    ui.image(image_path)
+ui.label(f'Results for {image_file}').classes(LABEL_CLASSES)
+results_image=ui.image(os.path.join(images_folder,image_file))
 
-    ui.label(f'Most common colors ({colors_count})').classes('mt-7 '+LABEL_CLASSES)
-    with ui.row().classes('w-full flex gap-3'):
-        ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #37bf37;')
-        ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #7359eb;')
-        ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #779bef;')
+ui.label(f'Most common colors ({colors_count})').classes('mt-7 '+LABEL_CLASSES)
+with ui.row().classes('w-full flex gap-3'):
+    ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #37bf37;')
+    ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #7359eb;') if colors_count>=2 else None
+    ui.element('span').classes('flex-1 py-5 rounded-md').style('background: #779bef;') if colors_count>=3 else None
 
-    closest_color_image_paths=[
-        os.path.join(images_folder,this_file_name) for this_file_name,this_color_value in closest_colors[:3]
-    ]
-    ui.label(f'Images with similar colors').classes('mt-7 '+LABEL_CLASSES)
-    with ui.row().classes('w-full'):
-        ui.image(closest_color_image_paths[0])
-        ui.image(closest_color_image_paths[1])
-        ui.image(closest_color_image_paths[2])
-
-    ui.button('AMEN',on_click=result.close).classes('w-full')
-
-ui.button('AMEN',on_click=result.open).classes('w-full')
+closest_color_image_paths=[
+    os.path.join(images_folder,this_file_name) for this_file_name,this_color_value in closest_colors[:3]
+]
+ui.label(f'Images with similar colors').classes('mt-7 '+LABEL_CLASSES)
+with ui.row().classes('w-full flex gap-3'):
+    ui.image(closest_color_image_paths[0]).classes('flex-1 rounded-md h-full display-cover')
+    ui.image(closest_color_image_paths[1]).classes('flex-1 rounded-md h-full display-cover')
+    ui.image(closest_color_image_paths[2]).classes('flex-1 rounded-md h-full display-cover')
 
 ui.run(favicon='🖼️',title='Image Colors')
