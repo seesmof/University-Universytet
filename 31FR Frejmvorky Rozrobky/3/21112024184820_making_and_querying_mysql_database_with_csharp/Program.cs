0fcc6a369ed2d81aa2e72c6edff995c4db03f9ba
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-22 12:45:39 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/3/21112024184820_making_and_querying_mysql_database_with_csharp/Program.cs
+++ 31FR Frejmvorky Rozrobky/3/21112024184820_making_and_querying_mysql_database_with_csharp/Program.cs
@@ -8,31 +8,61 @@ using MySql.Data.MySqlClient;
 
 public class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    var USER = "root";
+    var PASSWORD = "1313";
+    var HOST = "localhost";
+    var PORT = "3306";
+    var NAME = "data";
+    var connectionString = $"server={HOST};port={PORT};UID={USER};password={PASSWORD};database={NAME};";
+    var data = new DataTable();
+
+    var query = "SELECT * FROM data.finance_client;";
+    using (var connection = new MySqlConnection(connectionString))
+    using (var command = new MySqlCommand(query, connection))
     {
-        var USER = "root";
-        var PASSWORD = "1313";
-        var HOST = "localhost";
-        var PORT = "3306";
-        var NAME = "data";
-        var connectionString = $"server={HOST};port={PORT};UID={USER};password={PASSWORD};database={NAME};";
-        var data=new DataTable();
-
-        var query = "SELECT * FROM data.finance_client;";
-        using (var c=new MySqlConnection(connectionString))
-        using (var a=new MySqlDataAdapter(query, c))
-        {
-            a.Fill(data);
-        }
-
-        foreach (DataRow row in data.Rows)
-        {
-            foreach (var item in row.ItemArray)
-            {
-                Console.WriteLine(item.ToString());
-            }
-            Console.WriteLine();
-        }
-        Console.ReadKey();
+      connection.Open();
+      using (var reader = command.ExecuteReader())
+      {
+        data.Load(reader);
+      }
     }
+
+    using (var c = new MySqlConnection(connectionString))
+    using (var a = new MySqlDataAdapter(query, c))
+    {
+      a.Fill(data);
+    }
+
+    foreach (DataRow row in data.Rows)
+    {
+      foreach (var item in row.ItemArray)
+      {
+        Console.WriteLine(item.ToString());
+      }
+    }
+    Console.ReadKey();
+
+    var TABLE_PREFIX = "fr_";
+  }
+}
+
+var query = "SELECT * FROM finance_client;";
+using (var c = new MySqlConnection(connectionString))
+using (var a = new MySqlDataAdapter(query, c))
+{
+  a.Fill(data);
+}
+
+foreach (DataRow row in data.Rows)
+{
+  foreach (var item in row.ItemArray)
+  {
+    Console.WriteLine(item.ToString());
+  }
+  Console.WriteLine();
+}
+Console.ReadKey();
+  }
 }
