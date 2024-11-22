f4a883b46bb2849936afccdafb0bcc7a38546983
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-22 15:31:53 +0200

    AMEN

---
+++ 31FR Frejmvorky Rozrobky/src/Database.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace src
+{
+    public class Database
+    {
+        // IDEA: connect to database, provided all the data 
+        public MySqlConnection connection { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string host { get; set; }
+        public string port { get; set; }
+        public string databaseName { get; set; }
+        public string connectionString { get; set; }
+
+        // make a constructor that accepts database connection parameters
+        public Database(string userName, string password, string host, string port, string databaseName)
+        {
+            this.user = userName;
+            this.password = password;
+            this.host = host;
+            this.port = port;
+            this.databaseName = databaseName;
+            this.connectionString = formConnectionString();
+            this.connection=new MySqlConnection(this.connectionString);
+        }
+
+        public string formConnectionString()
+        {
+            return $"uid={user};pwd={password};server={host};port={port};database={databaseName}";
+        }
+        // store database connection object
+    }
+}
--- 31FR Frejmvorky Rozrobky/src/MainWindow.xaml.cs
+++ 31FR Frejmvorky Rozrobky/src/MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace src
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Database databaseConnection;
+        public string userName;
+        public string password;
+        public string host;
+        public string port;
+        public string databaseName;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            userName = "root";
+            password = "1313";
+            host = "localhost";
+            port = "3306";
+            databaseName = "fr_data";
+
+            User_Name.Text = userName;
+            Password.Text = password;
+            Host.Text = host;
+            Port.Text = port;
+            Database_Name.Text = databaseName;
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            databaseConnection=new Database(userName,password,host,port,databaseName);
+            Result.Text = databaseConnection.connection.ToString();
         }
     }
 }
