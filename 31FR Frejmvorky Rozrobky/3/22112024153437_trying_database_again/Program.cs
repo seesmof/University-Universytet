15e9e0537679b33d60845b13aa228a671488c3a7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-22 16:01:10 +0200

    AMEN

---
+++ 31FR Frejmvorky Rozrobky/3/22112024153437_trying_database_again/Program.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22112024153437_trying_database_again
+{
+    /*
+    public class Listing
+    {
+        public Listing() { }
+        public void createListing() { }
+        public Listing readListing() { }
+        public void updateListing() { }
+        public void deleteListing() { }
+    }
+
+    public class Meeting
+    {
+        public Meeting() { }
+        public void createMeeting() { }
+        public Meeting readMeeting() { }
+        public void updateMeeting() { }
+        public void deleteMeeting() { }
+    }
+
+    public class User
+    {
+        public User() { }
+        public void createUser() { }
+        public User readUser() { }
+        public void updateUser() { }
+        public void deleteUser() { }
+    }
+    */
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string USER_NAME = "root";
+            const string PASSWORD = "1313";
+            const string HOST = "localhost";
+            const string PORT = "3306";
+            const string DATABASE_NAME = "data";
+            string connectionString = $"uid={USER_NAME};pwd={PASSWORD};host={HOST};port={PORT};database={DATABASE_NAME}";
+            Console.WriteLine(connectionString);
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            string query = "select id,name from finance_client";
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    Console.WriteLine($"ID: {}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
