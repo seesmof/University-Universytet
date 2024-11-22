50ba035981cff7f17df3365d72d137be0124d4a3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-22 12:31:11 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/3/21112024184820_making_and_querying_mysql_database_with_csharp/Program.cs
+++ 31FR Frejmvorky Rozrobky/3/21112024184820_making_and_querying_mysql_database_with_csharp/Program.cs
@@ -1,25 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
-namespace _21112024184820_making_and_querying_mysql_database_with_csharp
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var USER = "root";
+        var PASSWORD = "1313";
+        var HOST = "localhost";
+        var PORT = "3306";
+        var NAME = "data";
+        var connectionString = $"server={HOST};port={PORT};UID={USER};password={PASSWORD};database={NAME};";
+        var data=new DataTable();
+
+        var query = "SELECT * FROM data.finance_client;";
+        using (var c=new MySqlConnection(connectionString))
+        using (var a=new MySqlDataAdapter(query, c))
         {
-            var USER = "root";
-            var PASSWORD = "1313";
-            var HOST = "localhost";
-            var PORT = "3306";
-            var NAME = "data";
-            var conn = new MySqlConnection($"server={HOST};port={PORT};UID={USER};password={PASSWORD};database={NAME};");
-            conn.Open();
-            Console.WriteLine("ALLEUJAH JESUS THANK YOU LORD GOD ALMIGHTY AMEN");
-            conn.Close();
+            a.Fill(data);
         }
+
+        foreach (DataRow row in data.Rows)
+        {
+            foreach (var item in row.ItemArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
     }
 }
