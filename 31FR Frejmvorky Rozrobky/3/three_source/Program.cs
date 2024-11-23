e08f64f6bbdbdd6f2d73cb51c617546088246010
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-23 18:08:04 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/3/three_source/Program.cs
+++ 31FR Frejmvorky Rozrobky/3/three_source/Program.cs
@@ -72,6 +72,23 @@ namespace three_source
             reader.Close();
             return null;
         }
+        public List<User> readAll()
+        {
+            query = "SELECT id,name,manager FROM user;";
+            command = new MySqlCommand(query, connection);
+            reader = command.ExecuteReader();
+            var users = new List<User>();
+            while (reader.Read())
+            {
+                var user = new User();
+                user.id = reader.GetInt32(0);
+                user.name = reader.GetString(1);
+                user.manager = reader.GetInt32(2);
+                users.Add(user);
+            }
+            reader.Close();
+            return users;
+        }
         public void update(User user)
         {
             query = $"UPDATE user SET name='{user.name}',manager='{user.manager}' WHERE id={user.id};";
@@ -117,6 +134,14 @@ namespace three_source
             {
                 Console.WriteLine($"Deleted user not found");
             }
+
+            // Show all
+            var users = handler.readAll();
+            Console.WriteLine($"All users ({users.Count}):");
+            foreach (var u in users)
+            {
+                Console.WriteLine($"User {u.id} name {u.name} manager {Convert.ToBoolean(u.manager)}");
+            }
         }
     }
     public class Program
@@ -125,31 +150,9 @@ namespace three_source
         {
             const string conStr = "uid=root;pwd=1313;host=localhost;port=3306;database=fr_data";
             var connection = new MySqlConnection(conStr);
-            string query;
-            MySqlCommand command;
-            MySqlDataReader reader;
             connection.Open();
 
             Test.testUser(connection);
-
-            // read all users 
-            query = $"SELECT id,name,manager FROM user;";
-            command = new MySqlCommand(query, connection);
-            reader = command.ExecuteReader();
-            var users = new List<User>();
-            while (reader.Read())
-            {
-                var u = new User();
-                u.id = int.Parse(reader[0].ToString());
-                u.name = reader[1].ToString();
-                u.manager = int.Parse(reader[2].ToString());
-                users.Add(u);
-            }
-            reader.Close();
-            foreach (var u in users)
-            {
-                Console.WriteLine($"User {u.id} name {u.name} manager {u.manager}");
-            }
 
             connection.Close();
         }
