4f62c7097f548dbb59d98da6fc3360e375e4b559
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-23 10:22:31 +0200

    AMEN

---
+++ 31FR Frejmvorky Rozrobky/3/three_source/Program.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three_source
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conStr = "uid=root;pwd=1313;host=localhost;port=3306;database=fr_data";
+            using MySqlConnection conn = new MySqlConnection(conStr);
+            Console.WriteLine("Connected and disconnected praise JESUS CHRIST my LODR GOD MOST HIGH!");
+            Console.ReadKey();
+        }
+    }
+}
