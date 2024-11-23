5116b67bb9671507e784ae1a30473b4e2953dbb7
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-23 12:10:01 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/3/three_source/Program.cs
+++ 31FR Frejmvorky Rozrobky/3/three_source/Program.cs
@@ -13,6 +13,14 @@ namespace three_source
         public string name { get; set; }
         public bool manager { get; set; } = false;
     }
+    public class Listing
+    {
+        public int id { get; set; }
+        public string name { get; set; }    
+        public int price { get; set; }
+        public string kind { get; set; }
+        public User owner { get; set; }
+    }
     public class Program
     {
         static void Main(string[] args)
@@ -27,7 +35,6 @@ namespace three_source
                 var query = "select id,name,manager from user";
                 var command = new MySqlCommand(query, connection);
                 var reader = command.ExecuteReader();
-
                 while (reader.Read())
                 {
                     var user = new User();
@@ -36,6 +43,22 @@ namespace three_source
                     user.manager = Convert.ToBoolean(Convert.ToInt16(reader[2].ToString()));
                     Console.WriteLine($"User {user.id} name {user.name} manager {user.manager}");
                 }
+                reader.Close();
+
+                query = "select id,name,price,kind,owner from listing";
+                command = new MySqlCommand(query, connection);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var listing = new Listing();
+                    listing.id = int.Parse(reader[0].ToString());
+                    listing.name = reader[1].ToString();
+                    listing.price = int.Parse(reader[2].ToString());
+                    listing.kind = reader[3].ToString();
+                    listing.owner = null;
+                    Console.WriteLine($"Listing {listing.name} price {listing.price} kind {listing.kind} owner {reader[4].ToString()}");
+                }
+                reader.Close();
             } catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
