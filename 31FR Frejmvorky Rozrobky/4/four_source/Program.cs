ac06197c0e89a312611ac2966da4e8d885701853
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-25 17:04:45 +0200

    AMEN

---
+++ 31FR Frejmvorky Rozrobky/4/four_source/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace four_source
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            login 
+            if no user then register 
+                keep track of current session with User class 
+            implement all the functions
+            */
+        }
+    }
+}
