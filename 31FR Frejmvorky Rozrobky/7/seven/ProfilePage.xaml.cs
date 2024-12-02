385f4797a2fc1680779f6f341bb48afc2ecbe4c8
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-02 16:34:41 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/7/seven/ProfilePage.xaml.cs
+++ 31FR Frejmvorky Rozrobky/7/seven/ProfilePage.xaml.cs
@@ -26,23 +26,18 @@ namespace seven
     {
         public User client;
         public MySqlConnection connection = new MySqlConnection(UtilityVariables.connectionString);
-        public UtilityFunctions helper = new UtilityFunctions();
         public Database database;
         public ProfilePage(string userName) {
             InitializeComponent();
             connection.Open();
             database = new Database(connection);
 
-            // fetch user here
             User found=database.getUsers().Where(u => u.Name == userName).ToList().ElementAtOrDefault(0);
             if (found != null) {
                 client = found;
             } else {
                 client = database.createUser(userName);
             }
-
-            showIncomingMeetings();
-            showOutgoingMeetings();
         }
         public void homeTabOpened(object sender, RoutedEventArgs e) {
             showUserData();
@@ -52,6 +47,12 @@ namespace seven
         }
         public void availableEstatesTabOpened(object sender, RoutedEventArgs e) {
             showAvailableEstates();
+        }
+        public void incomingMeetingsTabOpened(object sender, RoutedEventArgs e){
+            showIncomingMeetings();
+        }
+        public void outgoingMeetingsTabOpened(object sender, RoutedEventArgs e){
+            showOutgoingMeetings();
         }
         public void showUserData() {
             UserNameBox.Text=client.Name;
@@ -64,23 +65,9 @@ namespace seven
             foreach (var e in data) {
                 OwnedEstatesContainer.Items.Add($"{e.ID}. {e.Title} of kind {e.Kind} price {e.Price} owned by {e.Owner.Name}");
             }
-            OeTitleInput.Text = "";
-            OeKindInput.Text = EstateKind.Home;
-            OePriceInput.Text = "0";
-        }
-        public void showIncomingMeetings() {
-            var data = database.getMeetings().Where(m => m.Target.Owner.ID == client.ID).ToList();
-            IncomingMeetingsContainer.Items.Clear();
-            foreach (var m in data) {
-                IncomingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
-            }
-        }
-        public void showOutgoingMeetings() {
-            var data = database.getMeetings().Where(m=>m.Sender.ID==client.ID).ToList();
-            OutgoingMeetingsContainer.Items.Clear();
-            foreach (var m in data) {
-                OutgoingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
-            }
+            EditTitleInput.Text = "";
+            EditKindInput.Text = EstateKind.Home;
+            EditPriceInput.Text = "0";
         }
         public void showAvailableEstates() {
             var data = database.getEstates().Where(e=>e.Owner.ID!=client.ID).ToList();
@@ -89,49 +76,76 @@ namespace seven
                 AvailableEstatesContainer.Items.Add($"{e.ID}. {e.Title} of kind {e.Kind} price {e.Price} owned by {e.Owner.Name}");
             }
         }
+        public void showIncomingMeetings() {
+            var data = database.getMeetings().Where(m => m.Target.Owner.ID == client.ID).OrderBy(m=>m.ID).Reverse().ToList();
+            IncomingMeetingsContainer.Items.Clear();
+            foreach (var m in data) {
+                IncomingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
+            }
+        }
+        public void showOutgoingMeetings() {
+            var data = database.getMeetings().Where(m=>m.Sender.ID==client.ID).OrderBy(m => m.ID).Reverse().ToList();
+            OutgoingMeetingsContainer.Items.Clear();
+            foreach (var m in data) {
+                OutgoingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
+            }
+        }
         private void ChangeNameButton_Click(object sender, RoutedEventArgs e) {
             var name = UserNameBox.Text;
             client.Name = name;
             database.updateUser(client);
+
             client = database.getUser(client.ID);
-            UserNameBox.Text = client.Name;
+            showUserData();
         }
         private void ChangeBalanceButton_Click(object sender, RoutedEventArgs e) {
             int balance=client.Balance;
             try {
                 balance = Convert.ToInt32(UserBalanceBox.Text);
             } catch { MessageBox.Show("Wrong balance. Please enter a number"); }
+
             client.Balance = balance;
             database.updateUser(client);
+
             client = database.getUser(client.ID);
-            UserBalanceBox.Text = client.Balance.ToString();
+            showUserData();
         }
         private void UserStatusToggle_Click(object sender, RoutedEventArgs e) {
             var status = UserStatusToggle.IsChecked;
             client.Admin = Convert.ToInt32(status);
             database.updateUser(client);
+
             client = database.getUser(client.ID);
-            UserStatusToggle.IsChecked = Convert.ToBoolean(client.Admin);
+            showUserData();
         }
         private void BuyEstateButton_Click(object sender, RoutedEventArgs e) {
             if (AvailableEstatesContainer.SelectedIndex<0) { return; }
             string id = AvailableEstatesContainer.SelectedValue.ToString().Split('.')[0];
             var estate = database.getEstate(int.Parse(id));
+
             if (client.Balance < estate.Price) {
                 MessageBox.Show("Not enough money");
                 return;
             }
             estate.Owner = client;
             client.Balance -= estate.Price;
+
             database.updateEstate(estate);
             showAvailableEstates();
         }
-        private void AeSellButton_Click(object sender, RoutedEventArgs e) {
-            var title = AeTitleInput.Text;
-            var priceInput = AePriceInput.Text;
-            int price;
-            var kind = AeKindInput.Text;
+        private void SetMeetingButton_Click(object sender, RoutedEventArgs e) {
+            if (AvailableEstatesContainer.SelectedIndex < 0) { return; }
+            string id = AvailableEstatesContainer.SelectedValue.ToString().Split('.')[0];
+            var estate = database.getEstate(int.Parse(id));
 
+            database.createMeeting(client, estate);
+            showOutgoingMeetings();
+        }
+        private void AeSellButton_Click(object sender, RoutedEventArgs e) {
+            var title = SellTitleInput.Text;
+            var priceInput = SellPriceInput.Text;
+            int price;
+            var kind = SellKindInput.Text;
             try {
                 price = int.Parse(priceInput);
             } catch {
@@ -142,7 +156,7 @@ namespace seven
                 MessageBox.Show($"Estate of kind {EstateKind.New} may be added only by managers");
                 return;
             }
-            MessageBox.Show($"title {title} price {price} kind {kind}");
+
             database.createEstate(title, kind, client, price);
             showAvailableEstates();
         }
@@ -151,14 +165,18 @@ namespace seven
             string id = OwnedEstatesContainer.SelectedValue.ToString().Split('.')[0];
             var estate = database.getEstate(int.Parse(id));
 
-            OeTitleInput.Text = estate.Title;
-            OeKindInput.Text = estate.Kind;
-            OePriceInput.Text = estate.Price.ToString();
+            EditTitleInput.Text = estate.Title;
+            EditKindInput.Text = estate.Kind;
+            EditPriceInput.Text = estate.Price.ToString();
         }
         private void OeEditButton_Click(object sender, RoutedEventArgs e) {
-            var title = OeTitleInput.Text;
-            var kind = OeKindInput.Text;
-            var priceInput = OePriceInput.Text;
+            if (OwnedEstatesContainer.SelectedIndex < 0) { return; }
+            string id = OwnedEstatesContainer.SelectedValue.ToString().Split('.')[0];
+            var estate = database.getEstate(int.Parse(id));
+
+            var title = EditTitleInput.Text;
+            var kind = EditKindInput.Text;
+            var priceInput = EditPriceInput.Text;
             int price;
             try {
                 price = int.Parse(priceInput);
@@ -166,15 +184,48 @@ namespace seven
                 MessageBox.Show("Incorrect price, please enter a number");
                 return;
             }
-            if (OwnedEstatesContainer.SelectedIndex < 0) { return; }
-            string id = OwnedEstatesContainer.SelectedValue.ToString().Split('.')[0];
-            var estate = database.getEstate(int.Parse(id));
-
             estate.Title = title;
             estate.Price = price;
             estate.Kind = kind;
+            
             database.updateEstate(estate);
             showOwnedEstates();
+        }
+        private void ProcessButton_Click(object sender, RoutedEventArgs e) {
+            if (IncomingMeetingsContainer.SelectedIndex < 0) { return; }
+            string id = IncomingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
+            var meeting = database.getMeeting(int.Parse(id));
+
+            var status = ProcessInput.Text;
+            meeting.Status = status;
+
+            database.updateMeeting(meeting);
+            showIncomingMeetings();
+        }
+        private void RateButton_Click(object sender, RoutedEventArgs e) {
+            if (OutgoingMeetingsContainer.SelectedIndex < 0) { return; }
+            string id = OutgoingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
+            var meeting = database.getMeeting(int.Parse(id));
+
+            var score = RateInput.Text;
+            meeting.Score = score;
+
+            database.updateMeeting(meeting);
+            showOutgoingMeetings();
+        }
+        private void IncomingMeetingsContainer_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (IncomingMeetingsContainer.SelectedIndex < 0) { return; }
+            string id = IncomingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
+            var meeting = database.getMeeting(int.Parse(id));
+
+            ProcessInput.Text = meeting.Status;
+        }
+        private void OutgoingMeetingsContainer_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (OutgoingMeetingsContainer.SelectedIndex < 0) { return; }
+            string id = OutgoingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
+            var meeting = database.getMeeting(int.Parse(id));
+
+            RateInput.Text = meeting.Score;
         }
     }
 }
