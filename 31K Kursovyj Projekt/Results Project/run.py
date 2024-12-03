4b29cac3b66bf51766a9efef96e67d276da104b5
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-03 16:22:57 +0200

    AMEN

--- 31K Kursovyj Projekt/Results Project/run.py
+++ 31K Kursovyj Projekt/Results Project/run.py
@@ -21,7 +21,7 @@ def get_formatted_size(bytes,suffix='B'):
     '''
     factor=1024
     for unit in ['','K','M','G','T','P']:
-        if bytes<factor: return f'{bytes:.2f}{unit}{suffix}'
+        if bytes<factor: return f'{bytes:.2f} {unit}{suffix}'
         bytes/=factor
 
 print()
@@ -85,7 +85,60 @@ print()
 if_addrs=psutil.net_if_addrs()
 for interface_name,interface_addresses in if_addrs.items():
     for address in interface_addresses:
-        print(interface_name)
-        print(type(address.family))
+        print(interface_name,address.family.name)
+        if address.family.name=='AF_INET':
+            print('ip address',address.address)
+            print('netmask',address.netmask)
+            print('broadcast ip',address.broadcast)
+        elif address.family.name=='AF_PACKET':
+            print('mac address',address.address)
+            print('netmask',address.netmask)
+            print('broadcast mac',address.broadcast)
+net_io=psutil.net_io_counters()
+print('total bytes sent',get_formatted_size(net_io.bytes_sent))
+print('total bytes received',get_formatted_size(net_io.bytes_recv))
 
+def update_ui():
+    system_data['type']=uname.system
+    system_data['user']=uname.node
+    system_table.rows=[{'property':k.capitalize(),'value':v} for k,v in system_data.items()]
+
+HEADING_CLASSES='font-bold text-xl'
+with ui.row().classes('flex gap-3'):
+    with ui.column():
+        system_data={
+            'type':uname.system,
+            'user':uname.node,
+            'release':uname.release,
+            'version':uname.version,
+            'machine':uname.machine,
+        }
+        system_table_rows=[{'property':k.capitalize(),'value':v} for k,v in system_data.items()]
+        system_table_columns=[
+            {'name':'property','label':'Property','field':'property'},
+            {'name':'value','label':'Value','field':'value'},
+        ]
+        system_table=ui.table(columns=system_table_columns,rows=system_table_rows,row_key='name',title='System')
+        boot_time_label=ui.label(f'Booted: {boot_time.day}.{boot_time.month}.{boot_time.year} {boot_time.hour:02d}:{boot_time.minute:02d}:{boot_time.second:02d}')
+    with ui.column():
+        processor_data={
+            'name':uname.processor,
+        }
+        processor_table_rows=[{'property':k.capitalize(),'value':v} for k,v in processor_data.items()]
+        processor_table_columns=[
+            {'name':'property','label':'Property','field':'property'},
+            {'name':'value','label':'Value','field':'value'},
+        ]
+        processor_table=ui.table(columns=processor_table_columns,rows=processor_table_rows,row_key='name',title='Processor')
+    with ui.column():
+        ui.label('Memory').classes(HEADING_CLASSES)
+        ui.label('Name')
+    with ui.column():
+        ui.label('Disks').classes(HEADING_CLASSES)
+        ui.label('Name')
+    with ui.column():
+        ui.label('Network').classes(HEADING_CLASSES)
+        ui.label('Name')
+
+ui.timer(1,update_ui,active=True)
 ui.run()
