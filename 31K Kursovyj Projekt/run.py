d4a76fa7c8f91a21bb94ef62e42b423f9d82a5ec
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-03 10:05:20 +0200

    AMEN

--- 31K Kursovyj Projekt/run.py
+++ 31K Kursovyj Projekt/run.py
@@ -37,4 +37,16 @@ boot_time_stamp=psutil.boot_time()
 boot_time=datetime.fromtimestamp(boot_time_stamp)
 print('boot time',f'{boot_time.day}.{boot_time.month}.{boot_time.year} {boot_time.hour:02d}:{boot_time.minute:02d}:{boot_time.second:02d}')
 
+print()
+print('physical cores',psutil.cpu_count(logical=False))
+print('total cores',psutil.cpu_count(logical=True))
+cpu_frequency=psutil.cpu_freq()
+print('max frequency',f'{cpu_frequency.max:.2f}MHz')
+print('min frequency',f'{cpu_frequency.min:.2f}MHz')
+print('current frequency',f'{cpu_frequency.current:.2f}MHz')
+print('cpu usage per core')
+for i,percentage in enumerate(psutil.cpu_percent(percpu=True,interval=1)):
+    print('core',i,f'{percentage}%')
+print('total cpu usage',f'{psutil.cpu_percent()}%')
+
 ui.run()
