67e41ef62830be8ddf61d3ec307207026f8d4314
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-06 08:32:08 +0200

    AMEN

--- 31FR Frejmvorky Rozrobky/7/seven/ViewModels/ProfileViewModel.cs
+++ 31FR Frejmvorky Rozrobky/7/seven/ViewModels/ProfileViewModel.cs
@@ -1,5 +1,6 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -7,25 +8,50 @@ using System.Windows.Input;
 namespace seven.ViewModels{
     public class ProfileViewModel : ViewModelBase{
         private string _userName;
-        private User _client;
-        private bool _clientStatus;
-
         public string userName{
             get => _userName;
             set => SetProperty(ref _userName, value);
         }
+
+        private User _client;
         public User client{
             get => _client;
             set => SetProperty(ref _client, value);
         }
 
-        public MySqlConnection connection = new MySqlConnection(UtilityVariables.connectionString);
-        public Database database;
-
-        public ProfileViewModel(){
-            connection.Open();
-            database = new Database(connection);
+        private ObservableCollection<Estate> _ownedEstates;
+        public ObservableCollection<Estate> ownedEstates{
+            get => _ownedEstates;
+            set => SetProperty(ref _ownedEstates, value);
         }
+
+        private ObservableCollection<Estate> _availableEstates;
+        public ObservableCollection<Estate> availableEstates{
+            get => _availableEstates;
+            set => SetProperty(ref _availableEstates, value);
+        }
+
+        private Estate _selectedOwnedEstate;
+        public Estate selectedOwnedEstate{
+            get => _selectedOwnedEstate;
+            set => SetProperty(ref _selectedOwnedEstate, value);
+        }
+
+        private Estate _newEstate;
+        public Estate newEstate{
+            get => _newEstate;
+            set => SetProperty(ref _newEstate, value);
+        }
+
+        private Estate _selectedAvailableEstate;
+        public Estate selectedAvailableEstate{
+            get => _selectedAvailableEstate;
+            set => SetProperty(ref _selectedAvailableEstate, value);
+        }
+
+        private MySqlConnection connection = new MySqlConnection(UtilityVariables.connectionString);
+        private Database database;
+
         public void setClient(){
             User found = database.getUsers().Where(u => u.Name == userName).ToList().ElementAtOrDefault(0);
             if (found != null){
@@ -33,6 +59,75 @@ namespace seven.ViewModels{
             } else {
                 client = database.createUser(userName);
             }
+        }
+        public void setOwnedEstates(){
+            ownedEstates = new ObservableCollection<Estate>(database.getEstates().Where(e => e.Owner.ID == client.ID));
+        }
+        public void setAvailableEstates(){
+            availableEstates = new ObservableCollection<Estate>(database.getEstates().Where(e => e.Owner.ID != client.ID));
+        }
+        private void _updateUser(){
+            database.updateUser(client);
+            client = database.getUser(client.ID);
+        }
+
+        private DelegateCommand _userStatusCommand;
+        private void _updateUserStatus(object parameters){
+            _updateUser();
+        }
+        public ICommand userStatusCommand => _userStatusCommand;
+
+        private DelegateCommand _userNameCommand;
+        private void _updateUserName(object p){
+            _updateUser();
+        }
+        public ICommand userNameCommand => _userNameCommand;
+
+        private DelegateCommand _userBalanceCommand;
+        private void _updateUserBalance(object p){
+            _updateUser();
+        }
+        public ICommand userBalanceCommand => _userBalanceCommand;
+
+        private DelegateCommand _selectedOwnedEstateChange;
+        private void _updateSelectedOwnedEstate(object p){
+            database.updateEstate(selectedOwnedEstate);
+        }
+        public ICommand selectedOwnedEstateChange => _selectedOwnedEstateChange;
+
+        private DelegateCommand _newEstateCommand;
+        private void _addNewEstate(object p){
+            if (newEstate.Kind==EstateKind.New && client.Admin==0){
+                MessageBox.Show("Can only add new buildings by managers");
+                return;
+            }
+            database.createEstate(newEstate.Title, newEstate.Kind,client, newEstate.Price);
+            newEstate = new Estate();
+        }
+        public ICommand newEstateCommand => _newEstateCommand;
+
+        private DelegateCommand _buyEstateCommand;
+        private void _buyEstate(object p){
+            if (client.Balance < selectedAvailableEstate.Price){
+                MessageBox.Show("Not enough money");
+                return;
+            }
+            selectedAvailableEstate.Owner = client;
+            client.Balance -= selectedAvailableEstate.Price;
+            database.updateEstate(selectedAvailableEstate);
+            setAvailableEstates();
+        }
+        public ICommand buyEstateCommand => _buyEstateCommand;
+
+        public ProfileViewModel(){
+            connection.Open();
+            database = new Database(connection);
+            _userStatusCommand = new DelegateCommand(_updateUserStatus);
+            _userNameCommand = new DelegateCommand(_updateUserName);
+            _userBalanceCommand = new DelegateCommand(_updateUserBalance);
+            _selectedOwnedEstateChange = new DelegateCommand(_updateSelectedOwnedEstate);
+            _newEstateCommand = new DelegateCommand(_addNewEstate);
+            _buyEstateCommand = new DelegateCommand(_buyEstate);
         }
     }
 }
--- 31FR Frejmvorky Rozrobky/7/seven/Views/ProfileView.xaml.cs
+++ 31FR Frejmvorky Rozrobky/7/seven/Views/ProfileView.xaml.cs
@@ -19,200 +19,33 @@ namespace seven {
             viewModel.setClient();
         }
         public void homeTabOpened(object sender, RoutedEventArgs e){
-            showUserData();
+            viewModel.setClient();
         }
         public void ownedEstatesTabOpened(object sender, RoutedEventArgs e){
-            showOwnedEstates();
+            viewModel.setOwnedEstates();
+            viewModel.selectedOwnedEstate = null;
         }
         public void availableEstatesTabOpened(object sender, RoutedEventArgs e){
-            showAvailableEstates();
+            viewModel.setAvailableEstates();
+            viewModel.newEstate = new Estate();
         }
         public void incomingMeetingsTabOpened(object sender, RoutedEventArgs e){
-            showIncomingMeetings();
+            //showIncomingMeetings();
         }
         public void outgoingMeetingsTabOpened(object sender, RoutedEventArgs e){
-            showOutgoingMeetings();
+            //showOutgoingMeetings();
         }
-        public void showUserData(){
-            UserNameBox.Text=client.Name;
-            UserBalanceBox.Text= client.Balance.ToString();
-            UserStatusToggle.IsChecked=Convert.ToBoolean(client.Admin);
+        private void ownedEstatesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e){
+            if (ownedEstatesGrid.SelectedIndex < 0) { return; }
+            try{
+                viewModel.selectedOwnedEstate = viewModel.ownedEstates[ownedEstatesGrid.SelectedIndex];
+            } catch { return; }
         }
-        public void showOwnedEstates(){
-            var data = database.getEstates().Where(e => e.Owner.ID == client.ID).ToList();
-            OwnedEstatesContainer.Items.Clear();
-            foreach (var e in data){
-                OwnedEstatesContainer.Items.Add($"{e.ID}. {e.Title} of kind {e.Kind} price {e.Price} owned by {e.Owner.Name}");
-            }
-            EditTitleInput.Text = "";
-            EditKindInput.Text = EstateKind.Home;
-            EditPriceInput.Text = "0";
-        }
-        public void showAvailableEstates(){
-            var data = database.getEstates().Where(e=>e.Owner.ID!=client.ID).ToList();
-            AvailableEstatesContainer.Items.Clear();
-            foreach (var e in data){
-                AvailableEstatesContainer.Items.Add($"{e.ID}. {e.Title} of kind {e.Kind} price {e.Price} owned by {e.Owner.Name}");
-            }
-            SellTitleInput.Text = "";
-            SellPriceInput.Text = "0";
-            SellKindInput.Text = "Home";
-        }
-        public void showIncomingMeetings(bool onlyPending=false){
-            var data = database.getMeetings().Where(m => m.Target.Owner.ID == client.ID).OrderBy(m=>m.ID).Reverse().ToList();
-            if (onlyPending) { data = data.Where(m => m.Status == MeetingStatus.Wait && m.Score == "Unrated").ToList(); }
-            IncomingMeetingsContainer.Items.Clear();
-            foreach (var m in data){
-                IncomingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
-            }
-        }
-        public void showOutgoingMeetings(){
-            var data = database.getMeetings().Where(m=>m.Sender.ID==client.ID).OrderBy(m => m.ID).Reverse().ToList();
-            OutgoingMeetingsContainer.Items.Clear();
-            foreach (var m in data){
-                OutgoingMeetingsContainer.Items.Add($"{m.ID}. For {m.Target.Title} by {m.Sender.Name} to {m.Target.Owner.Name} rated {m.Score} status {m.Status}");
-            }
-        }
-        private void ChangeNameButton_Click(object sender, RoutedEventArgs e){
-            var name = UserNameBox.Text;
-            client.Name = name;
-            database.updateUser(client);
-
-            client = database.getUser(client.ID);
-            showUserData();
-        }
-        private void ChangeBalanceButton_Click(object sender, RoutedEventArgs e){
-            int balance=client.Balance;
-            try {
-                balance = Convert.ToInt32(UserBalanceBox.Text);
-            } catch { MessageBox.Show("Wrong balance. Please enter a number"); }
-
-            client.Balance = balance;
-            database.updateUser(client);
-
-            client = database.getUser(client.ID);
-            showUserData();
-        }
-        private void UserStatusToggle_Click(object sender, RoutedEventArgs e){
-            var status = UserStatusToggle.IsChecked;
-            client.Admin = Convert.ToInt32(status);
-            database.updateUser(client);
-
-            client = database.getUser(client.ID);
-            showUserData();
-        }
-        private void BuyEstateButton_Click(object sender, RoutedEventArgs e){
-            if (AvailableEstatesContainer.SelectedIndex<0){ return; }
-            string id = AvailableEstatesContainer.SelectedValue.ToString().Split('.')[0];
-            var estate = database.getEstate(int.Parse(id));
-
-            if (client.Balance < estate.Price){
-                MessageBox.Show("Not enough money");
-                return;
-            }
-            estate.Owner = client;
-            client.Balance -= estate.Price;
-
-            database.updateEstate(estate);
-            showAvailableEstates();
-        }
-        private void SetMeetingButton_Click(object sender, RoutedEventArgs e){
-            if (AvailableEstatesContainer.SelectedIndex < 0){ return; }
-            string id = AvailableEstatesContainer.SelectedValue.ToString().Split('.')[0];
-            var estate = database.getEstate(int.Parse(id));
-
-            database.createMeeting(client, estate);
-            showOutgoingMeetings();
-        }
-        private void AeSellButton_Click(object sender, RoutedEventArgs e){
-            var title = SellTitleInput.Text;
-            var priceInput = SellPriceInput.Text;
-            int price;
-            var kind = SellKindInput.Text;
-            try {
-                price = int.Parse(priceInput);
-            } catch {
-                MessageBox.Show("Incorrect price, please enter a number");
-                return;
-            }
-            if (kind==EstateKind.New && client.Admin == 0){
-                MessageBox.Show($"Estate of kind {EstateKind.New} may be added only by managers");
-                return;
-            }
-
-            database.createEstate(title, kind, client, price);
-            showAvailableEstates();
-        }
-        private void OwnedEstatesContainer_SelectionChanged(object sender, SelectionChangedEventArgs e){
-            if (OwnedEstatesContainer.SelectedIndex<0){ return; }
-            string id = OwnedEstatesContainer.SelectedValue.ToString().Split('.')[0];
-            var estate = database.getEstate(int.Parse(id));
-
-            EditTitleInput.Text = estate.Title;
-            EditKindInput.Text = estate.Kind;
-            EditPriceInput.Text = estate.Price.ToString();
-        }
-        private void OeEditButton_Click(object sender, RoutedEventArgs e){
-            if (OwnedEstatesContainer.SelectedIndex < 0){ return; }
-            string id = OwnedEstatesContainer.SelectedValue.ToString().Split('.')[0];
-            var estate = database.getEstate(int.Parse(id));
-
-            var title = EditTitleInput.Text;
-            var kind = EditKindInput.Text;
-            var priceInput = EditPriceInput.Text;
-            int price;
-            try {
-                price = int.Parse(priceInput);
-            } catch {
-                MessageBox.Show("Incorrect price, please enter a number");
-                return;
-            }
-            estate.Title = title;
-            estate.Price = price;
-            estate.Kind = kind;
-            
-            database.updateEstate(estate);
-            showOwnedEstates();
-        }
-        private void ProcessButton_Click(object sender, RoutedEventArgs e){
-            if (IncomingMeetingsContainer.SelectedIndex < 0){ return; }
-            string id = IncomingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
-            var meeting = database.getMeeting(int.Parse(id));
-
-            var status = ProcessInput.Text;
-            meeting.Status = status;
-
-            database.updateMeeting(meeting);
-            showIncomingMeetings();
-        }
-        private void RateButton_Click(object sender, RoutedEventArgs e){
-            if (OutgoingMeetingsContainer.SelectedIndex < 0){ return; }
-            string id = OutgoingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
-            var meeting = database.getMeeting(int.Parse(id));
-
-            var score = RateInput.Text;
-            meeting.Score = score;
-
-            database.updateMeeting(meeting);
-            showOutgoingMeetings();
-        }
-        private void IncomingMeetingsContainer_SelectionChanged(object sender, SelectionChangedEventArgs e){
-            if (IncomingMeetingsContainer.SelectedIndex < 0){ return; }
-            string id = IncomingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
-            var meeting = database.getMeeting(int.Parse(id));
-
-            ProcessInput.Text = meeting.Status;
-        }
-        private void OutgoingMeetingsContainer_SelectionChanged(object sender, SelectionChangedEventArgs e){
-            if (OutgoingMeetingsContainer.SelectedIndex < 0){ return; }
-            string id = OutgoingMeetingsContainer.SelectedValue.ToString().Split('.')[0];
-            var meeting = database.getMeeting(int.Parse(id));
-
-            RateInput.Text = meeting.Score;
-        }
-        private void CheckBox_Click(object sender, RoutedEventArgs e) {
-            var status = Convert.ToBoolean(pendingMeetingsToggle.IsChecked);
-            showIncomingMeetings(status);
+        private void availableEstatesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e){
+            if (availableEstatesGrid.SelectedIndex < 0) { return; }
+            try{
+                viewModel.selectedAvailableEstate = viewModel.availableEstates[availableEstatesGrid.SelectedIndex];
+            } catch { return; }
         }
     }
 }
