b23a9a48f296f02c5a5d1f6acd9e88fb67953ba0
Oleh Onyshchenko <seesmwork@gmail.com>
2025-02-24 13:02:43 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/1/traffic_light.cpp
+++ 32IV Inzhenerija vbudovanyh/1/traffic_light.cpp
@@ -1,28 +1,28 @@
-int led_red = 2;
-int led_yellow = 1;
-int led_green = 0;
+int red=2;
+int yellow=1;
+int green=0;
 
 void setup(){
-  pinMode(led_red, OUTPUT);
-  pinMode(led_yellow, OUTPUT);
-  pinMode(led_green, OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
 }
 
 int delay_time=1000;
 
 void loop(){
-  digitalWrite(led_red, LOW);
-  digitalWrite(led_yellow, LOW);
-  digitalWrite(led_green, HIGH);
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
   delay(delay_time);
 
-  digitalWrite(led_red, LOW);
-  digitalWrite(led_yellow, HIGH);
-  digitalWrite(led_green, LOW);
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,HIGH);
+  digitalWrite(green,LOW);
   delay(delay_time);
 
-  digitalWrite(led_red, HIGH);
-  digitalWrite(led_yellow, LOW);
-  digitalWrite(led_green, LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
   delay(delay_time);
 }
