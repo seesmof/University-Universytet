ba5e5460a074effe0f4570aa65863003dbb3bca5
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-01 09:45:38 +0200

    Click to see changes

--- 32KP Konstrujuvannja program/1/a.java
+++ 32KP Konstrujuvannja program/1/a.java
@@ -1,9 +1,16 @@
 class a {
   public static void main(String[] args) {
-    String[] texts = new String[3];
+    // Оголошення
+    String[] texts;
+    // Встановлення
+    texts = new String[3];
+    // Ініціаліація
     texts[0] = "One";
     texts[1] = "Two";
     texts[2] = "Three";
-
+    // Вивід
+    for (String text : texts) {
+      System.err.println(text);
+    }
   }
 }
