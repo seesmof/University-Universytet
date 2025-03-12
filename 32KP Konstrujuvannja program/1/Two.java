c59ed59bf8b1cac53007b67c45b69ee30f82ce26
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-12 18:10:26 +0200

    Click to see changes

--- 32KP Konstrujuvannja program/1/Two.java
+++ 32KP Konstrujuvannja program/1/Two.java
@@ -5,12 +5,12 @@ import java.util.TreeMap;
 class Two {
   public static void showMatrix(int[][] matrix) {
     System.out.println();
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix.length; j++) {
-        if (i == j) {
-          System.out.print("\033[0;1m" + matrix[i][j] + "\033[0m ");
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix.length; col++) {
+        if (row == col) {
+          System.out.print("\033[0;1m" + matrix[row][col] + "\033[0m ");
         } else {
-          System.out.print(matrix[i][j] + " ");
+          System.out.print(matrix[row][col] + " ");
         }
       }
       System.out.println();
@@ -29,19 +29,19 @@ class Two {
     // Заповнення
     int[][] matrix = new int[matrixSize][matrixSize];
     TreeMap<Integer, String> numbers = new TreeMap<Integer, String>(Collections.reverseOrder());
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix.length; j++) {
-        int readableRow = i + 1;
-        int readableCol = j + 1;
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix.length; col++) {
+        int readableRow = row + 1;
+        int readableCol = col + 1;
         System.out.print("Please enter element on row " + readableRow + " and col " + readableCol + ": ");
 
         int givenElement = inputReader.nextInt();
-        matrix[i][j] = givenElement;
-        String position = i + "," + j;
+        matrix[row][col] = givenElement;
+        String position = row + "," + col;
         numbers.put(givenElement, position);
       }
     }
-    System.out.println();
+    showMatrix(matrix);
 
     int matrixSizeCounter = 0;
     for (Integer key : numbers.keySet()) {
@@ -53,13 +53,12 @@ class Two {
       String[] positionParts = value.split(",");
       int positionRow = Integer.parseInt(positionParts[0]);
       int positionCol = Integer.parseInt(positionParts[1]);
-
       System.out.println(key + ": row " + positionRow + " and col " + positionCol);
 
       int neededRow = matrixSizeCounter;
       int neededCol = matrixSizeCounter;
-      System.out.println("Needs to be on row " + neededRow + " and on col " + neededCol);
       String neededPosition = neededRow + "," + neededCol;
+      System.out.println("Needs to be on row " + neededRow + " and on col " + neededCol);
 
       int swappedElement = matrix[neededRow][neededCol];
       int currentElement = matrix[positionRow][positionCol];
@@ -68,7 +67,6 @@ class Two {
 
       showMatrix(matrix);
       numbers.put(key, neededPosition);
-
       matrixSizeCounter += 1;
     }
 
