084f1f18a565bc96e55c8e0cacae9329dcabbd07
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-14 16:30:28 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -1,11 +1,17 @@
+int redPin=11;
+int greenPin=9;
+int bluePin=10;
+
 void setup()
 {
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 }
 
 void loop()
 {
-  
+  analogWrite(greenPin,213);
+  analogWrite(bluePin,139);
+  analogWrite(redPin,255);
 }
