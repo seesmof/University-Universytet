850638338d3b63cd75ff59eb6880ff8b2b5c100d
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-15 12:52:06 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -1,55 +1,17 @@
-int MIN_VALUE=85;
-int MAX_VALUE=385;
-
-int smokePin=A0;
-int redPin=9;
-int greenPin=11;
+int redPin=11;
+int greenPin=9;
 int bluePin=10;
 
 void setup()
 {
-  pinMode(smokePin, INPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-
-  digitalWrite(redPin, HIGH);
-  digitalWrite(greenPin, HIGH);
-  digitalWrite(bluePin, HIGH);
-}
-
-void turnLedOff() {
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(bluePin, LOW);
 }
 
 void loop()
 {
-  int smokeValue=analogRead(smokePin);
-  int convertedValue=map(smokeValue, MIN_VALUE, MAX_VALUE, 0, 100);
-  if (convertedValue<25){
-    // Зелений
-    digitalWrite(redPin, 138);
-    digitalWrite(greenPin, 201);
-    digitalWrite(bluePin, 38);
-  } else if (convertedValue>=25 && convertedValue<50){
-    // Жовтий
-    digitalWrite(redPin, 255);
-    digitalWrite(greenPin, 202);
-    digitalWrite(bluePin, 58);
-  } else if (convertedValue>=50 && convertedValue<75){
-    // Помаранчевий
-    digitalWrite(redPin, 255);
-    digitalWrite(greenPin, 146);
-    digitalWrite(bluePin, 76);
-  } else if (convertedValue>=75){
-    // Червоний
-    digitalWrite(redPin, 255);
-    digitalWrite(greenPin, 89);
-    digitalWrite(bluePin, 94);
-  } else {
-    turnLedOff();
-  }
-  delay(100);
+  digitalWrite(redPin, 255);
+  digitalWrite(greenPin, 0);
+  digitalWrite(bluePin, 0);
 }
