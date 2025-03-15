080436988688c3b45ec1352c5d0c22af823568d8
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-15 12:18:17 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -18,14 +18,38 @@ void setup()
   digitalWrite(bluePin, HIGH);
 }
 
+void turnLedOff() {
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+}
+
 void loop()
 {
   int smokeValue=analogRead(smokePin);
   int convertedValue=map(smokeValue, MIN_VALUE, MAX_VALUE, 0, 100);
-  if (convertedValue>30){
-    digitalWrite(ledPin, HIGH);
+  if (convertedValue<25){
+    // Зелений
+    digitalWrite(redPin, 138);
+    digitalWrite(greenPin, 201);
+    digitalWrite(bluePin, 38);
+  } else if (convertedValue>=25 && convertedValue<50){
+    // Жовтий
+    digitalWrite(redPin, 255);
+    digitalWrite(greenPin, 202);
+    digitalWrite(bluePin, 58);
+  } else if (convertedValue>=50 && convertedValue<75){
+    // Помаранчевий
+    digitalWrite(redPin, 255);
+    digitalWrite(greenPin, 146);
+    digitalWrite(bluePin, 76);
+  } else if (convertedValue>=75){
+    // Червоний
+    digitalWrite(redPin, 255);
+    digitalWrite(greenPin, 89);
+    digitalWrite(bluePin, 94);
   } else {
-    digitalWrite(ledPin, LOW);
+    turnLedOff();
   }
   delay(100);
 }
