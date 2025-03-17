09a44522b5de2dd0da1ed4fb74ef852f8f8d6dea
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-17 19:46:13 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -5,36 +5,23 @@ int smokePin=A0;
 int ledPin=7;
 int piezoPin=3;
 
-int delayTime=1000;
-bool appState=true;
-
 void setup()
 {
   pinMode(smokePin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(piezoPin, OUTPUT);
-
-  Serial.begin(9600);
 }
 
 void loop()
 {
   int smokeValue = analogRead(smokePin);
-  int convertedValue = map(smokeValue, MIN_VALUE, MAX_VALUE, 0, 100);
-  Serial.println(convertedValue);
-  delayTime=convertedValue*10;
-  if (appState) {
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(piezoPin, HIGH);
-    appState=false;
-  } else {
-    digitalWrite(ledPin, LOW);
-    digitalWrite(piezoPin, LOW);
-    appState=true;
-  }
-  if (convertedValue<=0 || convertedValue>100){
-    delay(1000);
-  } else {
-    delay(-delayTime);
-  }
+  int convertedValue = map(smokeValue, MIN_VALUE, MAX_VALUE, 1, 100);
+  int delayTime=map(convertedValue, 100, 1, 1, 100);
+  delayTime=abs(-delayTime*10);
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(piezoPin, HIGH);
+  delay(delayTime);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(piezoPin, LOW);
+  delay(delayTime);
 }
