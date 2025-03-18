e1f20c17f81bfb5799da6652ff74f175f7ee60f6
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-18 17:33:02 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,30 +1,19 @@
-#define DECODE_NEC
 #include <IRremote.hpp>
 
 const int RECEIVER_PIN=3;
+IRrecv receiver(RECEIVER_PIN);
+decode_results results;
+
 void setup()
 {
   Serial.begin(9600);
-  IrReceiver.begin(RECEIVER_PIN);
+  receiver.enableIRIn();
 }
 
 void loop()
 {
-  receiveSignal();
-}
-
-uint16_t receiveSignal(){
-  uint16_t received{0};
-
-  if (IrReceiver.decode()){
-    if (IrReceiver.decodedIRData.protocol==UNKNOWN){
-      IrReceiver.printIRResultRawFormatted(&Serial,true);
-    }
-    if (IrReceiver.decodedIRData.protocol==NEC){
-      received=IrReceiver.decodedIRData.command;
-      Serial.println(received,HEX);
-    }
-    IrReceiver.resume();
+  if (receiver.decode(&results)){
+    Serial.println(results.value,HEX);
+    receiver.resume();
   }
-  return received;
 }
