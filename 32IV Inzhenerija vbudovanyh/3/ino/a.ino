8d2060168fbce30f1c479bbf8713e27e6d9c9722
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-18 09:51:53 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -2,7 +2,11 @@ const int RED_PIN=11;
 const int GREEN_PIN=9;
 const int BLUE_PIN=10;
 const int BUTTON_PIN=7;
-const int COLORS[7][3] = [[255,255,255],[255,0,0],[255,255,0],[0,255,0],[0,255,255],[0,0,255],[255,0,255]];
+
+const int COLORS_ROWS=7;
+const int COLORS_COLS=3;
+const int COLORS[COLORS_ROWS][COLORS_COLS] = {{255,255,255},{255,0,0},{255,255,0},{0,255,0},{0,255,255},{0,0,255},{255,0,255}};
+int row=-1;
 
 void setup()
 {
@@ -10,8 +14,6 @@ void setup()
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-
-  Serial.begin(9600);
 }
 
 void writeRgb(int redValue, int greenValue, int blueValue){
@@ -20,9 +22,21 @@ void writeRgb(int redValue, int greenValue, int blueValue){
   digitalWrite(BLUE_PIN, blueValue);
 }
 
+void turnLedOff(){
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, LOW);
+}
+
 void loop()
 {
-  for(int i=0; i<COLORS.length(); i++)
-    for (int j=0; j<COLORS[i].length(); j++)
-      Serial.println(COLORS[i][j])
+  int buttonState=digitalRead(BUTTON_PIN);
+  if (buttonState==1){
+    if (row<COLORS_ROWS) row+=1;
+    else row=0;
+    writeRgb(COLORS[row][0],COLORS[row][1],COLORS[row][2]);
+  } else {
+    turnLedOff();
+  }
+  delay(100);
 }
