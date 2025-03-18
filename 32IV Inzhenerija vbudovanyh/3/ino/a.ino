8e6896aa26b9b2048da1b939787dd58525455822
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-18 09:33:16 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/3/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/3/ino/a.ino
@@ -2,6 +2,7 @@ const int RED_PIN=11;
 const int GREEN_PIN=9;
 const int BLUE_PIN=10;
 const int BUTTON_PIN=7;
+const int COLORS[7][3] = [[255,255,255],[255,0,0],[255,255,0],[0,255,0],[0,255,255],[0,0,255],[255,0,255]];
 
 void setup()
 {
@@ -9,6 +10,8 @@ void setup()
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
+
+  Serial.begin(9600);
 }
 
 void writeRgb(int redValue, int greenValue, int blueValue){
@@ -19,5 +22,7 @@ void writeRgb(int redValue, int greenValue, int blueValue){
 
 void loop()
 {
-  
+  for(int i=0; i<COLORS.length(); i++)
+    for (int j=0; j<COLORS[i].length(); j++)
+      Serial.println(COLORS[i][j])
 }
