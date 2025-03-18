ef4b6afd0acddd46e7f243903b339f1e2a2ae062
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-18 12:03:29 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,9 +1,17 @@
+const int MIN_PHOTORESISTOR=6;
+const int MAX_PHOTORESISTOR=679;
+
+const int PHOTORESISTOR_PIN=A0;
+
 void setup()
 {
-  
+  pinMode(PHOTORESISTOR_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  
+  int photoresistorValue=analogRead(PHOTORESISTOR_PIN);
+  Serial.println(photoresistorValue);
+  delay(100);
 }
