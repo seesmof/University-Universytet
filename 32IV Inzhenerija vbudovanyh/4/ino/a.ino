f9db45e38a8a9c7968191c83b7d03e7f0337a2e3
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-19 18:52:00 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,23 +1,17 @@
 #include <Servo.h>
 
 const int SERVO_PIN=7;
+const int RIGHT=87;
+const int LEFT=97;
 Servo servo;
-int angle=0;
 
 void setup()
 {
   servo.attach(SERVO_PIN);
   Serial.begin(9600);
-
-  servo.write(angle);
 }
 
 void loop()
 {
-  int angle=servo.read();
-  for (int i=angle;i<=360*2;i++){
-    servo.write(i);
-    Serial.println(i);
-  }
-  delay(1000);
+  delay(300);
 }
