4155e76cb046a372f02bfd8aa3ea6adda6674cee
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-19 18:59:56 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,8 +1,14 @@
 #include <Servo.h>
 
 const int SERVO_PIN=7;
-const int RIGHT=87;
-const int LEFT=97;
+const int LEFT_PHOTORESISTOR_PIN=11;
+const int RIGHT_PHOTORESISTOR_PIN=10;
+
+const int ROTATE_RIGHT=87;
+const int ROTATE_LEFT=97;
+const int MIN_SENSOR=6;
+const int MAX_SENSOR=679;
+
 Servo servo;
 
 void setup()
@@ -13,5 +19,11 @@ void setup()
 
 void loop()
 {
+  int leftValue=analogRead(LEFT_PHOTORESISTOR_PIN);
+  int rightValue=analogRead(RIGHT_PHOTORESISTOR_PIN);
+  if (leftValue>rightValue)
+    servo.write(ROTATE_LEFT);
+  else
+    servo.write(ROTATE_RIGHT);
   delay(300);
 }
