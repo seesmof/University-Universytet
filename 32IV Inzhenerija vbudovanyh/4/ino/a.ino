d968a96b4b8b7858386aaf6f8789dcf0ee931a8a
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-21 10:48:46 +0200

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,18 +1,48 @@
-int pin=5;
+#define DECODE_NEC
+#include <IRremote.hpp>
+
+const unsigned int RECEIVER_PIN=5;
+const unsigned int SWITCH_PIN=2;
+const unsigned int LED_PIN=3;
 int brightness=1;
+int switchValue=0;
 
 void setup()
 {
-  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+  IrReceiver.begin(RECEIVER_PIN);
+  pinMode(SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void changeLed(int brightness){
+  analogWrite(LED_PIN, brightness);
+  Serial.println(brightness);
 }
 
 void loop()
 {
-  brightness+=10;
-  analogWrite(pin, brightness);
+  switchValue=digitalRead(SWITCH_PIN);
+  if (IrReceiver.decode() && switchValue==1){
+    auto data=IrReceiver.decodedIRData.command;
+
+    // Power
+    if (data==0x0){
+      if (brightness>1) brightness=0;
+      else brightness=255;
+    }
+    // +
+    else if (data==0x1) {
+      if (brightness<230) brightness+=25;
+    }
+    // -
+    else if (data==0x9){
+      if (brightness>25) brightness-=25;
+    }
+    
+    IrReceiver.resume();
+  }
+  Serial.println(brightness);
+  analogWrite(LED_PIN, brightness);
   delay(300);
-  digitalWrite(pin, LOW);
-  delay(300);
-  if (brightness>=255)
-    brightness=0;
 }
