d2e6b82b028e1cc0b0b60d549c45660507ff8801
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-21 09:54:42 +0200

    Click to see changes

---
+++ 32IV Inzhenerija vbudovanyh/4/ino/32IV-4_Bright-LED.ino
@@ -0,0 +1,50 @@
+#define DECODE_NEC
+#include <IRremote.hpp>
+
+const unsigned int RECEIVER_PIN=5;
+const unsigned int SWITCH_PIN=2;
+const unsigned int LED_PIN=3;
+int brightness=0;
+int switchValue=0;
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(RECEIVER_PIN);
+  pinMode(SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void changeLed(){
+  int value = map(brightness, 0, 10, 0, 255);
+  analogWrite(LED_PIN, value);
+  Serial.println(value);
+}
+
+void loop()
+{
+  switchValue=digitalRead(SWITCH_PIN);
+  if (IrReceiver.decode() && switchValue==1){
+    auto data=IrReceiver.decodedIRData.command;
+
+    // Power
+    if (data==0x0){
+      if (brightness>1) brightness=0;
+      else brightness=10;
+      changeLed();
+    }
+    // +
+    else if (data==0x1) {
+      if (brightness<10) brightness+=1;
+      changeLed();
+    }
+    // -
+    else if (data==0x9){
+      if (brightness>0) brightness-=1;
+      changeLed();
+    }
+    
+    IrReceiver.resume();
+  }
+  delay(300);
+}
--- 32IV Inzhenerija vbudovanyh/4/ino/a.ino
+++ 32IV Inzhenerija vbudovanyh/4/ino/a.ino
@@ -1,55 +1,18 @@
-#define DECODE_NEC
-#include <IRremote.hpp>
-
-const unsigned int RECEIVER_PIN=5;
-const unsigned int SWITCH_PIN=2;
-const unsigned int LED_PIN=3;
+int pin=5;
 int brightness=1;
-int switchValue=0;
-bool ledOff=true;
 
 void setup()
 {
-  Serial.begin(9600);
-  IrReceiver.begin(RECEIVER_PIN);
-  pinMode(SWITCH_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
-}
-
-void changeLed(){
-  int value = map(brightness, 1, 10, 0, 255);
-  analogWrite(LED_PIN, value);
-  Serial.println(value);
+  pinMode(pin, OUTPUT);
 }
 
 void loop()
 {
-  switchValue=digitalRead(SWITCH_PIN);
-  if (IrReceiver.decode() && switchValue==1){
-    auto data=IrReceiver.decodedIRData.command;
-
-    // Power
-    if (data==0x0){
-      if (ledOff==true){
-        digitalWrite(LED_PIN, HIGH);
-        ledOff=false;
-      } else if (ledOff==false){
-        changeLed();
-        ledOff=true;
-      }
-    }
-    // +
-    else if (data==0x1) {
-      if (brightness<10) brightness+=1;
-      changeLed();
-    }
-    // -
-    else if (data==0x9){
-      if (brightness>1) brightness-=1;
-      changeLed();
-    }
-
-    IrReceiver.resume();
-  }
-  delay(100);
+  brightness+=10;
+  analogWrite(pin, brightness);
+  delay(300);
+  digitalWrite(pin, LOW);
+  delay(300);
+  if (brightness>=255)
+    brightness=0;
 }
