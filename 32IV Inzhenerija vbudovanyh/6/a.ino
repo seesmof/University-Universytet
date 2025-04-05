e4941261a3ed7b15e60ff0e30d8de6a9d7785ed7
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-05 17:03:05 +0300

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/6/a.ino
+++ 32IV Inzhenerija vbudovanyh/6/a.ino
@@ -1,28 +1,29 @@
-int sensorOn=0;
-int sensorPin=3;
-int ledPin=13;
-int photoPin=A0;
-int MIN_PHOTO=6;
-int MAX_PHOTO=679;
+int greenLed=13;
+int yellowLed=11;
+int orangeLed=7;
+int tempPin=A0;
+
+int getTemperatureInDegrees() {
+  int value=analogRead(tempPin);
+  int percents=(double) value/1024;
+  Serial.println(percents);
+  int voltage=percents*5;
+  int minusOffset=voltage-0.5;
+  int degreesCelcius=minusOffset*100;
+  return degreesCelcius;
+}
 
 void setup()
 {
-  pinMode(sensorPin, INPUT);
-  pinMode(photoPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(orangeLed, OUTPUT);
+  pinMode(tempPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  sensorOn=digitalRead(sensorPin);
-  int photoValue=analogRead(photoPin);
-  int convertedValue=map(photoValue, MIN_PHOTO, MAX_PHOTO, 1, 100);
-  Serial.println(convertedValue);
-  if (sensorOn==HIGH && convertedValue>=50){
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
-  }
-  delay(120);
+  int temperature=getTemperatureInDegrees();
+  delay(100);
 }
