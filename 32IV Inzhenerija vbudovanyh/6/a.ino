507604cadfe76d1c8d246160706e6cde57c6cd81
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-07 09:41:49 +0300

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/6/a.ino
+++ 32IV Inzhenerija vbudovanyh/6/a.ino
@@ -2,6 +2,7 @@
 
 LiquidCrystal lcd(13,12,11,10,9,8);
 int upPin=7,downPin=6,goPin=5,backPin=4;
+int cursorRow=0;
 
 void setup()
 {
@@ -14,9 +15,32 @@ void setup()
   Serial.begin(9600);
 }
 
+void showFirstMenu() {
+  lcd.setCursor(2,0);
+  lcd.print("Open");
+  lcd.setCursor(2,1);
+  lcd.print("Close");
+}
+
+void updateCursor() {
+  if (cursorRow==0) {
+    lcd.setCursor(0,0);
+  } else { 
+    lcd.setCursor(0,1);
+  }
+  lcd.print("- ");
+}
+
 void loop()
 {
-  
+  int upPressed=digitalRead(upPin);
+  int downPressed=digitalRead(downPin);
+  int goPressed=digitalRead(goPin);
+  int backPressed=digitalRead(backPin);
+
+  showFirstMenu();
+  updateCursor();
+
   delay(100);
   lcd.clear();
 }
