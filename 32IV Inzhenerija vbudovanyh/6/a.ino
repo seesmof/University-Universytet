0dcd7f8c1024f65036e3d08c1f6391a54530cf5a
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-07 09:37:17 +0300

    Click to see changes

--- 32IV Inzhenerija vbudovanyh/6/a.ino
+++ 32IV Inzhenerija vbudovanyh/6/a.ino
@@ -1,32 +1,21 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(13,12,11,10,9,8);
-int upPin=7,downPin=6,leftPin=5,rightPin=4;
-
-int getTemperature() {
-  float temperature=analogRead(tempPin)*0.004882814;
-  temperature=(temperature-0.5)*100.0;
-  int tmp=round(temperature);
-  return tmp;
-}
+int upPin=7,downPin=6,goPin=5,backPin=4;
 
 void setup()
 {
-  lcd.begin(16,2);
-  pinMode(tempPin, INPUT);
   pinMode(upPin, INPUT);
   pinMode(downPin, INPUT);
-  pinMode(leftPin, INPUT);
-  pinMode(rightPin, INPUT);
+  pinMode(goPin, INPUT);
+  pinMode(backPin, INPUT);
+
+  lcd.begin(16,2);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int temperature = getTemperature();
-  lcd.setCursor(0,0);
-  lcd.print(temperature);
-  lcd.print(" C");
   
   delay(100);
   lcd.clear();
