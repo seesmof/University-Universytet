48d2eaa61b58a5961fd721fcec01f3b3d2751e88
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-19 10:19:04 +0300

    Click to see changes

--- 32K Kursach/test_nuxt/app/app.vue
+++ 32K Kursach/test_nuxt/app/app.vue
@@ -1,5 +1,29 @@
+<script setup>
+import { reactive, computed } from "vue";
+
+const authors = reactive(
+  {
+    name: "John Doe",
+    books: [
+      "Vue 2 - Advanced Guide",
+      "Vue 3 - Basic Guide",
+      "Vue 4 - The Mystery",
+    ],
+  },
+  {
+    name: "Some",
+    books: [],
+  }
+);
+
+// a computed ref
+const publishedBooksMessage = computed((author) => {
+  return author.books.length > 0 ? "Yes" : "No";
+});
+</script>
+
 <template>
-  <UApp>
-    <NuxtPage />
-  </UApp>
+  <ul v-for="author of authors">
+    <span>{{ publishedBooksMessage(author) }}</span>
+  </ul>
 </template>
