a59e1a1271e3214846f421a221ac18bb2685802a
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-21 22:25:09 +0300

    Click to see changes

---
+++ 32K Kursach/app/ChildComp.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({
+  message: String,
+});
+</script>
+
+<template>
+  <h2>Amen</h2>
+  <p>{{ message }}</p>
+</template>
--- 32K Kursach/app/app.vue
+++ 32K Kursach/app/app.vue
@@ -1,49 +1,11 @@
 <script setup>
-import { ref, computed } from "vue";
+import ChildComp from "./ChildComp.vue";
+import { ref } from "vue";
 
-let id = 0;
-
-const newTodo = ref("");
-const hideCompleted = ref(false);
-const todos = ref([
-  { id: id++, text: "Learn HTML", done: true },
-  { id: id++, text: "Learn JavaScript", done: true },
-  { id: id++, text: "Learn Vue", done: false },
-]);
-
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false });
-  newTodo.value = "";
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo);
-}
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
-});
+const greets = ref("Allelujah");
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" required placeholder="new todo" />
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? "Show all" : "Hide completed" }}
-  </button>
+  <input type="text" name="message" id="message" v-model="greets" />
+  <ChildComp :message="greets" />
 </template>
-
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
