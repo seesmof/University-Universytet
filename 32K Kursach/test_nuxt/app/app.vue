6b2653033cc6fa38fa2451d5b010571c6489c271
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-21 11:38:23 +0300

    Click to see changes

--- 32K Kursach/test_nuxt/app/app.vue
+++ 32K Kursach/test_nuxt/app/app.vue
@@ -1,29 +1,49 @@
 <script setup>
-import { reactive, computed } from "vue";
+import { ref, computed } from "vue";
 
-const authors = reactive(
-  {
-    name: "John Doe",
-    books: [
-      "Vue 2 - Advanced Guide",
-      "Vue 3 - Basic Guide",
-      "Vue 4 - The Mystery",
-    ],
-  },
-  {
-    name: "Some",
-    books: [],
-  }
-);
+let id = 0;
 
-// a computed ref
-const publishedBooksMessage = computed((author) => {
-  return author.books.length > 0 ? "Yes" : "No";
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const todos = ref([
+  { id: id++, text: "Learn HTML", done: true },
+  { id: id++, text: "Learn JavaScript", done: true },
+  { id: id++, text: "Learn Vue", done: false },
+]);
+
+const filteredTodos = computed(() => {
+  return [...todos.value].filter((t) => (t.done ? !hideCompleted.value : t));
 });
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false });
+  newTodo.value = "";
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
 </script>
 
 <template>
-  <ul v-for="author of authors">
-    <span>{{ publishedBooksMessage(author) }}</span>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
+  </button>
 </template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
