ad2fae411557d17f55cb4fd3e0e4d6245bd6f400
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-28 16:26:24 +0300

    Click to see changes

--- 32K Kursach/app/grapes/models.py
+++ 32K Kursach/app/grapes/models.py
@@ -7,6 +7,7 @@ class Bunch(models.Model):
     sugar=models.BooleanField(blank=False)
     shape=models.BooleanField(blank=False)
     color=models.BooleanField(blank=False)
+    color=models.BooleanField()
 
     def __str__(self):
         return f'{self.count} grapes: outside {"damaged" if self.outside else "okay"}, sugar {"imbalanced" if self.sugar else "okay"}, shape {"uneven" if self.shape else "okay"}, color {"changed" if self.color else "okay"}'
