553ce6c061940637177adb478108c35fceaa2204
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-28 16:04:22 +0300

    Click to see changes

--- 32K Kursach/app/grapes/models.py
+++ 32K Kursach/app/grapes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Bunch(models.Model):
+    count=models.IntegerField(blank=False)
+    outside=models.BooleanField(blank=False)
+    sugar=models.BooleanField(blank=False)
+    shape=models.BooleanField(blank=False)
+    color=models.BooleanField(blank=False)
