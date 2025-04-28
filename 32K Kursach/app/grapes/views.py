b5d7295f7bc4a61b5fadd1b9663619d3da481819
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-28 16:16:14 +0300

    Click to see changes

--- 32K Kursach/app/grapes/views.py
+++ 32K Kursach/app/grapes/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from grapes.models import Bunch
+
 # Create your views here.
 def index(request):
-    return HttpResponse('AMEN')
+    all_bunches=Bunch.objects.all()
+    res=', '.join([str(b) for b in all_bunches])
+    return HttpResponse(res)
