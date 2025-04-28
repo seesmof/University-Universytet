d4d24ce42b144cb1eb31518c11bd43ee96fabd16
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-28 16:31:45 +0300

    Click to see changes

--- 32K Kursach/app/grapes/urls.py
+++ 32K Kursach/app/grapes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'grapes'
 urlpatterns = [
     path('',views.index,name='index'),
     path('bunches/<int:id>',views.bunches,name='bunch')
