43a00e883e528c55028d86f77df8acd010eb450d
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-29 19:46:39 +0300

    Click to see changes

--- 32K Kursach/app/runner/settings.py
+++ 32K Kursach/app/runner/settings.py
@@ -35,16 +35,18 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'grapes.apps.GrapesConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK='bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
