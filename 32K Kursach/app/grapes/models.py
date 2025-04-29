21e1c608a9b6867b9a04d947ba9bda2e910831e3
Oleh Onyshchenko <seesmwork@gmail.com>
2025-04-29 09:44:11 +0300

    Click to see changes

--- 32K Kursach/app/grapes/models.py
+++ 32K Kursach/app/grapes/models.py
@@ -7,7 +7,15 @@ class Bunch(models.Model):
     sugar=models.BooleanField(blank=False)
     shape=models.BooleanField(blank=False)
     color=models.BooleanField(blank=False)
-    color=models.BooleanField()
+    processed=models.BooleanField()
+    stage_options={
+    'NO':'Processing...',
+    'JU':'Juice',
+    'RS':'Raisins',
+    'GR':'Grapes',
+    'JL':'Jelly',
+    'WN':'Wine'}
+    stage=models.CharField(max_length=7,choices=stage_options,default=stage_options['NO'])
 
     def __str__(self):
         return f'{self.count} grapes: outside {"damaged" if self.outside else "okay"}, sugar {"imbalanced" if self.sugar else "okay"}, shape {"uneven" if self.shape else "okay"}, color {"changed" if self.color else "okay"}'
