e0024c1d4613810a9492c046f0b74487b26bf27a
Oleh Onyshchenko <seesmwork@gmail.com>
2025-05-10 14:34:18 +0300

    Click to see changes

--- 32K Kursach/server/grapes/tests.py
+++ 32K Kursach/server/grapes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from grapes.models import Bunch
+from grapes.utils import process_bunch
+
 # Create your tests here.
+class BunchTestCase(TestCase):
+    def setUp(self):
+        self.bunch=Bunch.objects.create(count=12,outside=True,shape=False,color=False,sugar=False)
+    
+    def test_bunch_processing(self):
+        processed_bunch=process_bunch(self.bunch)
+        self.assertEqual(processed_bunch.stage, Bunch.Stage.Raisins)
