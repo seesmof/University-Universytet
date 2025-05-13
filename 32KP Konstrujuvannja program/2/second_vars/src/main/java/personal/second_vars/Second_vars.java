2c3a53477ef69a190cd80a8c99285bfd3d68fbd8
Oleh Onyshchenko <seesmwork@gmail.com>
2025-05-13 19:26:09 +0300

    Click to see changes

---
+++ 32KP Konstrujuvannja program/2/second_vars/src/main/java/personal/second_vars/Second_vars.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package personal.second_vars;
+
+/**
+ *
+ * @author seesm
+ */
+public class Second_vars {
+
+    public static void main(String[] args) {
+        VariablesFrame frame = new VariablesFrame();
+        frame.setVisible(true);
+    }
+}
