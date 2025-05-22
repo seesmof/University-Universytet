4fbd06894bf5389f36313b9abd18bf8d98cf142f
Oleh Onyshchenko <seesmwork@gmail.com>
2025-05-22 14:28:02 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/1/a.py
+++ 32SS Systemy shtuchnogo/1/a.py
@@ -1,12 +1,25 @@
 import os
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_folder, 'train.csv')
 df = pd.read_csv(file_path)
-needed='Survived,Name,Age'.split(',')
+
+needed='Name,Age,Sex,Survived'.split(',')
 df=df[needed].dropna()
+le=LabelEncoder()
+df['Sex']=le.fit_transform(df['Sex'])
+print(df)
+
+'''
 
 train,test=train_test_split(df,test_size=0.3)
-print(train['Age'].mean(),test['Age'].mean(),['Age'].mean())
+print(train)
+model=KMeans()
+model.fit(train)
+res=model.predict(test)
+'''
--- 32SS Systemy shtuchnogo/1/test.py
+++ 32SS Systemy shtuchnogo/1/test.py
@@ -1,6 +1,13 @@
 from sklearn.datasets import make_classification
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-X,y=make_classification(random_state=0)
-plt.scatter(X[:,0],X[:,1],marker='o',c=y,edgecolors='k')
-plt.show()
+X=np.array([[0.33],[0.72],[0.1],[0.01],[0.7]])
+y=np.array([0,1,0,0,1])
+
+model=LinearRegression()
+model.fit(X,y)
+predictions=model.predict(X)
+print(predictions)
