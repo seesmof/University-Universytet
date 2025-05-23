ba26fd6fc2596f521fbb2da17da7555bd168da8a
Oleh Onyshchenko <seesmwork@gmail.com>
2025-05-23 08:14:24 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/1/a.py
+++ 32SS Systemy shtuchnogo/1/a.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,15 +19,15 @@ needed='Name,Age,Survived'.split(',')
 df=df[needed].dropna()
 le=LabelEncoder()
 
-target=df.Survived
+target=df.Survived.to_numpy()
 df=df.drop(['Survived'],axis=1)
 df=df.drop(['Name'],axis=1)
 
-train,test=train_test_split(df,test_size=0.3)
+X_train,X_test,y_train,y_test=train_test_split(df,target,test_size=0.3)
 model=KMeans(n_clusters=3)
-model.fit(train)
+model.fit(X_train)
 print(f'{model.cluster_centers_ = }')
-prediction_results=model.predict(test)
+prediction_results=model.predict(X_test)
 print(f'{prediction_results = }')
 
 # 1.2 Вибірка
@@ -40,6 +41,7 @@ twos_range=range(2,size,4)
 threes_range=range(3,size,4)
 
 input_array=[]
+target_array=[]
 for data_row in range(1,size):
     row=[]
     for feature_col in range(1,number_of_features):
@@ -51,11 +53,39 @@ for data_row in range(1,size):
         elif feature_col in threes_range:
             result=feature_col*random.random()
         row.append(result)
+
+    if row[0]**2+row[1]**2 < variant**2+0.04*size**2: target_array.append(0)
+    elif row[0]**2+row[1]**2 >= variant**2+0.04*size**2: target_array.append(1)
+
     input_array.append(row)
-output_array=np.array(input_array)
-print(f'{output_array = }')
+features=np.array(input_array)
+targets=np.array(target_array)
+print(f'{features = }')
+print(f'{targets = }')
 
 # 1.3 Нормування
 
-normalized_array=normalize(output_array)
-print(f'{normalized_array = }')
+normalized_features=normalize(features)
+print(f'{normalized_features = }')
+
+# 1.4 Побудова моделі
+
+features_train,features_test,targets_train,targets_test=train_test_split(normalized_features,targets)
+
+start_time=time.perf_counter()
+model=KMeans(n_clusters=number_of_features)
+model.fit(features_train)
+end_time=time.perf_counter()
+learning_time=end_time-start_time
+print(f'{learning_time = }')
+
+# 1.5 Розпізнавання
+
+start_time=time.perf_counter()
+result=model.predict(features_test)
+print(f'{result = }')
+end_time=time.perf_counter()
+prediction_time=end_time-start_time
+print(f'{prediction_time = }')
+
+print(model.score(features_test))
