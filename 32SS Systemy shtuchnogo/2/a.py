491ec1447e9aacd6a6dbade05ffc49e2b285f16f
Oleh Onyshchenko <seesmwork@gmail.com>
2025-06-08 14:55:36 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/2/a.py
+++ 32SS Systemy shtuchnogo/2/a.py
@@ -1,4 +1,16 @@
+from matplotlib import pyplot as plt
+import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
-iris=load_iris()
-print(iris)
+data,target,target_names=load_iris()['data'],load_iris()['target'],load_iris()['target_names']
+
+sepal_lengths=data[:,np.array([True,False,False,False])]
+sepal_widths=data[:,np.array([False,True,False,False])]
+petal_lenghts=data[:,np.array([False,False,True,False])]
+petal_widths=data[:,np.array([False,False,False,True])]
+
+plt.scatter(sepal_lengths,sepal_widths,c=target,cmap='berlin')
+
+plt.legend(target_names)
+plt.show()
--- 32SS Systemy shtuchnogo/2/test.py
+++ 32SS Systemy shtuchnogo/2/test.py
@@ -1,7 +1,19 @@
-import matplotlib.pyplot as plt
+# importing modules
 import numpy as np
+import matplotlib.pyplot as plt
 
-hor=np.array(['KNeighbor','Forest'])
-ver=np.array([20,12])
-plt.bar(hor,ver)
+# Y-axis values
+y1 = [2, 3, 4.5]
+
+# Y-axis values
+y2 = [1, 1.5, 5]
+
+# Function to plot
+plt.plot(y1)
+plt.plot(y2)
+
+# Function add a legend
+plt.legend(["blue", "green"], loc="lower right")
+
+# function to show the plot
 plt.show()
