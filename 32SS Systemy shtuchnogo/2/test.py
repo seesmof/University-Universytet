e7046caf6745e18a835e05318ed0b86c84225f60
Oleh Onyshchenko <seesmwork@gmail.com>
2025-06-11 18:25:11 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/2/test.py
+++ 32SS Systemy shtuchnogo/2/test.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-import pandas as pd
 from sklearn.datasets import load_iris
 
 iris=load_iris()
@@ -8,9 +7,8 @@ pairs=[(i,j) for i in range(len(iris['feature_names'])) for j in range(i+1,len(i
 
 for i,(f1,f2) in enumerate(pairs):
     fig=plt.figure(figsize=(8,5),tight_layout=True)
-    plot=plt.scatter(X[:,f1],X[:,f2],cmap="viridis",c=iris['target'])
+    plot=plt.scatter(X[:,f1],X[:,f2],cmap="berlin",c=iris['target'])
     plt.xlabel(iris['feature_names'][f1])
     plt.ylabel(iris['feature_names'][f2])
-    # Colors to legend: https://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html#:~:text=iris.target_names.tolist()%2C
     fig.legend(plot.legend_elements()[0], iris.target_names.tolist())
     plt.show()
