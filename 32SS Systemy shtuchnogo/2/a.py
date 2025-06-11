a18063fd2f8f1948f10bf56b28663217c6a65917
Oleh Onyshchenko <seesmwork@gmail.com>
2025-06-11 12:15:38 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/2/a.py
+++ 32SS Systemy shtuchnogo/2/a.py
@@ -3,18 +3,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 
 iris=load_iris()
-data=pd.DataFrame(iris.data,columns=iris.feature_names)
-data['species']=pd.Categorical.from_codes(iris.target,iris.target_names)
+X=iris['data']
+df=pd.DataFrame(X,columns=iris.feature_names)
+print(df)
 
-features=iris.feature_names
-feature_pairs=[(i,j) for i in range(len(features)) for j in range(i+1,len(features))]
-
-fig,axes=plt.subplots(2,3,figsize=(20,12),tight_layout=True)
-for ax,(feature_one,feature_two) in zip(axes.flatten(),feature_pairs):
-    for species in data['species'].unique():
-        subset=data[data['species']==species]
-        ax.scatter(subset[features[feature_one]],subset[features[feature_two]],label=species,alpha=0.8)
-        ax.set_xlabel(features[feature_one])
-        ax.set_ylabel(features[feature_two])
-fig.legend(data['species'].unique(),loc='upper center',ncol=3,fontsize='medium')
-plt.show()
