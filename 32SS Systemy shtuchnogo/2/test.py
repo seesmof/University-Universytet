bf6b958f5aaf79444001be644568493586b39b14
Oleh Onyshchenko <seesmwork@gmail.com>
2025-06-18 11:56:10 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/2/test.py
+++ 32SS Systemy shtuchnogo/2/test.py
@@ -3,6 +3,8 @@ import pprint
 import random
 from matplotlib import pyplot as plt
 from pandas import DataFrame
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
 
 names = [
     "Liam",
@@ -31,6 +33,16 @@ data = [
 pprint.pprint(data)
 
 df = DataFrame(data, columns=Instance._fields)
+y = df["chosen_car"]
+df = df.drop(["chosen_car", "name"], axis=1)
 print(df.head())
-df.plot(kind="scatter", x="age", y="chosen_car")
+
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.3)
+
+model = LinearRegression()
+trainings = model.fit(X_train, y_train)
+results = model.predict(X_test)
+print(results)
+
+plt.scatter(df.age, results)
 plt.show()
