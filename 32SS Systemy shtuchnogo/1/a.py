0a8e1f2f91b7a47304ad67dde919eb177cb273fd
Oleh Onyshchenko <seesmwork@gmail.com>
2025-06-20 12:30:04 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/1/a.py
+++ 32SS Systemy shtuchnogo/1/a.py
@@ -1,14 +1,39 @@
+from math import inf
+import pprint
+import random
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.preprocessing import Normalizer
 
 
-d = [150, 190, 130, 160, 155, 158, 163]
-d = sorted(d)
-d = np.array(d)
-print(d)
+def get_min(arr):
+    min_so_far = 1000 * 1000
+    for element in arr:
+        if element < min_so_far:
+            min_so_far = element
+    return min_so_far
 
-plt.plot(d, "o")
-plt.show()
+
+def get_max(arr):
+    max_so_far = -1000 * 1000
+    for element in arr:
+        if element > max_so_far:
+            max_so_far = element
+    return max_so_far
+
+
+data = [[random.randint(150, 170) for _ in range(10)] for _ in range(1)]
+for i, row in enumerate(data):
+    low = get_min(row) - 30
+    high = get_max(row) + 30
+    data[i] = sorted([low] + row + [high])
+data = np.array(data)
+print(data)
+
+print()
+for i, row in enumerate(data):
+    print(sum(row))
 
 scaler = Normalizer(norm="max")
+normalized_data = scaler.fit_transform(data)
+print(normalized_data)
