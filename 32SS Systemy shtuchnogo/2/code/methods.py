0c50fda879983c0459143a7c645f828b5c892747
Oleh Onyshchenko <seesmwork@gmail.com>
2025-08-05 22:25:00 +0300

    Click to see changes

--- 32SS Systemy shtuchnogo/2/code/methods.py
+++ 32SS Systemy shtuchnogo/2/code/methods.py
@@ -1,6 +1,6 @@
 # Кількість кластерів та ознак
-CLUSTERS=4
-FEATURES=4
+CLUSTERS = 3
+FEATURES = 4
 
 from matplotlib import pyplot as plt
 from sklearn.cluster import AgglomerativeClustering, KMeans
@@ -8,33 +8,34 @@ from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
-cluster_names=[f'Cluster {number}' for number in range(CLUSTERS)]
+cluster_names = [f"Cluster {number}" for number in range(CLUSTERS)]
 
-iris=load_iris()
-X=iris.data
-X_reduced=PCA(n_components=FEATURES).fit_transform(X)
+iris = load_iris()
+X = iris.data
+X_reduced = PCA(n_components=FEATURES).fit_transform(X)
 print(X_reduced)
-y=iris.target
+y = iris.target
 # Розбиття вибірки на тестову та тренувальну
-X_train,X_test,y_train,y_test=train_test_split(X_reduced,y)
+X_train, X_test, y_train, y_test = train_test_split(X_reduced, y)
 
-kmeans=KMeans(n_clusters=CLUSTERS)
-kmeans_results=kmeans.fit_predict(X_test)
-# Оцінка похибки методу
-kmeans_score=kmeans.score(X_test)/len(X_test)
+kmeans = KMeans(n_clusters=CLUSTERS)
+kmeans_results = kmeans.fit_predict(X_test)
+print(f"{kmeans.cluster_centers_ = }")
+kmeans_score = kmeans.score(X_test) / len(X_test)
 
-agglomerative=AgglomerativeClustering(n_clusters=CLUSTERS)
-agglomerative_results=agglomerative.fit_predict(X_test)
-# Кількість розбіжностей між результатами двох методів
-agglomerative_score=sum([agglomerative_results[i]!=kmeans_results[i] for i in range(len(kmeans_results))])
+agglomerative = AgglomerativeClustering(n_clusters=CLUSTERS)
+agglomerative_results = agglomerative.fit_predict(X_test)
+agglomerative_score = sum(
+    [agglomerative_results[i] != kmeans_results[i] for i in range(len(kmeans_results))]
+)
 
-print(f'\n{kmeans_results = }')
-print(f'K-Means clustering accuracy: {kmeans_score}')
-print(f'\n{agglomerative_results = }')
-print(f'Hierarchical clustering results difference: {agglomerative_score}')
+print(f"\n{kmeans_results = }")
+print(f"K-Means clustering accuracy: {kmeans_score}")
+print(f"\n{agglomerative_results = }")
+print(f"Hierarchical clustering results difference: {agglomerative_score}")
 
-fig,ax=plt.subplots(nrows=1,ncols=2,figsize=(10,5),tight_layout=True)
-for axis,result in zip(ax,[kmeans_results,agglomerative_results]):
-    plot=axis.scatter(X_test[:,0],X_test[:,1],c=result,cmap='viridis')
-fig.legend(plot.legend_elements()[0], cluster_names, loc='upper left')
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), tight_layout=True)
+for axis, result in zip(ax, [kmeans_results, agglomerative_results]):
+    plot = axis.scatter(X_test[:, 0], X_test[:, 1], c=result, cmap="viridis")
+fig.legend(plot.legend_elements()[0], cluster_names, loc="upper left")
 plt.show()
