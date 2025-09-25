4b16d3d7d66425450e33db1d99c6eefa7f2f19f4
Oleh Onyshchenko <seesmwork@gmail.com>
2025-09-25 22:28:15 +0300

    Click to see changes

---
+++ 41BZ Bezpeka ta zahyst program/1/vigenere-svelte/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ 41BZ Bezpeka ta zahyst program/1/vigenere-svelte/src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  let count: number = $state(0)
+</script>
+
+<button onclick={() => count++}>
+  count is {count}
+</button>
+<p transition:fade>Jesus Christ is the LORD</p>
