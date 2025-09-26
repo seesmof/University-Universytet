029854a42a30e0bf8a6b95bca584e0515992e60f
Oleh Onyshchenko <seesmwork@gmail.com>
2025-09-26 13:32:59 +0300

    Click to see changes

---
+++ 41BZ Bezpeka ta zahyst program/vigenere/lib/vigenere.ts
@@ -0,0 +1,23 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export function vigenereEncrypt(message: string, key: string): string {
+  let result = "";
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i];
+    const keyChar = key[i % key.length];
+
+    const charIndex = alphabet.indexOf(char);
+    const keyIndex = alphabet.indexOf(keyChar);
+
+    if (charIndex === -1 || keyIndex === -1) {
+      result += char;
+      continue;
+    }
+
+    const encryptedIndex = (charIndex + keyIndex) % alphabet.length;
+    const encryptedChar = alphabet[encryptedIndex];
+    result += encryptedChar;
+  }
+
+  return result;
+}
