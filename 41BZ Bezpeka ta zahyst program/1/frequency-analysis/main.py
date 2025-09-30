1a3f2d7ed834bbd45c48893b3ec2dcc7c2a16da7
Oleh Onyshchenko <seesmwork@gmail.com>
2025-09-30 20:24:07 +0300

    Click to see changes

--- 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
+++ 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
@@ -7,28 +7,54 @@ REGULAR = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
 org_text = "І повідкидав Йосія всю гидоту з усїх земель, що в синів Ізрайлевих; і приказав усїм, що знаходились в Ізраїлї, служити Господеві, Богові свойму. І за ввесь час живоття його не одступали вони від Господа, Бога отцїв своїх."
 cph_text = "И мннжґщєґля Шлрцщ ноь азрлсд ж ррчт фглсїщ, ша б озяжб Жжвьїьгбхт; ж бнзщьжля доію, іл фкяєлґхїзгч н Ифнячїі, рьрєхпз Ангмнргбц, Млваяи обаилд. Ц жл бнгрї цло уєбапск їаан кд лґгптбькх бакз яир Ваооавя, Ааая лсжзб обазф."
 
-clean_text = cph_text.upper().replace(" ", "")
-common_letters = [
-    letter for letter, number in collections.Counter(clean_text).most_common()
-]
-cut_alphabet = list(FREQUENT[: len(common_letters)])
-data = dict(zip(common_letters, cut_alphabet))
-print(f"{common_letters = }")
-print(f"{cut_alphabet = }")
-print(f"{data = }")
 
-new_text = str()
-for index, letter in enumerate(cph_text):
-    if letter.isalpha() and letter.upper() in data.keys():
-        new_letter = data[letter.upper()]
-        new_letter = new_letter if letter == letter.upper() else new_letter.lower()
-    else:
-        new_letter = letter
-    new_text += new_letter
-    print(f"{org_text[index]} was {letter} and is {new_letter}")
-print(new_text)
+def frequency_analysis(text: str, original_text: str = "", log_process: bool = False):
+    clean_text = "".join(letter.upper() for letter in text if letter.isalpha())
+    common_letters = [
+        letter for letter, _ in collections.Counter(clean_text).most_common()
+    ]
 
-error_amount = sum(
-    letter != org_letter for letter, org_letter in zip(new_text, org_text)
-) / len(org_text)
-print(error_amount)
+    """
+    Співставляє найбільш використані літери наданого тексту (text)
+    з найбільш популярними літерами алфавіту (FREQUENT)
+    """
+    mapping = dict(zip(common_letters, FREQUENT))
+
+    new_text = str()
+    for index, letter in enumerate(text):
+        if letter.isalpha() and letter.upper() in mapping:
+            new_letter = mapping[letter.upper()]
+            new_letter = new_letter if letter == letter.upper() else new_letter.lower()
+        else:
+            new_letter = letter
+        new_text += new_letter
+
+        if log_process:
+            try:
+                print(f"{original_text[index]} was {letter} and is {new_letter}")
+            except IndexError:
+                print(f"{letter} is {new_letter}")
+
+    return new_text
+
+
+def calculate_error(deciphered_text: str, original_text: str):
+    clean_deciphered = [letter for letter in deciphered_text if letter.isalpha()]
+    clean_original = [letter for letter in original_text if letter.isalpha()]
+
+    if not clean_original:
+        return 0.0
+
+    mismatches = sum(d != o for d, o in zip(clean_deciphered, clean_original))
+    return mismatches / len(clean_original)
+
+
+original_text = "Це текст"
+print(original_text)
+
+given_text = "Їф шхуяш"
+deciphered_text = frequency_analysis(given_text, original_text, True)
+print(deciphered_text)
+
+error = calculate_error(deciphered_text, original_text)
+print(error)
---
+++ 41PR Teorija pryjnattja rishen/C/topic.md
@@ -0,0 +1,8 @@
+GLAUCOMA DIAGNOSTICS BY MULTI WAVELET TRANSFORM
+
+whats glaucoma
+how to diagnose them 
+what is multi wavelet transform 
+how to apply it 
+
+Dubrovin Petunin. Glaucoma diagnostic using machine learning methods
