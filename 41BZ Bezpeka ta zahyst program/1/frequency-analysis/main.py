913553ce587fbb6fd2148c9a80700e83b3793653
Oleh Onyshchenko <seesmwork@gmail.com>
2025-09-30 18:52:41 +0300

    Click to see changes

--- 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
+++ 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
@@ -1,60 +1,28 @@
 import collections
 
 
-cipher_text = "Игрр Тпхосао ОЛРБЛҐЇ"
+org_text = "І повідкидав Йосія всю гидоту з усїх земель, що в синів Ізрайлевих; і приказав усїм, що знаходились в Ізраїлї, служити Господеві, Богові свойму. І за ввесь час живоття його не одступали вони від Господа, Бога отцїв своїх."
+cph_text = "И мннжґщєґля Шлрцщ ноь азрлсд ж ррчт фглсїщ, ша б озяжб Жжвьїьгбхт; ж бнзщьжля доію, іл фкяєлґхїзгч н Ифнячїі, рьрєхпз Ангмнргбц, Млваяи обаилд. Ц жл бнгрї цло уєбапск їаан кд лґгптбькх бакз яир Ваооавя, Ааая лсжзб обазф."
 frequent_alphabet = "ОАИВНЕТСІРДЛМУПКГБЇЙЗЯЬХЖЧЮШЩЦЄФҐ"
 regular_alphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
 
-"""
-code taken from:
+clean_text = cph_text.upper().replace(" ", "")
+common_letters = [
+    letter for letter, number in collections.Counter(clean_text).most_common()
+]
+cut_alphabet = list(frequent_alphabet[: len(common_letters)])
+data = dict(zip(common_letters, cut_alphabet))
+print(f"{common_letters = }")
+print(f"{cut_alphabet = }")
+print(f"{data = }")
 
-https://stackoverflow.com/questions/29797475/python-frequency-analysis
-"""
-
-
-def generate_caesar_key(offset):
-    dictionary = {}
-    index = 0
-    for letter in regular_alphabet:
-        if index + offset >= len(regular_alphabet):
-            index -= len(regular_alphabet)
-        dictionary.update({letter: regular_alphabet[index + offset]})
-        index += 1
-    return dictionary
-
-
-def switch_encode(string, key):
-    encoded = ""
-    for letter in string.lower():
-        if letter in key.keys():
-            encoded += key[letter]
-        else:
-            encoded += letter
-    return encoded
-
-
-def switch_decode(string, key):
-    key = dict(zip(key.values(), key.keys()))
-    decoded = ""
-    for letter in string.lower():
-        if letter in key.keys():
-            decoded += key[letter]
-        else:
-            decoded += letter
-    return decoded
-
-
-def switch_crack(string):
-    key = {}
-    frequent_letters = collections.Counter(string).most_common()
-    index = 0
-    for letter in frequent_letters:
-        if letter[0] in regular_alphabet:
-            key[frequent_alphabet[index]] = letter[0]
-            index += 1
-    return key
-
-
-key = generate_caesar_key(3)
-res = switch_decode(cipher_text, key)
-print(res)
+new_text = str()
+for letter in cph_text:
+    if letter.upper() in data:
+        new_letter = data[letter.upper()]
+        new_letter = new_letter if letter == letter.upper() else new_letter.lower()
+        new_text += new_letter
+    else:
+        new_text += letter
+    print(letter, new_letter)
+print(new_text)
