ae7b44f65c0e630bfb524e8519a4b688860d0728
Oleh Onyshchenko <seesmwork@gmail.com>
2025-09-30 19:05:48 +0300

    Click to see changes

--- 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
+++ 41BZ Bezpeka ta zahyst program/1/frequency-analysis/main.py
@@ -1,28 +1,34 @@
 import collections
 
+FREQUENT = "ОАИВНЕТСІРДЛМУПКГБЇЙЗЯЬХЖЧЮШЩЦЄФҐ"
+REGULAR = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+
 
 org_text = "І повідкидав Йосія всю гидоту з усїх земель, що в синів Ізрайлевих; і приказав усїм, що знаходились в Ізраїлї, служити Господеві, Богові свойму. І за ввесь час живоття його не одступали вони від Господа, Бога отцїв своїх."
 cph_text = "И мннжґщєґля Шлрцщ ноь азрлсд ж ррчт фглсїщ, ша б озяжб Жжвьїьгбхт; ж бнзщьжля доію, іл фкяєлґхїзгч н Ифнячїі, рьрєхпз Ангмнргбц, Млваяи обаилд. Ц жл бнгрї цло уєбапск їаан кд лґгптбькх бакз яир Ваооавя, Ааая лсжзб обазф."
-frequent_alphabet = "ОАИВНЕТСІРДЛМУПКГБЇЙЗЯЬХЖЧЮШЩЦЄФҐ"
-regular_alphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
 
 clean_text = cph_text.upper().replace(" ", "")
 common_letters = [
     letter for letter, number in collections.Counter(clean_text).most_common()
 ]
-cut_alphabet = list(frequent_alphabet[: len(common_letters)])
+cut_alphabet = list(FREQUENT[: len(common_letters)])
 data = dict(zip(common_letters, cut_alphabet))
 print(f"{common_letters = }")
 print(f"{cut_alphabet = }")
 print(f"{data = }")
 
 new_text = str()
-for letter in cph_text:
-    if letter.upper() in data:
+for index, letter in enumerate(cph_text):
+    if letter.isalpha() and letter.upper() in data.keys():
         new_letter = data[letter.upper()]
         new_letter = new_letter if letter == letter.upper() else new_letter.lower()
-        new_text += new_letter
     else:
-        new_text += letter
-    print(letter, new_letter)
+        new_letter = letter
+    new_text += new_letter
+    print(f"{org_text[index]} was {letter} and is {new_letter}")
 print(new_text)
+
+error_amount = sum(
+    letter != org_letter for letter, org_letter in zip(new_text, org_text)
+) / len(org_text)
+print(error_amount)
