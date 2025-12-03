408918db6e9454271794b2446a6c2daa9867ab3b
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-03 20:21:04 +0200

    start IA2

---
+++ 41IA Intelektualnyj analiz danyh/2/main.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Load data
+root_folder = os.path.dirname(os.path.abspath(__file__))
+file_name = "breast-cancer.csv"
+file_path = os.path.join(root_folder, file_name)
+
+# Print table
+data = pd.read_csv(file_path, na_values=["?"])
+print(data.head())
+print()
+
+# Fill missing values
+data["node-caps"] = data["node-caps"].fillna(data["node-caps"].mode())
+print(data.head())
+
+# Replace categorical with numbers
+data["node-caps"].replace({"yes": 1, "no": 0}, inplace=True)
+data["breast"].replace({"left": 1, "right": 0}, inplace=True)
+data["irradiat"].replace({"yes": 1, "no": 0}, inplace=True)
+print(data.head())
+
+# pie, hist, scatter, bar
