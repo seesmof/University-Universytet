ef1a472bff3b1ca4810f4661423b53652e38c5dc
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-06 18:36:29 +0200

    simple python api

--- 41BZ Bezpeka ta zahyst program/K/a.py
+++ 41BZ Bezpeka ta zahyst program/K/a.py
@@ -1,27 +1,38 @@
 ASCII_LENGTH = 128
 
-message = "This, it is glorious!"
+
+class Mode:
+    ENCRYPT = "encrypt"
+    DECRYPT = "decrypt"
+
+
+def vigenere(message: str, key: str, mode: str = "encrypt") -> str:
+    # Make key as long as the message is
+    key_stream = key * len(message)
+    # Truncate it, so that it does not exceed the message length
+    long_key = key_stream[: len(message)]
+
+    result: str = str()
+    for index, _ in enumerate(message):
+        key_ascii = ord(long_key[index])
+        message_ascii = ord(message[index])
+
+        letter_ascii = 0
+        if mode == Mode.ENCRYPT:
+            letter_ascii = (message_ascii + key_ascii) % ASCII_LENGTH
+        elif mode == Mode.DECRYPT:
+            letter_ascii = (message_ascii - key_ascii) % ASCII_LENGTH
+        else:
+            raise ValueError("Invalid mode...")
+
+        new_letter = chr(letter_ascii)
+        result += new_letter
+
+    return result
+
+
 key = "G0D"
-
-# Make key as long as the message is
-key_stream = key * len(message)
-# Truncate it, so that it does not exceed message length
-long_key = key_stream[: len(message)]
-
-cipher_text: str = str()
-for index, message_char in enumerate(message):
-    key_ascii_value = ord(long_key[index])
-    message_ascii_value = ord(message[index])
-
-    new_letter_ascii = (message_ascii_value + key_ascii_value) % ASCII_LENGTH
-    new_letter = chr(new_letter_ascii)
-    cipher_text += new_letter
-
-deciphered_text: str = str()
-for index, cipher_letter in enumerate(cipher_text):
-    key_ascii_value = ord(long_key[index])
-    cipher_ascii_value = ord(cipher_text[index])
-
-    original_letter_ascii = (cipher_ascii_value - key_ascii_value) % ASCII_LENGTH
-    original_letter = chr(original_letter_ascii)
-    deciphered_text += original_letter
+encrypted = vigenere("This, some message!", key, Mode.ENCRYPT)
+decrypted = vigenere(encrypted, key, Mode.DECRYPT)
+print(encrypted)
+print(decrypted)
