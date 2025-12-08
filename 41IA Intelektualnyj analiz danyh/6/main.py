310aacfa0e900ce9040fbc8ebd5c080f0ee184a4
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-08 10:39:10 +0200

    IA6 some kmeans ;(

--- 41IA Intelektualnyj analiz danyh/6/main.py
+++ 41IA Intelektualnyj analiz danyh/6/main.py
@@ -1,30 +1,40 @@
 import os
 import pandas as pd
-from mlxtend.frequent_patterns import apriori, association_rules, fpgrowth
+
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import contingency_matrix
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_name = "marketbasket.csv"
+file_name = "vehicle.csv"
 file_path = os.path.join(current_dir, file_name)
 
 df = pd.read_csv(file_path)
-df = df.fillna(0)
-for column in df.columns:
-    df[column] = df[column].astype("bool")
-print(df.head())
+df = df.dropna()
 
-# --- Apriori ---
-apriori_result = apriori(df, min_support=0.01, use_colnames=True)
-print(apriori_result)
+TARGET_FEATURE = "Class"
+Y = df[TARGET_FEATURE]
+X = df.drop([TARGET_FEATURE], axis=1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33)
 
-RULES_COUNT = 3
-rules = association_rules(apriori_result, metric="confidence", min_threshold=0.1)
-sorted_rules = rules.sort_values("confidence", ascending=False)
-results_matrix = rules[["antecedents", "consequents", "confidence"]]
-print(results_matrix.head(RULES_COUNT))
+KM = KMeans(n_clusters=len(Y_train))
+KM.fit(X_train)
 
-# --- FP-Growth ---
-fpgrowth_result = fpgrowth(df, min_support=0.01, use_colnames=True)
-rules = association_rules(fpgrowth_result, metric="confidence", min_threshold=0.1)
-sorted_rules = rules.sort_values("confidence", ascending=False)
-results_matrix = rules[["antecedents", "consequents", "confidence"]]
-print(results_matrix.head(7))
+cluster_centers = pd.DataFrame(KM.cluster_centers_)
+cluster_centers.columns = X.columns
+print(cluster_centers)
+
+Y_predict = KM.predict(X_test)
+matrix = pd.DataFrame(contingency_matrix(Y_test, Y_predict))
+matrix.index = Y.unique()
+print(matrix)
+
+right_cluster_count = 0
+for column in matrix.columns:
+    right_cluster_count += matrix[column].max()
+    accuracy = right_cluster_count / matrix.sum().sum()
+    error_rate = 1 - accuracy
+
+print(f"{right_cluster_count=}")
+print(f"{accuracy=}")
+print(f"{error_rate=}")
