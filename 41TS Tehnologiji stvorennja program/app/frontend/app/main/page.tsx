4c42a6a546cf13e8a1260abd728c81079be97464
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-16 16:23:33 +0200

    TS not sure on what framework to use

--- 41TS Tehnologiji stvorennja program/app/frontend/app/main/page.tsx
+++ 41TS Tehnologiji stvorennja program/app/frontend/app/main/page.tsx
@@ -1,67 +1,9 @@
 "use client";
 
+import { products } from "@/data/products";
 import Image from "next/image";
 
-// Sushi type
-type Sushi = {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-};
-
 export default function Main() {
-  const products: Sushi[] = [
-    {
-      id: 1,
-      name: "Nuggets Bowl",
-      price: 10,
-      image: "bowl-nuggets.jpg",
-    },
-    {
-      id: 2,
-      name: "Potato Bowl",
-      price: 10,
-      image: "bowl-potato.jpg",
-    },
-    {
-      id: 3,
-      name: "Rice Bowl",
-      price: 10,
-      image: "bowl-two.jpg",
-    },
-    {
-      id: 4,
-      name: "Nuggets Plate",
-      price: 8,
-      image: "nuggets.jpg",
-    },
-    {
-      id: 5,
-      name: "Sticks Plate",
-      price: 8,
-      image: "sticks.jpg",
-    },
-    {
-      id: 6,
-      name: "Sushi Coral",
-      price: 12,
-      image: "sushi-coral.jpg",
-    },
-    {
-      id: 7,
-      name: "Sushi Green",
-      price: 12,
-      image: "sushi-green.jpg",
-    },
-    {
-      id: 8,
-      name: "Sushi White",
-      price: 12,
-      image: "sushi-white.jpg",
-    },
-  ];
-
   return (
     <>
       <div className="flex flex-row flex-wrap gap-3 p-3">
@@ -79,9 +21,13 @@ export default function Main() {
               <p className="">${product.price}</p>
               <button
                 className="w-min bg-sky-500 text-sky-50 rounded-md px-3 py-1 cursor-pointer"
-                onClick={() =>
-                  sessionStorage.setItem("product", product.id.toString())
-                }
+                onClick={() => {
+                  sessionStorage.setItem("cart", [
+                    ...sessionStorage.getItem("cart"),
+                    product.id,
+                  ]);
+                  console.log(product.name);
+                }}
               >
                 +
               </button>
