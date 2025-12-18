983a373d3b6b3538cfa7f7ab6864a3bd916678c8
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-18 21:49:43 +0200

    TS5 add verse references

--- 41TS Tehnologiji stvorennja program/app/backend/frontend/views.py
+++ 41TS Tehnologiji stvorennja program/app/backend/frontend/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
 import requests
 
-"""
-def index(request):
-    base_url = f"https://bolls.life/v2/find/UBIO?search=Гріх"
-    try:
-        response = requests.get(base_url)
-        data = response.json()
-        print(data)
-
-        context = {"verses": data}
-        return render(request, "index.html", context)
-
-    except requests.exceptions.RequestException as e:
-        error_message = f"Failed fetching from Bolls.Life, {e}"
-        context = {"error": error_message}
-        return render(request, "error.hmtl", context)
-"""
+from .consts import BIBLE_BOOK_NUMBER_TO_HOMENKO_BIBLE_NAME
 
 
 def index(request):
@@ -26,7 +11,14 @@ def index(request):
         base_url = f"https://bolls.life/v2/find/UBIO?search={text}&limit=10"
         response = requests.get(base_url)
         data = response.json()["results"]
-        print(data)
+
+        # Add a readable verse reference to each verse:
+        for verse in data:
+            Book_number = verse["book"]
+            Book_name = BIBLE_BOOK_NUMBER_TO_HOMENKO_BIBLE_NAME[Book_number]
+
+            verse["reference"] = f"{Book_name} {verse['chapter']}:{verse['verse']}"
+            print(verse["reference"])
 
         context = {"verses": data, "request": text}
         return render(request, "verses.html", context)
