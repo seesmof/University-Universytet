a02508a8095d3e0d42f41e3a86c82e738d3d47b3
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-18 18:29:38 +0200

    remove all but nextjs TS5

--- 41TS Tehnologiji stvorennja program/app/frontend/data/cart.ts
+++
@@ -1,4 +0,0 @@
-import { Product } from "./products";
-
-export let cartProducts: Product[] = [];
-export let orders: Product[] = [];
--- 41TS Tehnologiji stvorennja program/app/frontend/data/user.ts
+++
@@ -1,3 +0,0 @@
-export let loggedIn: boolean = false;
-export let userName: string = "";
-export let userPass: string = "";
