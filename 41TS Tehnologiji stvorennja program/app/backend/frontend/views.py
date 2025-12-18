5632cb265647ddebe7d346291b9f3bf9d17d0ee2
Oleg Onyshchenko <seesmwork@gmail.com>
2025-12-18 19:55:24 +0200

    soemthing confusing

--- 41TS Tehnologiji stvorennja program/app/backend/frontend/views.py
+++ 41TS Tehnologiji stvorennja program/app/backend/frontend/views.py
@@ -1,7 +1,35 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+import requests
 
-
-# Create your views here.
+"""
 def index(request):
-    return render(request, "index.html")
+    base_url = f"https://bolls.life/v2/find/UBIO?search=Гріх"
+    try:
+        response = requests.get(base_url)
+        data = response.json()
+        print(data)
+
+        context = {"verses": data}
+        return render(request, "index.html", context)
+
+    except requests.exceptions.RequestException as e:
+        error_message = f"Failed fetching from Bolls.Life, {e}"
+        context = {"error": error_message}
+        return render(request, "error.hmtl", context)
+"""
+
+
+def index(request):
+    if request.method == "POST":
+        text = request.POST.get("search")
+
+        base_url = f"https://bolls.life/v2/find/UBIO?search={text}&limit=10"
+        response = requests.get(base_url)
+        data = response.json()
+        print(data)
+
+        context = {"verses": data}
+        return render(request, "verses.html", context)
+
+    else:
+        return render(request, "index.html", {})
