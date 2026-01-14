eeecd298e810b5dccb019df2595d00d6ae6f14ec
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-14 13:58:24 +0200

    diploma working

--- 42D Dyplom/frontend/app/page.tsx
+++ 42D Dyplom/frontend/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Field() {
-  return <div className="max-w-3xl mx-auto bg-cyan-500 p-3"></div>;
+  return (
+    <>
+      <main className="max-w-3xl flex flex-col gap-2 p-2">
+        <nav className="justify-between p-2 border">
+          <h1>Trucks</h1>
+        </nav>
+        <section className="border p-2"></section>
+      </main>
+    </>
+  );
 }
