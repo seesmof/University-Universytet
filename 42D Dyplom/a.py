2194922e850a93863f8538c931c6f175eaa48217
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-29 10:11:43 +0200

    feat: create tables and fill in trucks, diploma

--- 42D Dyplom/a.py
+++ 42D Dyplom/a.py
@@ -1,2 +1,44 @@
-from tortoise.models import Model
-from tortoise import fields
+import os
+from narwhals import Duration
+from peewee import *
+import datetime
+
+db_name = "database.db"
+db = SqliteDatabase(db_name)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Truck(BaseModel):
+    name = CharField()
+    category = CharField()
+    price = IntegerField()
+    picture = CharField()
+
+
+class Loan(BaseModel):
+    amount = IntegerField()
+    duration = IntegerField()
+    bank = CharField()
+
+
+class Order(BaseModel):
+    price = IntegerField()
+    location = CharField()
+    coordinates = CharField()
+
+
+db.connect()
+db.create_tables([Order, Truck, Loan])
+
+file_name: str = "trucks.csv"
+file_path: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+with open(file_path, encoding="utf-8", mode="r") as f:
+    lines = f.readlines()
+for line in lines:
+    data = line.strip().split(",")
+    truck = Truck.create(name=data[0], category=data[1], price=data[2], picture=data[3])
+    print(truck.name)
