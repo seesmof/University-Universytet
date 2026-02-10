4b9b98bd636cf9bdeec400e88ab66ee6fca4346c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-10 11:42:24 +0200

    feat (TR2): add all graphs, now gonna be doing the comparison table

--- 42TR Tehnologiji Rozpodilenyh System/2/binary-power.py
+++ 42TR Tehnologiji Rozpodilenyh System/2/binary-power.py
@@ -1,3 +1,8 @@
-"""
-Реалізувати бінарний метод модульного піднесення до степені.
-"""
+import time
+
+start = time.perf_counter()
+
+print("Jesus is LORD")
+
+end = time.perf_counter()
+print(f"Time: {end - start:.6f}s")
