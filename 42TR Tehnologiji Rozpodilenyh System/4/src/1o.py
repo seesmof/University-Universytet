0b6514d0b56d56029d505e62b0407a3ac4dcc46b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-12 19:01:33 +0200

    feat - TR4: working, learning mpi4py

---
+++ 42TR Tehnologiji Rozpodilenyh System/4/src/1o.py
@@ -0,0 +1,46 @@
+import random
+import time
+from mpi4py import MPI
+import numpy as np
+
+N: int = 400000
+
+
+def main():
+    comm = MPI.COMM_WORLD
+    P = comm.Get_size()
+    myrank = comm.Get_rank()
+
+    x: list[float] = list()
+    a: list[float] = list()
+    M: float = 0.0
+    sum: float = 0.0
+    total: float = 0.0
+
+    if myrank == 0:
+        np.random.seed(int(time.time()))
+        x = (-1.073741824 + np.random.rand(N) * 1e-9).astype("f8")
+        a = ((-1.073741824 + np.random.rand(N) * 1e-9) * 0.1).astype("f8")
+        M = N / P
+
+    if myrank == 0:
+        start_time = time.perf_counter()
+    comm.bcast(M, root=1)
+    comm.scatter(x, M, 0)
+    comm.scatter(a, M, 0)
+
+    for i in range(M):
+        sum += a[i] * x[i]
+
+    comm.barrier()
+    comm.reduce([sum, total], MPI.SUM, root=0)
+
+    if myrank == 0:
+        end_time = time.perf_counter()
+        eval_time = end_time - start_time
+        print(f"time = {eval_time:.3f}")
+        print(f"sum in {P} processes: {total:.5f}")
+
+
+if __name__ == "__main__":
+    main()
