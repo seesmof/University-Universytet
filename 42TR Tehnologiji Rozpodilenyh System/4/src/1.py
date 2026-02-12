b5359160ecc6f4a203ca03de725717722ed90684
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-12 19:04:58 +0200

    feat - TR4: add a summing and average code

--- 42TR Tehnologiji Rozpodilenyh System/4/src/1.py
+++ 42TR Tehnologiji Rozpodilenyh System/4/src/1.py
@@ -1,16 +1,31 @@
+"""
+code taken from https://github.com/MolSSI-Education/parallel-programming/blob/main/examples/mpi4py/example2/example2.py
+"""
+
 from mpi4py import MPI
+import numpy as np
+
+N: int = 240000
 
 
 def main():
-    N: int = 240000
+    N: int = 10_000_000
+    a = np.ones(N)
+    b = np.zeros(N)
+    c: list[float] = list()
 
-    comm = MPI.COMM_WORLD
-    world_size = comm.Get_size()
-    rank = comm.Get_rank()
+    for i in range(N):
+        b[i] = 1.0 + i
 
-    print(f"{world_size=}")
-    print(f"{rank=}")
-    print()
+    for i in range(N):
+        c.append(a[i] + b[i])
+
+    sum: float = 0.0
+    average: float = 0
+    for i in range(N):
+        sum += c[i]
+    average = sum // N
+    print(f"{average=}")
 
 
 if __name__ == "__main__":
