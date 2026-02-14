4a8271dff2c762de04917f8f60014e510810387c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-14 14:31:50 +0200

    refactor -- TR4: move images to their own folder, trying to understand Scatter

--- 42TR Tehnologiji Rozpodilenyh System/4/src/t.py
+++ 42TR Tehnologiji Rozpodilenyh System/4/src/t.py
@@ -1,19 +1,40 @@
-import matplotlib.pyplot as plt
+from mpi4py import MPI
+import numpy as np
 
-x = [
-    20000,
-    40000,
-    60000,
-    80000,
-]
-y = [
-    0.00018030,
-    0.00014770,
-    0.00018110,
-    0.00032590,
-]
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
-plt.scatter(x, y, c="magenta", marker="o")
-# Або через plot для з'єднання лінією:
-plt.plot(x, y, "m-o")
-plt.show()
+N = 240_000
+chunk_size = N // size
+if rank == 0:
+    print(f"Chunk size: {chunk_size}\n")
+
+if rank == 0:
+    a_full = np.random.uniform(-1, 1, N).astype("d")
+else:
+    a_full = None
+
+a_local = np.empty(chunk_size, dtype="d")
+
+comm.Barrier()
+t_start = MPI.Wtime()
+
+comm.Scatter(a_full, a_local, root=0)
+
+t_end = MPI.Wtime()
+t_scatter = t_end - t_start
+
+
+if rank == 0:
+    total_bytes = 2 * N * 8
+    if t_scatter > 0:
+        speed_bytes_s = total_bytes / t_scatter
+    else:
+        speed_bytes_s = 0
+
+    print(f"--- Results for N = {N} ---")
+    print(f"Scatter time: {t_scatter:.8f} sec")
+    print(f"Speed S: {speed_bytes_s:.2f} bytes/s")
+    print(f"Speed S: {speed_bytes_s / 1e6:.2f} Mbytes/s")
+    print("-" * 30)
