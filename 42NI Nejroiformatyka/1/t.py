558162f1a4251e2c74e6df3a00dcc966b379e48e
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-24 14:33:24 +0200

    chore: plotting sigmoid activation function

--- 42NI Nejroiformatyka/1/t.py
+++ 42NI Nejroiformatyka/1/t.py
@@ -1,31 +1,20 @@
+from matplotlib import pyplot as plt
 import numpy as np
-import neurolab as nl
 
-target = [
-    [1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-]
-chars = list("NERO")
 
-target = np.asfarray(target)
-target[target == 0] = -1
+def sigmoid(x):
+    return 2 / (1 + np.exp(-2 * x)) - 1
 
-# Create and train network
-net = nl.net.newhop(target)
 
-output = net.sim(target)
-print("Test on train samples:")
-for i in range(len(target)):
-    print(
-        chars[i], (output[i] == target[i]).all(), "Sim. steps", len(net.layers[0].outs)
-    )
-print("\nTest on defaced N:")
-test = np.asfarray(
-    [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1]
-)
-test[test == 0] = -1
-out = net.sim([test])
+x = np.linspace(-10, 10, 100)
+y = sigmoid(x)
 
-print((out[0] == target[0]).all(), "Sim. steps", len(net.layers[0].outs))
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, label="Sigmoid Function", color="green")
+
+plt.xlabel("Input (x)")
+plt.ylabel("Output (Sigmoid (x))")
+plt.title("Sigmoidal Activation Function Plot")
+plt.grid(True)
+plt.legend()
+plt.show()
