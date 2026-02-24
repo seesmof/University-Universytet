b3eb3edda67bc44d5871b2c6d37de7860d0ed6f6
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-24 16:30:57 +0200

    docs -- NI: add an individual task

--- 42NI Nejroiformatyka/1/t.py
+++ 42NI Nejroiformatyka/1/t.py
@@ -2,20 +2,19 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def relu(x):
-    return np.maximum(0, x)
+def sigmoid(x):
+    return 2 / (1 + np.exp(-2 * x)) - 1
 
 
 x = np.linspace(-10, 10, 100)
-y = relu(x)
+y = sigmoid(x)
 
-plt.figure(figsize=(10, 7))
-plt.plot(x, y, label="ReLU Function")
-plt.title("ReLU Activation Function Plot")
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, label="Sigmoid Function", color="green")
+
 plt.xlabel("Input (x)")
-plt.ylabel("Output (f(x) = max(0,x))")
+plt.ylabel("Output (Sigmoid (x))")
+plt.title("Sigmoidal Activation Function Plot")
 plt.grid(True)
 plt.legend()
-plt.axhline(0, color="black", linewidth=1)
-plt.axvline(0, color="black", linewidth=1)
 plt.show()
