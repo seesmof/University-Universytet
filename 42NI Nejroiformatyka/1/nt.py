e18e32273c59bdeedf7a494ca414fbab4248cc49
Oleg Onyshchenko <seesmwork@gmail.com>
2026-03-27 09:58:11 +0200

    chore -- NI: copying code

---
+++ 42NI Nejroiformatyka/1/nt.py
@@ -0,0 +1,21 @@
+import random
+
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+
+iris = load_iris()
+print(iris.target_names)
+datasets = train_test_split(iris.data, iris.target, test_size=0.2)
+train_data, test_data, train_labels, test_labels = datasets
+p = Perceptron(random_state=42, max_iter=30, tol=0.001)
+p.fit(train_data, train_labels)
+
+sample = random.sample(range(len(train_data)), 10)
+for i in sample:
+    print(i, p.predict([train_data[i]]))
+
+print(classification_report(p.predict(train_data), train_labels))
+print(classification_report(p.predict(test_data), test_labels))
--- 42NI Nejroiformatyka/1/t.py
+++ 42NI Nejroiformatyka/1/t.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
-from sklearn.datasets import make_blobs
+from sklearn.datasets import load_iris, make_blobs
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 
