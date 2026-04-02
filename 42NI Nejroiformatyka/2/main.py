b503eb02c94d6a1c7e6db9124ecb69a1d3ebb572
Oleg Onyshchenko <seesmwork@gmail.com>
2026-04-02 13:43:33 +0300

    feat -- NI: done the normalization

--- 42NI Nejroiformatyka/2/main.py
+++ 42NI Nejroiformatyka/2/main.py
@@ -1,5 +1,27 @@
 import numpy as np
+import random
 
+# зробити порогову функцію
 X = np.random.rand(500, 18)
-y = np.random.randint(0, 2, 500)
+# зробити діапазон [-1, 1]
+y = [random.choice([-1, 1]) for _ in range(500)]
 
+for row_i, row in enumerate(X):
+    row_average = np.mean(row)
+
+    for cel_i, el in enumerate(row):
+        if el < row_average:
+            X[row_i][cel_i] = -1
+        elif el >= row_average:
+            X[row_i][cel_i] = 1
+
+# --- Хопфілд ---
+
+"""
+hopfield_net = cpl.HopfieldNet(num_cells=35)
+# hopfield = HopfieldNetwork(N=19)
+print(hopfield_net.train(X))
+"""
+
+# --- Ельман ---
+# подивитись ельмана
