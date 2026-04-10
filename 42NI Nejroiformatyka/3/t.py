07b3b8c8a8d57f0108f98a52a9126f69d3aab191
Oleg Onyshchenko <seesmwork@gmail.com>
2026-04-10 11:10:47 +0300

    feat -- NI: adding the activation functions. D: adding the stuff

--- 42NI Nejroiformatyka/3/t.py
+++ 42NI Nejroiformatyka/3/t.py
@@ -6,13 +6,24 @@ def heavyside(x: int):
     return 1 if x >= 0 else 0
 
 
+def linera_bipolar(x, k, a1, a2):
+    return 1 if x > a2 else k * x if a1 <= x <= a2 else -1
+
+
 # Linear
 c = 5
 x = 10
 res = linear(c, x)
-print(f"{c=} * {x=} = {res=}")
+print(f"{c=} * {x=} = {res}")
 
 # Heavyside
 x = -2
 res = heavyside(x)
+print(res)
+
+# Linear Bipolar
+x = 3
+a1, a2 = 0, 10
+k = 5
+res = linera_bipolar(x, k, a1, a2)
 print(res)
