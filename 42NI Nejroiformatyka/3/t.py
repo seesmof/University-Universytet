568bf75a0936ec52f4c0fa30befd53211c30205b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-04-14 12:29:58 +0300

    feat -- NI: calculate weighted sum

--- 42NI Nejroiformatyka/3/t.py
+++ 42NI Nejroiformatyka/3/t.py
@@ -1,29 +1,9 @@
-def linear(c: int, x: int):
-    return c * x
+def weighted_sum(x, w):
+    return sum([xi * wi for xi, wi in zip(x, w)])
 
 
-def heavyside(x: int):
-    return 1 if x >= 0 else 0
+x = [10, 2, 0]
+w = [0.8, 0.2, 0]
 
-
-def linera_bipolar(x, k, a1, a2):
-    return 1 if x > a2 else k * x if a1 <= x <= a2 else -1
-
-
-# Linear
-c = 5
-x = 10
-res = linear(c, x)
-print(f"{c=} * {x=} = {res}")
-
-# Heavyside
-x = -2
-res = heavyside(x)
-print(res)
-
-# Linear Bipolar
-x = 3
-a1, a2 = 0, 10
-k = 5
-res = linera_bipolar(x, k, a1, a2)
+res = weighted_sum(x, w)
 print(res)
