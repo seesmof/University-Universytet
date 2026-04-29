0468316bf09d73cbb515c3c07f21719ffd6949ce
Oleg Onyshchenko <seesmwork@gmail.com>
2026-04-29 15:31:46 +0300

    feat -- SI: nicegui doesnt work, Jesus thank you

--- 42SI Stvorennja IDE/4/nicegui/main.py
+++ 42SI Stvorennja IDE/4/nicegui/main.py
@@ -1,7 +1,13 @@
-from nicegui import ui
+from nicegui import ui, app
 
-ui.textarea(placeholder="Your code here...").props("outlined").classes("w-full")
+
+ui.textarea(placeholder="Your code here...").props("outlined").classes(
+    "w-full"
+).bind_value_to(app.storage.general, "input")
 ui.textarea().props("outlined disable").classes("w-full")
 ui.button("Run").classes("w-full").props("outline")
+
+value = app.storage.general["input"]
+print(value)
 
 ui.run(title="Rust IDE", favicon="🚜")
