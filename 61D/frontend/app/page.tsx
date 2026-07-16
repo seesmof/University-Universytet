cf0042e1ed5f16acd5935ba3ccf8ba175fa21f3f
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-16 12:52:47 +0300

    feat: make fetching work

--- 61D/frontend/app/page.tsx
+++ 61D/frontend/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 const API_URL = "https://open-bible-api.vercel.app";
 
@@ -8,9 +8,17 @@ export default function IndexPage() {
   const [reference, setReference] = useState<string>("");
   const [text, setText] = useState<string>("");
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch(`${API_URL}/${reference}`);
+    const data = await response.json();
+    setText(data.verse);
+  };
+
   return (
     <div className="max-w-3xl mx-auto w-full p-3">
-      <form className="flex gap-3">
+      <form className="flex gap-3" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Please enter a Bible reference..."
