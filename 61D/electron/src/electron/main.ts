8b349f41ff8afe8484f28baf8c7144667208b63c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-18 19:01:52 +0300

    chore: electron failed, trying tauri

--- 61D/electron/src/electron/main.ts
+++ 61D/electron/src/electron/main.ts
@@ -1,6 +1,10 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
+interface User {
+  name: string;
+}
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
