8d24a072f2b54804c192ddf2b239225bc9b17bac
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-30 18:28:31 +0300

    feat: redirect to list by default

--- 51PM Projektuvannja modeluvannja/1/app/client/frontend/urls.py
+++ 51PM Projektuvannja modeluvannja/1/app/client/frontend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list", views.measurements_list, name="list"),
+    # path("list", views.measurements_list, name="list"),
+    path("", views.index_view, name="index"),
+    path("list", views.MeasurementList.as_view(), name="list"),
     path("create", views.create_measurement, name="create"),
 ]
--- 51PM Projektuvannja modeluvannja/1/app/client/frontend/views.py
+++ 51PM Projektuvannja modeluvannja/1/app/client/frontend/views.py
@@ -1,6 +1,11 @@
+import os
+from typing import Any
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
+from django.views import generic
 
+from django.conf import settings
 from .forms import MeasurementForm
 from .models import Measurement
 
@@ -15,7 +20,6 @@ def create_measurement(req: HttpRequest):
     if req.method == "POST":
         form = MeasurementForm(req.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             measurement = Measurement()
             measurement.value = form.cleaned_data["value"]
             measurement.description = form.cleaned_data["description"]
@@ -25,3 +29,27 @@ def create_measurement(req: HttpRequest):
         form = MeasurementForm()
 
     return render(req, "measurements/create.html", {"form": form})
+
+
+def index_view(req: HttpRequest):
+    return redirect("list")
+
+
+class MeasurementList(generic.ListView):
+    model = Measurement
+    context_object_name = "measurements"
+    template_name = "measurements/list.html"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        try:
+            file_path: str = os.path.join(settings.BASE_DIR, "test_data.txt")
+            with open(file_path, encoding="utf-8", mode="r") as f:
+                lines = f.readlines()
+            for value in lines:
+                print(value)
+                Measurement.objects.get_or_create(value=value)
+        except IOError:
+            print("ERROR: Couldn't read the file.")
+            pass
+
+        return super(MeasurementList, self).get(request, *args, **kwargs)
