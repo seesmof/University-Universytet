0fe6ad7902f8bc447b531c60545b535b3e876d67
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-31 08:35:01 +0300

    feat: add the doctor model

---
+++ 51PM Projektuvannja modeluvannja/1/project/app/frontend/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_full_name(value: str):
+    if not " " in value:
+        raise ValidationError(
+            "Full name does not contain a space.", params={"value": value}
+        )
+
+
+class Doctor(models.Model):
+    full_name = models.CharField(max_length=250, validators=[validate_full_name])
