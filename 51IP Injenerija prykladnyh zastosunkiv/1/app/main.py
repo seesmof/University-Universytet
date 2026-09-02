ac4549921e71402b5864c03216740ed45243d306
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-02 13:14:15 +0300

    feat: add the wind direction plotting

--- 51IP Injenerija prykladnyh zastosunkiv/1/app/main.py
+++ 51IP Injenerija prykladnyh zastosunkiv/1/app/main.py
@@ -42,3 +42,31 @@ plt.show()
 df["wv (m/s)"] = df["wv (m/s)"].clip(lower=0.0)
 df["max. wv (m/s)"] = df["max. wv (m/s)"].clip(lower=0.0)
 print(df["wv (m/s)"].min())
+
+
+plt.hist2d(df["wd (deg)"], df["wv (m/s)"], bins=(50, 50), vmax=400)
+plt.colorbar()
+plt.xlabel("Wind Direction [deg]")
+plt.ylabel("Wind Velocity [m/s]")
+plt.show()
+
+
+wv = df.pop("wv (m/s)")
+max_wv = df.pop("max. wv (m/s)")
+
+wd_rad = df.pop("wd (deg)") * np.pi / 180
+
+df["Wx"] = wv * np.cos(wd_rad)
+df["Wy"] = wv * np.sin(wd_rad)
+
+df["max Wx"] = max_wv * np.cos(wd_rad)
+df["max Wy"] = max_wv * np.sin(wd_rad)
+
+
+plt.hist2d(df["Wx"], df["Wy"], bins=(50, 50), vmax=400)
+plt.colorbar()
+plt.xlabel("Wind X [m/s]")
+plt.ylabel("Wind Y [m/s]")
+ax = plt.gca()
+ax.axis("tight")
+plt.show()
