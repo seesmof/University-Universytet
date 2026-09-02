d001b0ebe8d451d6f808ec3a3c1183056de030a9
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-02 11:46:57 +0300

    feat: code for lab 1 intro done

--- 51IP Injenerija prykladnyh zastosunkiv/1/app/main.py
+++ 51IP Injenerija prykladnyh zastosunkiv/1/app/main.py
@@ -364,3 +364,33 @@ wide_conv_window = WindowGenerator(
 )
 print(wide_conv_window)
 wide_conv_window.plot(conv_model)
+
+lstm_model = tf.keras.models.Sequential(
+    [
+        # Shape [batch, time, features] => [batch, time, lstm_units]
+        tf.keras.layers.LSTM(32, return_sequence=True),
+        # Shape => [batch, time, features]
+        tf.keras.layers.Dense(units=1),
+    ]
+)
+
+history = compile_and_fit(lstm_model, wide_window)
+val_performance["LSTM"] = lstm_model.evaluate(wide_window.val)
+performance["LSRM"] = lstm_model.evaluate(wide_window.test, verbose=0)
+wide_window.plot(lstm_model)
+
+x = np.arange(len(performance))
+width = 0.3
+metric_name = "mean_absolute_error"
+metric_index = lstm_model.metrics_names.index("mean_absolute_error")
+val_mae = [v[metric_index] for v in val_performance.values()]
+test_mae = [v[metric_index] for v in performance.values()]
+
+plt.ylabel("mean_absolute_error [T (degC), normalized]")
+plt.bar(x - 0.17, val_mae, width, label="Validation")
+plt.bar(x + 0.17, test_mae, width, label="Test")
+plt.xticks(ticks=x, labels=performance.keys(), rotation=45)
+_ = plt.legend()
+
+for name, value in performance.items():
+    print(f"{name:12s}: {value[1]:0.4f}")
