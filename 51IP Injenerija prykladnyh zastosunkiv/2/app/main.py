a0419d7335f576810ebe439965f966f93a0f47c2
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-15 15:36:37 +0300

    chore: following the guides

--- 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
+++ 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
@@ -1,28 +1,19 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
 
-mnist = tf.keras.datasets.mnist
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+import pathlib
 
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10),
-    ]
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(
+    "flower_photos.tar", origin=dataset_url, extract=True
 )
-predictions = model(x_train[:1]).numpy()
-print(predictions)
-result = tf.nn.softmax(predictions).numpy()
-print(result)
+data_dir = pathlib.Path(data_dir).with_suffix("")
 
-loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-result = loss_function(y_train[:1], predictions).numpy()
-print(result)
-
-model.compile(optimizer="adam", loss=loss_function, metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=5)
-
-print(model.evaluate(x_test, y_test, verbose=2))
+image_count = len(list(data_dir.glob("*/*.jpg")))
+print(image_count)
