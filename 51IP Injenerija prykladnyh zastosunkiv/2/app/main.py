d397af599eee1cd22510fd0785e6a550cbe59892
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-16 12:04:22 +0300

    chore: failing to get opencv up and running

--- 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
+++ 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
@@ -1,19 +1,8 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import numpy as np
-import PIL
+import cv2
+import matplotlib.pyplot as plt
 
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-
-import pathlib
-
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file(
-    "flower_photos.tar", origin=dataset_url, extract=True
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
-data_dir = pathlib.Path(data_dir).with_suffix("")
-
-image_count = len(list(data_dir.glob("*/*.jpg")))
-print(image_count)
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
