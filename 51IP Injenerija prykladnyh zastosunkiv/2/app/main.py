44e6f1fdb1cf5d327d80f9b192a5be8ecfd8a637
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-16 17:05:09 +0300

    chore: create a django app, didnt go well

--- 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
+++ 51IP Injenerija prykladnyh zastosunkiv/2/app/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+cv2.cvtColor
+
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
