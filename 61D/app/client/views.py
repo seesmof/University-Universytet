b40419575ce3815711238aed7608602263d4ce04
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-18 21:36:08 +0300

    feat: render a home page

--- 61D/app/client/views.py
+++ 61D/app/client/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request: HttpRequest):
+    context = dict()
+    return render(request, "client/index.html", context)
