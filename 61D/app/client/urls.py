35c2d03b18f09aa45d738ad2f8a8c82e1acbcbad
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-18 21:50:08 +0300

    feat: add a task list view

--- 61D/app/client/urls.py
+++ 61D/app/client/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("tasks/", views.TasksList.as_view(), name="tasks"),
 ]
--- 61D/app/client/views.py
+++ 61D/app/client/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Task
 
 
 # Create your views here.
 def index(request: HttpRequest):
     context = dict()
     return render(request, "client/index.html", context)
+
+
+class TasksList(ListView):
+    model = Task
