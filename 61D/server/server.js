bcdb6930bc73afaa8fdcabe3cb5217b8c5325234
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-19 11:06:39 +0300

    chore: finish tutorial code

--- 61D/server/server.js
+++ 61D/server/server.js
@@ -55,6 +55,40 @@ app.get("/posts/:id", async (req, res) => {
   }
 });
 
+app.put("/posts/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const { data: updatedData, error: updatedError } = await supabase
+      .from("posts")
+      .update({
+        title: req.body.title ? req.body.title : data[0].title,
+        body: req.body.body ? req.body.body : data[0].body,
+      })
+      .eq("id", req.params.id);
+    const { data, err } = await supabase.from("posts").select();
+    return res.status(200).send(data);
+  } catch (error) {
+    return res.send({ error });
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const { data, error } = await supabase
+      .from("posts")
+      .delete()
+      .eq("id", req.params.id);
+    const { datar, errorr } = await supabase.from("posts").select();
+    if (error) {
+      return res.status(400).json(error);
+    }
+    return res.send(datar);
+  } catch (error) {
+    return res.send({ error });
+  }
+});
+
 app.listen(port, () =>
   console.log(`Listening on port ${port}: http://localhost:${port}`),
 );
