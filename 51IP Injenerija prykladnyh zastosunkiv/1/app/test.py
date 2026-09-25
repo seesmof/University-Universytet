9f9b82868f0426484df6e66a78e26f0808653277
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-25 12:08:12 +0300

    feat: calculate circle's area

--- 51IP Injenerija prykladnyh zastosunkiv/1/app/test.py
+++ 51IP Injenerija prykladnyh zastosunkiv/1/app/test.py
@@ -1,14 +1,10 @@
-def f(x: int) -> int:
-    return x * 2
+import numpy as np
 
 
-tests = [
-    (2, 4),
-    (4, 8),
-    (10, 20),
-]
-for test in tests:
-    test_input, expected_output = test
-    output = f(test_input)
-    assert output == expected_output, f"Failed for {test_input}."
-print("All tests ran well.")
+def circle_area(r: int | float):
+    return np.pi * r**2
+
+
+radius = 3
+result = circle_area(radius)
+print(f"{result} for {radius} radius.")
