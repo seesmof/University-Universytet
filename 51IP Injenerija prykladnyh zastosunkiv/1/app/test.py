d4ba61a0b373338f556cd9e9f3385d72881dbfc4
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-25 11:58:08 +0300

    feat: testing a simple math function

--- 51IP Injenerija prykladnyh zastosunkiv/1/app/test.py
+++ 51IP Injenerija prykladnyh zastosunkiv/1/app/test.py
@@ -1,1 +1,14 @@
-print(24 / 15)
+def f(x: int) -> int:
+    return x * 2
+
+
+tests = [
+    (2, 4),
+    (4, 8),
+    (10, 20),
+]
+for test in tests:
+    test_input, expected_output = test
+    output = f(test_input)
+    assert output == expected_output, f"Failed for {test_input}."
+print("All tests ran well.")
